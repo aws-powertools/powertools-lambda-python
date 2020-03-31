094aaee9183aa255c162d4801fd89f9283b7760e
Heitor Lessa <heitor.lessa@hotmail.com>
2020-03-31 17:38:32 +0100

    [Python] Add example (#13)
    * fix: delete Pipenv files
    
    * feat: add example project
    
    * fix: clean up duplicate readme
    
    * chore: linting
    
    * fix: CI build
    
    Co-authored-by: heitorlessa <lessa@amazon.co.uk>

---
+++ python/example/hello_world/app.py
@@ -0,0 +1,51 @@
+import json
+
+from aws_lambda_powertools.logging import logger_inject_lambda_context, logger_setup
+from aws_lambda_powertools.tracing import Tracer
+
+# import requests
+
+tracer = Tracer()
+logger = logger_setup()
+
+@tracer.capture_lambda_handler
+@logger_inject_lambda_context
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    logger.debug("Fetching current location")
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+    
+    logger.info("Returning message to the caller")
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
