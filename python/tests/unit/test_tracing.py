bfe3404a50e3df10ef340635e8baf474d739e61c
heitorlessa <lessa@amazon.co.uk>
2020-04-18 21:06:46 +0100

    improv(tracer): test auto patch method

--- python/tests/unit/test_tracing.py
+++ python/tests/unit/test_tracing.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pytest
 
 from aws_lambda_powertools.tracing import Tracer
@@ -127,3 +129,27 @@ def test_tracer_custom_annotation(mocker, dummy_response, xray_stub):
 
     assert put_annotation_mock.call_count == 1
     assert put_annotation_mock.call_args == mocker.call(key=annotation_key, value=annotation_value)
+
+@mock.patch('aws_lambda_powertools.tracing.Tracer.patch')
+def test_tracer_autopatch(patch_mock):
+    # GIVEN tracer is instantiated
+    # WHEN default options were used, or patch() was called
+    # THEN tracer should patch all modules
+    tracer_a = Tracer(disabled=True)    
+    
+    assert patch_mock.call_count == 1
+
+    tracer_b = Tracer(disabled=True, auto_patch=False)
+    tracer_b.patch()
+
+    assert patch_mock.call_count == 2
+
+@mock.patch('aws_lambda_powertools.tracing.Tracer.patch')
+def test_tracer_no_autopatch(patch_mock):
+    # GIVEN tracer is instantiated
+    # WHEN auto_patch is disabled
+    # THEN tracer should not patch any module
+    tracer_a = Tracer(disabled=True, auto_patch=False)
+    tracer_b = tracer_a.instance()
+
+    assert patch_mock.call_count == 0
