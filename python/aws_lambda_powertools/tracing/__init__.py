4b22f7533a4eac632c3208ad21d53972ad126dff
heitorlessa <lessa@amazon.co.uk>
2020-05-08 20:19:39 +0100

    chore: linting

--- python/aws_lambda_powertools/tracing/__init__.py
+++ python/aws_lambda_powertools/tracing/__init__.py
@@ -1,6 +1,7 @@
 """Tracing utility
 """
-from .tracer import Tracer
 from aws_xray_sdk.ext.aiohttp.client import aws_xray_trace_config as aiohttp_trace_config
+
+from .tracer import Tracer
 
 __all__ = ["Tracer", "aiohttp_trace_config"]
