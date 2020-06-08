ad8de347749273cf3d8fc38edaa743fe27ce53e1
heitorlessa <lessa@amazon.co.uk>
2020-06-08 11:15:30 +0100

    feat: add core utilities as high level imports

--- aws_lambda_powertools/__init__.py
+++ aws_lambda_powertools/__init__.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for Lambda Python Powertools."""
-import logging
+
+
+from .logging import Logger  # noqa: F401
+from .metrics import Metrics, single_metric  # noqa: F401
+from .tracing import Tracer  # noqa: F401
 
 __author__ = """Amazon Web Services"""
 
-logger = logging.getLogger("aws_lambda_powertools")
-logger.addHandler(logging.NullHandler())
-logger.propagate = False
+
+def set_package_logging_null_handler():
+    import logging
+
+    logger = logging.getLogger("aws_lambda_powertools")
+    logger.addHandler(logging.NullHandler())
+    logger.propagate = False
+
+
+set_package_logging_null_handler()
