5860e7e8eed06ed53311824b4332772184414512
heitorlessa <lessa@amazon.co.uk>
2020-08-11 18:28:49 +0200

    improv: logger test utility no longer necessary
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- tests/functional/utility_functions.py
+++
@@ -1,8 +0,0 @@
-import secrets
-
-from aws_lambda_powertools import Logger
-
-
-def get_random_logger(**kwargs):
-    """Return vanilla logger w/ random name"""
-    return Logger(name=secrets.token_urlsafe(), **kwargs)
