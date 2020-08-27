c999f9604499853ad95ceb9ff79abc4ff3a50170
Guilherme Martins Crocetti <gmcrocetti@gmail.com>
2020-08-26 21:51:28 -0300

    refactor: changes partial_sqs middleware in favor of a generic interface always expecting a BatchProcessor

--- aws_lambda_powertools/utilities/batch/middlewares.py
+++ aws_lambda_powertools/utilities/batch/middlewares.py
@@ -7,21 +7,47 @@ from typing import Callable, Dict
 
 from aws_lambda_powertools.middleware_factory import lambda_handler_decorator
 
-from .sqs import PartialSQSProcessor
+from .base import BaseProcessor
 
 
 @lambda_handler_decorator
-def partial_sqs_processor(
-    handler: Callable, event: Dict, context: Dict, record_handler: Callable, processor: PartialSQSProcessor = None
+def batch_processor(
+    handler: Callable, event: Dict, context: Dict, record_handler: Callable, processor: BaseProcessor = None
 ):
     """
+    Middleware to handle batch event processing
+
+    Parameters
+    ----------
+    handler: Callable
+        Lambda's handler
+    event: Dict
+        Lambda's Event
+    context: Dict
+        Lambda's Context
+    record_handler: Callable
+        Callable to process each record from the batch
+    processor: PartialSQSProcessor
+        Batch Processor to handle partial failure cases
 
     Examples
     --------
+    **Processes Lambda's event with PartialSQSProcessor**
+        >>> from aws_lambda_powertools.utilities.batch import batch_processor
+        >>>
+        >>> def record_handler(record):
+        >>>     return record["body"]
+        >>>
+        >>> @batch_processor(record_handler=record_handler, processor=PartialSQSProcessor)
+        >>> def handler(event, context):
+        >>>     return {"StatusCode": 200}
+
+    Limitations
+    -----------
+    * Async batch processors
 
     """
     records = event["Records"]
-    processor = processor or PartialSQSProcessor()
 
     with processor(records, record_handler) as ctx:
         ctx.process()
