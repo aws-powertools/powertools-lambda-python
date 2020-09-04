349e88d09f1b3535be10bd3a5ed553bf317a2500
heitorlessa <lessa@amazon.co.uk>
2020-09-04 17:32:48 +0200

    feat: add cloudwatch_logs based on Bryan's feedback

--- aws_lambda_powertools/utilities/validation/envelopes.py
+++ aws_lambda_powertools/utilities/validation/envelopes.py
@@ -7,3 +7,4 @@ SNS = "Records[0].Sns.Message"
 EVENTBRIDGE = "detail"
 CLOUDWATCH_EVENTS_SCHEDULED = EVENTBRIDGE
 KINESIS_DATA_STREAM = "Records[*].kinesis.powertools_base64(data) | powertools_json(@)"
+CLOUDWATCH_LOGS = "awslogs.powertools_base64(data) | powertools_json(@)"
