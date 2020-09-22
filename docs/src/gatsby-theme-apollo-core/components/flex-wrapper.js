286bf92958fc304f0d5e9463a06d041cedf638ae
heitorlessa <lessa@amazon.co.uk>
2020-09-22 15:34:45 +0200

    improv: increase width for content

--- docs/src/gatsby-theme-apollo-core/components/flex-wrapper.js
+++ docs/src/gatsby-theme-apollo-core/components/flex-wrapper.js
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 const FlexWrapper = styled.div({
     display: 'flex',
     minHeight: '100vh',
-    maxWidth: 1600,
+    maxWidth: '87vw',
     margin: '0 auto'
 });
 
