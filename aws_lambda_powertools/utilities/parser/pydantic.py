bcb763b0128145144cae7cbecceab0a603b7cb86
Heitor Lessa <heitor.lessa@hotmail.com>
2020-10-13 11:01:29 +0200

    Update aws_lambda_powertools/utilities/parser/pydantic.py
    Co-authored-by: Joris Conijn <jco@woodwing.com>

--- aws_lambda_powertools/utilities/parser/pydantic.py
+++ aws_lambda_powertools/utilities/parser/pydantic.py
@@ -3,6 +3,6 @@
 # As Pydantic export new types, new utilities, we will have to keep up
 # with a project that's not used in our core functionalities.
 # For this reason, we're relying on Pydantic's __all__ attr to allow customers
-# to use `from from aws_lambda_powertools.utilities.parser.pydantic import <anything>`
+# to use `from aws_lambda_powertools.utilities.parser.pydantic import <anything>`
 
 from pydantic import *  # noqa: F403,F401
