c005cc76112d6a03ff1598e05b496ca0f1ca3673
Ran Isenberg <ran.isenberg@cyberk.com>
2020-11-22 11:10:34 +0200

    feat: Add Ses lambda event support to Parser utility #213

--- aws_lambda_powertools/utilities/parser/models/__init__.py
+++ aws_lambda_powertools/utilities/parser/models/__init__.py
@@ -1,5 +1,6 @@
 from .dynamodb import DynamoDBStreamChangedRecordModel, DynamoDBStreamModel, DynamoDBStreamRecordModel
 from .event_bridge import EventBridgeModel
+from .ses import SesModel, SesRecordModel
 from .sns import SnsModel, SnsNotificationModel, SnsRecordModel
 from .sqs import SqsModel, SqsRecordModel
 
@@ -8,6 +9,8 @@ __all__ = [
     "EventBridgeModel",
     "DynamoDBStreamChangedRecordModel",
     "DynamoDBStreamRecordModel",
+    "SesModel",
+    "SesRecordModel",
     "SnsModel",
     "SnsNotificationModel",
     "SnsRecordModel",
