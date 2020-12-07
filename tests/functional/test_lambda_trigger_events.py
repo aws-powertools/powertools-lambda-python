0ab2786f44f6363cbc7e79f75f74f5d158077e71
Joris Conijn <jco@woodwing.com>
2020-12-07 19:45:42 +0100

    refactor: convert dict into a literal dict object and re-use it

--- tests/functional/test_lambda_trigger_events.py
+++ tests/functional/test_lambda_trigger_events.py
@@ -49,10 +49,13 @@ def test_dict_wrapper_equals():
         def message(self) -> str:
             return self.get("message")
 
-    assert DataClassSample({"message": "foo1"}) == DataClassSample({"message": "foo1"})
-    assert DataClassSample({"message": "foo1"}) != DataClassSample({"message": "foo2"})
-    assert DataClassSample({"message": "foo1"}) is not object()
-    assert object() is not DataClassSample({"message": "foo1"})
+    data1 = {"message": "foo1"}
+    data2 = {"message": "foo2"}
+
+    assert DataClassSample(data1) == DataClassSample(data1)
+    assert DataClassSample(data1) != DataClassSample(data2)
+    assert DataClassSample(data1) is not data1
+    assert data1 is not DataClassSample(data1)
 
 
 def test_cloud_watch_trigger_event():
