0cfda341b47ac1e1a52b0805cbd9e6add931fd70
Tom McCarthy <thommcc@amazon.com>
2021-01-25 17:35:14 +0100

    fix: Add tracebacks for all caught exceptions to SQSBatchProcessingError output

--- aws_lambda_powertools/utilities/batch/exceptions.py
+++ aws_lambda_powertools/utilities/batch/exceptions.py
@@ -1,7 +1,28 @@
 """
 Batch processing exceptions
 """
+import traceback
 
 
 class SQSBatchProcessingError(Exception):
     """When at least one message within a batch could not be processed"""
+
+    def __init__(self, msg="", child_exceptions=()):
+        super().__init__(msg)
+        self.msg = msg
+        self.child_exceptions = child_exceptions
+
+    def __str__(self):
+        parent_exception_str = super(SQSBatchProcessingError, self).__str__()
+        exception_list = [f"{parent_exception_str}\n"]
+
+        for exception in self.child_exceptions:
+            extype, ex, tb = exception
+            formatted = "".join(traceback.format_exception(extype, ex, tb))
+            exception_list.append(formatted)
+
+        return "\n".join(exception_list)
+
+    def log(self):
+        for exc in self.child_exceptions:
+            print(exc)
