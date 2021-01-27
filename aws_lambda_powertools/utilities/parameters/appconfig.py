d3eb4e4e5182cc886e4836eae90edfaacd7e0679
Michael Brewer <michael.brewer@gyft.com>
2021-01-27 08:11:43 -0800

    docs(appconfig): Use correct import for docstring (#271)
    * docs(appconfig): Use correct import for docstring
    
    * docs: Rename variable back

--- aws_lambda_powertools/utilities/parameters/appconfig.py
+++ aws_lambda_powertools/utilities/parameters/appconfig.py
@@ -32,7 +32,7 @@ class AppConfigProvider(BaseProvider):
     -------
     **Retrieves the latest configuration value from App Config**
 
-        >>> from aws_lambda_powertools.utilities.parameters import AppConfigProvider
+        >>> from aws_lambda_powertools.utilities import parameters
         >>> appconf_provider = parameters.AppConfigProvider(environment="my_env", application="my_app")
         >>>
         >>> value : bytes = appconf_provider.get("my_conf")
@@ -43,7 +43,7 @@ class AppConfigProvider(BaseProvider):
     **Retrieves a configuration value from App Config in another AWS region**
 
         >>> from botocore.config import Config
-        >>> from aws_lambda_powertools.utilities.parameters import AppConfigProvider
+        >>> from aws_lambda_powertools.utilities import parameters
         >>>
         >>> config = Config(region_name="us-west-1")
         >>> appconf_provider = parameters.AppConfigProvider(environment="my_env", application="my_app", config=config)
