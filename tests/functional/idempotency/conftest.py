834db1c37063659082ad7a28b2f8797f9ea8f914
Tom McCarthy <thommcc@amazon.com>
2021-02-09 17:20:39 +0100

    chore: improve test coverage, refactor fixtures

--- tests/functional/idempotency/conftest.py
+++ tests/functional/idempotency/conftest.py
@@ -59,6 +59,54 @@ def expected_params_update_item(lambda_response, hashed_idempotency_key):
 
 
 @pytest.fixture
+def expected_params_update_item_with_validation(lambda_response, hashed_idempotency_key, hashed_validation_key):
+    return {
+        "ExpressionAttributeNames": {
+            "#expiry": "expiration",
+            "#response_data": "data",
+            "#status": "status",
+            "#validation_key": "validation",
+        },
+        "ExpressionAttributeValues": {
+            ":expiry": stub.ANY,
+            ":response_data": json.dumps(lambda_response),
+            ":status": "COMPLETED",
+            ":validation_key": hashed_validation_key,
+        },
+        "Key": {"id": hashed_idempotency_key},
+        "TableName": "TEST_TABLE",
+        "UpdateExpression": "SET #response_data = :response_data, "
+        "#expiry = :expiry, #status = :status, "
+        "#validation_key = :validation_key",
+    }
+
+
+@pytest.fixture
+def expected_params_put_item(hashed_idempotency_key):
+    return {
+        "ConditionExpression": "attribute_not_exists(id) OR expiration < :now",
+        "ExpressionAttributeValues": {":now": stub.ANY},
+        "Item": {"expiration": stub.ANY, "id": hashed_idempotency_key, "status": "INPROGRESS"},
+        "TableName": "TEST_TABLE",
+    }
+
+
+@pytest.fixture
+def expected_params_put_item_with_validation(hashed_idempotency_key, hashed_validation_key):
+    return {
+        "ConditionExpression": "attribute_not_exists(id) OR expiration < :now",
+        "ExpressionAttributeValues": {":now": stub.ANY},
+        "Item": {
+            "expiration": stub.ANY,
+            "id": hashed_idempotency_key,
+            "status": "INPROGRESS",
+            "validation": hashed_validation_key,
+        },
+        "TableName": "TEST_TABLE",
+    }
+
+
+@pytest.fixture
 def hashed_idempotency_key(lambda_apigw_event):
     return hashlib.md5(json.dumps(lambda_apigw_event["body"]).encode()).hexdigest()
 
@@ -69,26 +117,23 @@ def hashed_validation_key(lambda_apigw_event):
 
 
 @pytest.fixture
-def persistence_store(config):
-    persistence_store = DynamoDBPersistenceLayer(event_key_jmespath="body", table_name=TABLE_NAME, boto_config=config)
-    return persistence_store
-
-
-@pytest.fixture
-def persistence_store_with_cache(config):
+def persistence_store(config, request):
     persistence_store = DynamoDBPersistenceLayer(
-        event_key_jmespath="body", table_name=TABLE_NAME, boto_config=config, use_local_cache=True
+        event_key_jmespath="body",
+        table_name=TABLE_NAME,
+        boto_config=config,
+        use_local_cache=request.param["use_local_cache"],
     )
     return persistence_store
 
 
 @pytest.fixture
-def persistence_store_with_validation(config):
+def persistence_store_with_validation(config, request):
     persistence_store = DynamoDBPersistenceLayer(
         event_key_jmespath="body",
         table_name=TABLE_NAME,
         boto_config=config,
-        use_local_cache=True,
+        use_local_cache=request.param,
         payload_validation_jmespath="requestContext",
     )
     return persistence_store
--- tests/functional/idempotency/test_idempotency.py
+++ tests/functional/idempotency/test_idempotency.py
@@ -14,6 +14,9 @@ from aws_lambda_powertools.utilities.idempotency.idempotency import idempotent
 TABLE_NAME = "TEST_TABLE"
 
 
+# Using parametrize to run test twice, with two separate instances of persistence store. One instance with caching
+# enabled, and one without.
+@pytest.mark.parametrize("persistence_store", [{"use_local_cache": False}, {"use_local_cache": True}], indirect=True)
 def test_idempotent_lambda_already_completed(
     persistence_store, lambda_apigw_event, timestamp_future, lambda_response, hashed_idempotency_key,
 ):
@@ -51,6 +54,7 @@ def test_idempotent_lambda_already_completed(
     stubber.deactivate()
 
 
+@pytest.mark.parametrize("persistence_store", [{"use_local_cache": False}, {"use_local_cache": True}], indirect=True)
 def test_idempotent_lambda_in_progress(
     persistence_store, lambda_apigw_event, lambda_response, timestamp_future, hashed_idempotency_key
 ):
@@ -88,14 +92,75 @@ def test_idempotent_lambda_in_progress(
             ex.value.args[0] == "Execution already in progress with idempotency key: "
             "body=a3edd699125517bb49d562501179ecbd"
         )
-        print(ex)
 
     stubber.assert_no_pending_responses()
     stubber.deactivate()
 
 
+@pytest.mark.parametrize("persistence_store", [{"use_local_cache": True}], indirect=True)
+def test_idempotent_lambda_in_progress_with_cache(
+    persistence_store, lambda_apigw_event, lambda_response, timestamp_future, hashed_idempotency_key, mocker
+):
+    """
+    Test idempotent decorator where lambda_handler is already processing an event with matching event key, cache
+    enabled.
+    """
+    save_to_cache_spy = mocker.spy(persistence_store, "_save_to_cache")
+    retrieve_from_cache_spy = mocker.spy(persistence_store, "_retrieve_from_cache")
+    stubber = stub.Stubber(persistence_store.table.meta.client)
+
+    expected_params = {
+        "TableName": TABLE_NAME,
+        "Key": {"id": hashed_idempotency_key},
+        "ConsistentRead": True,
+    }
+    ddb_response = {
+        "Item": {
+            "id": {"S": hashed_idempotency_key},
+            "expiration": {"N": timestamp_future},
+            "data": {"S": '{"message": "test", "statusCode": 200}'},
+            "status": {"S": "INPROGRESS"},
+        }
+    }
+
+    stubber.add_client_error("put_item", "ConditionalCheckFailedException")
+    stubber.add_response("get_item", ddb_response, expected_params)
+    stubber.activate()
+
+    @idempotent(persistence_store=persistence_store)
+    def lambda_handler(event, context):
+        return lambda_response
+
+    loops = 3
+    for _ in range(loops):
+        with pytest.raises(IdempotencyAlreadyInProgressError) as ex:
+            lambda_handler(lambda_apigw_event, {})
+            assert (
+                ex.value.args[0] == "Execution already in progress with idempotency key: "
+                "body=a3edd699125517bb49d562501179ecbd"
+            )
+
+    assert retrieve_from_cache_spy.call_count == 2 * loops
+    retrieve_from_cache_spy.assert_called_with(idempotency_key=hashed_idempotency_key)
+
+    assert save_to_cache_spy.call_count == 1
+    first_call_args_data_record = save_to_cache_spy.call_args_list[0].kwargs["data_record"]
+    assert first_call_args_data_record.idempotency_key == hashed_idempotency_key
+    assert first_call_args_data_record.status == "INPROGRESS"
+    assert persistence_store._cache.get(hashed_idempotency_key)
+
+    stubber.assert_no_pending_responses()
+    stubber.deactivate()
+
+
+@pytest.mark.parametrize("persistence_store", [{"use_local_cache": False}, {"use_local_cache": True}], indirect=True)
 def test_idempotent_lambda_first_execution(
-    persistence_store, lambda_apigw_event, expected_params_update_item, lambda_response, hashed_idempotency_key
+    persistence_store,
+    lambda_apigw_event,
+    expected_params_update_item,
+    expected_params_put_item,
+    lambda_response,
+    hashed_idempotency_key,
 ):
     """
     Test idempotent decorator when lambda is executed with an event with a previously unknown event key
@@ -104,12 +169,6 @@ def test_idempotent_lambda_first_execution(
     stubber = stub.Stubber(persistence_store.table.meta.client)
     ddb_response = {}
 
-    expected_params_put_item = {
-        "ConditionExpression": "attribute_not_exists(id) OR expiration < :now",
-        "ExpressionAttributeValues": {":now": stub.ANY},
-        "Item": {"expiration": stub.ANY, "id": hashed_idempotency_key, "status": "INPROGRESS"},
-        "TableName": "TEST_TABLE",
-    }
     stubber.add_response("put_item", ddb_response, expected_params_put_item)
     stubber.add_response("update_item", ddb_response, expected_params_update_item)
     stubber.activate()
@@ -124,54 +183,60 @@ def test_idempotent_lambda_first_execution(
     stubber.deactivate()
 
 
+@pytest.mark.parametrize("persistence_store", [{"use_local_cache": True}], indirect=True)
 def test_idempotent_lambda_first_execution_cached(
-    persistence_store_with_cache,
+    persistence_store,
     lambda_apigw_event,
     expected_params_update_item,
+    expected_params_put_item,
     lambda_response,
     hashed_idempotency_key,
+    mocker,
 ):
     """
     Test idempotent decorator when lambda is executed with an event with a previously unknown event key. Ensure
     result is cached locally on the persistence store instance.
     """
-
-    stubber = stub.Stubber(persistence_store_with_cache.table.meta.client)
+    save_to_cache_spy = mocker.spy(persistence_store, "_save_to_cache")
+    retrieve_from_cache_spy = mocker.spy(persistence_store, "_retrieve_from_cache")
+    stubber = stub.Stubber(persistence_store.table.meta.client)
     ddb_response = {}
-
-    expected_params_put_item = {
-        "ConditionExpression": "attribute_not_exists(id) OR expiration < :now",
-        "ExpressionAttributeValues": {":now": stub.ANY},
-        "Item": {"expiration": stub.ANY, "id": hashed_idempotency_key, "status": "INPROGRESS"},
-        "TableName": "TEST_TABLE",
-    }
 
     stubber.add_response("put_item", ddb_response, expected_params_put_item)
     stubber.add_response("update_item", ddb_response, expected_params_update_item)
     stubber.activate()
 
-    @idempotent(persistence_store=persistence_store_with_cache)
+    @idempotent(persistence_store=persistence_store)
     def lambda_handler(event, context):
         return lambda_response
 
     lambda_handler(lambda_apigw_event, {})
 
-    assert persistence_store_with_cache._cache.get(hashed_idempotency_key)
+    assert retrieve_from_cache_spy.call_count == 1
+    assert save_to_cache_spy.call_count == 2
+    first_call_args, second_call_args = save_to_cache_spy.call_args_list
+    assert first_call_args.args[0].status == "INPROGRESS"
+    assert second_call_args.args[0].status == "COMPLETED"
+    assert persistence_store._cache.get(hashed_idempotency_key)
 
     # This lambda call should not call AWS API
     lambda_handler(lambda_apigw_event, {})
+    assert retrieve_from_cache_spy.call_count == 3
+    retrieve_from_cache_spy.assert_called_with(idempotency_key=hashed_idempotency_key)
 
     # This assertion fails if an AWS API operation was called more than once
     stubber.assert_no_pending_responses()
     stubber.deactivate()
 
 
+@pytest.mark.parametrize("persistence_store", [{"use_local_cache": False}, {"use_local_cache": True}], indirect=True)
 def test_idempotent_lambda_expired(
     persistence_store,
     lambda_apigw_event,
     timestamp_expired,
     lambda_response,
     expected_params_update_item,
+    expected_params_put_item,
     hashed_idempotency_key,
 ):
     """
@@ -183,13 +248,6 @@ def test_idempotent_lambda_expired(
 
     ddb_response = {}
 
-    expected_params_put_item = {
-        "ConditionExpression": "attribute_not_exists(id) OR expiration < :now",
-        "ExpressionAttributeValues": {":now": stub.ANY},
-        "Item": {"expiration": stub.ANY, "id": hashed_idempotency_key, "status": "INPROGRESS"},
-        "TableName": "TEST_TABLE",
-    }
-
     stubber.add_response("put_item", ddb_response, expected_params_put_item)
     stubber.add_response("update_item", ddb_response, expected_params_update_item)
     stubber.activate()
@@ -204,9 +262,14 @@ def test_idempotent_lambda_expired(
     stubber.deactivate()
 
 
-# Note - this test will need to change depending on how we define event handling behavior
+@pytest.mark.parametrize("persistence_store", [{"use_local_cache": False}, {"use_local_cache": True}], indirect=True)
 def test_idempotent_lambda_exception(
-    persistence_store, lambda_apigw_event, timestamp_future, lambda_response, hashed_idempotency_key
+    persistence_store,
+    lambda_apigw_event,
+    timestamp_future,
+    lambda_response,
+    hashed_idempotency_key,
+    expected_params_put_item,
 ):
     """
     Test idempotent decorator when lambda is executed with an event with a previously unknown event key, but
@@ -219,12 +282,6 @@ def test_idempotent_lambda_exception(
     stubber = stub.Stubber(persistence_store.table.meta.client)
 
     ddb_response = {}
-    expected_params_put_item = {
-        "ConditionExpression": "attribute_not_exists(id) OR expiration < :now",
-        "ExpressionAttributeValues": {":now": stub.ANY},
-        "Item": {"expiration": stub.ANY, "id": hashed_idempotency_key, "status": "INPROGRESS"},
-        "TableName": "TEST_TABLE",
-    }
     expected_params_delete_item = {"TableName": TABLE_NAME, "Key": {"id": hashed_idempotency_key}}
 
     stubber.add_response("put_item", ddb_response, expected_params_put_item)
@@ -242,7 +299,10 @@ def test_idempotent_lambda_exception(
     stubber.deactivate()
 
 
-def test_idempotent_lambda_already_completed_bad_payload(
+@pytest.mark.parametrize(
+    "persistence_store_with_validation", [{"use_local_cache": False}, {"use_local_cache": True}], indirect=True
+)
+def test_idempotent_lambda_already_completed_with_validation_bad_payload(
     persistence_store_with_validation,
     lambda_apigw_event,
     timestamp_future,
@@ -283,6 +343,7 @@ def test_idempotent_lambda_already_completed_bad_payload(
     stubber.deactivate()
 
 
+@pytest.mark.parametrize("persistence_store", [{"use_local_cache": False}, {"use_local_cache": True}], indirect=True)
 def test_idempotent_lambda_expired_during_request(
     persistence_store,
     lambda_apigw_event,
@@ -306,18 +367,19 @@ def test_idempotent_lambda_expired_during_request(
             "status": {"S": "INPROGRESS"},
         }
     }
+    ddb_response_get_item_missing = {}
     expected_params_get_item = {
         "TableName": TABLE_NAME,
         "Key": {"id": hashed_idempotency_key},
         "ConsistentRead": True,
     }
 
-    # Record repeatedly changes between put_item and get_item
+    # Simulate record repeatedly changing state between put_item and get_item
     stubber.add_client_error("put_item", "ConditionalCheckFailedException")
     stubber.add_response("get_item", ddb_response_get_item, expected_params_get_item)
 
     stubber.add_client_error("put_item", "ConditionalCheckFailedException")
-    stubber.add_response("get_item", copy.deepcopy(ddb_response_get_item), copy.deepcopy(expected_params_get_item))
+    stubber.add_response("get_item", ddb_response_get_item_missing)
 
     stubber.add_client_error("put_item", "ConditionalCheckFailedException")
     stubber.add_response("get_item", copy.deepcopy(ddb_response_get_item), copy.deepcopy(expected_params_get_item))
@@ -336,24 +398,22 @@ def test_idempotent_lambda_expired_during_request(
     stubber.deactivate()
 
 
-def test_idempotent_persistence_exception(
-    persistence_store, lambda_apigw_event, timestamp_future, lambda_response, hashed_idempotency_key
+@pytest.mark.parametrize("persistence_store", [{"use_local_cache": False}, {"use_local_cache": True}], indirect=True)
+def test_idempotent_persistence_exception_updating(
+    persistence_store,
+    lambda_apigw_event,
+    timestamp_future,
+    lambda_response,
+    hashed_idempotency_key,
+    expected_params_put_item,
 ):
     """
     Test idempotent decorator when lambda is executed with an event with a previously unknown event key, but
     lambda_handler raises an exception which is retryable.
     """
-
-    # Stub the boto3 client
     stubber = stub.Stubber(persistence_store.table.meta.client)
 
     ddb_response = {}
-    expected_params_put_item = {
-        "ConditionExpression": "attribute_not_exists(id) OR expiration < :now",
-        "ExpressionAttributeValues": {":now": stub.ANY},
-        "Item": {"expiration": stub.ANY, "id": hashed_idempotency_key, "status": "INPROGRESS"},
-        "TableName": "TEST_TABLE",
-    }
 
     stubber.add_response("put_item", ddb_response, expected_params_put_item)
     stubber.add_client_error("delete_item", "UnrecoverableError")
@@ -365,6 +425,70 @@ def test_idempotent_persistence_exception(
 
     with pytest.raises(IdempotencyPersistenceLayerError):
         lambda_handler(lambda_apigw_event, {})
+
+    stubber.assert_no_pending_responses()
+    stubber.deactivate()
+
+
+@pytest.mark.parametrize("persistence_store", [{"use_local_cache": False}, {"use_local_cache": True}], indirect=True)
+def test_idempotent_persistence_exception_deleting(
+    persistence_store,
+    lambda_apigw_event,
+    timestamp_future,
+    lambda_response,
+    hashed_idempotency_key,
+    expected_params_put_item,
+):
+    """
+    Test idempotent decorator when lambda is executed with an event with a previously unknown event key, but
+    lambda_handler raises an exception which is retryable.
+    """
+    stubber = stub.Stubber(persistence_store.table.meta.client)
+
+    ddb_response = {}
+
+    stubber.add_response("put_item", ddb_response, expected_params_put_item)
+    stubber.add_client_error("update_item", "UnrecoverableError")
+    stubber.activate()
+
+    @idempotent(persistence_store=persistence_store)
+    def lambda_handler(event, context):
+        return {"message": "success!"}
+
+    with pytest.raises(IdempotencyPersistenceLayerError):
+        lambda_handler(lambda_apigw_event, {})
+
+    stubber.assert_no_pending_responses()
+    stubber.deactivate()
+
+
+@pytest.mark.parametrize(
+    "persistence_store_with_validation", [{"use_local_cache": False}, {"use_local_cache": True}], indirect=True
+)
+def test_idempotent_lambda_first_execution_with_validation(
+    persistence_store_with_validation,
+    lambda_apigw_event,
+    expected_params_update_item_with_validation,
+    expected_params_put_item_with_validation,
+    lambda_response,
+    hashed_idempotency_key,
+    hashed_validation_key,
+):
+    """
+    Test idempotent decorator when lambda is executed with an event with a previously unknown event key
+    """
+    stubber = stub.Stubber(persistence_store_with_validation.table.meta.client)
+    ddb_response = {}
+
+    stubber.add_response("put_item", ddb_response, expected_params_put_item_with_validation)
+    stubber.add_response("update_item", ddb_response, expected_params_update_item_with_validation)
+    stubber.activate()
+
+    @idempotent(persistence_store=persistence_store_with_validation)
+    def lambda_handler(lambda_apigw_event, context):
+        return lambda_response
+
+    lambda_handler(lambda_apigw_event, {})
 
     stubber.assert_no_pending_responses()
     stubber.deactivate()
