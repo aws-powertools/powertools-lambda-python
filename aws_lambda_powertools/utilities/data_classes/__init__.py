56f2cba0ffe835f4bec7ae7e36b395ee8f49bfb2
Giancarlo Rubio <gianrubio@gmail.com>
2021-02-23 11:07:07 +0100

    Update __init__.py

--- aws_lambda_powertools/utilities/data_classes/__init__.py
+++ aws_lambda_powertools/utilities/data_classes/__init__.py
@@ -1,8 +1,7 @@
 from .alb_event import ALBEvent
 from .api_gateway_proxy_event import APIGatewayProxyEvent, APIGatewayProxyEventV2
 from .cloud_watch_logs_event import CloudWatchLogsEvent
-from .dynamo_db_stream_event import DynamoDBStreamEvent
-from .dynamo_db_stream_event import DynamoDBRecordEventName
+from .dynamo_db_stream_event import DynamoDBStreamEvent, DynamoDBRecordEventName
 from .event_bridge_event import EventBridgeEvent
 from .kinesis_stream_event import KinesisStreamEvent
 from .s3_event import S3Event
