b041f3fb904781da9816c1f412691a362ec78a70
Michael Brewer <michael.brewer@gyft.com>
2021-03-04 22:18:39 -0800

    chore: Hide missing code coverage

--- aws_lambda_powertools/tracing/extensions.py
+++ aws_lambda_powertools/tracing/extensions.py
@@ -8,8 +8,8 @@ def aiohttp_trace_config():
     TraceConfig
         aiohttp trace config
     """
-    from aws_xray_sdk.ext.aiohttp.client import aws_xray_trace_config
+    from aws_xray_sdk.ext.aiohttp.client import aws_xray_trace_config  # pragma: no cover
 
-    aws_xray_trace_config.__doc__ = "aiohttp extension for X-Ray (aws_xray_trace_config)"
+    aws_xray_trace_config.__doc__ = "aiohttp extension for X-Ray (aws_xray_trace_config)"  # pragma: no cover
 
-    return aws_xray_trace_config()
+    return aws_xray_trace_config()  # pragma: no cover
