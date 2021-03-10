c3fa1173770f2e5f03ac0c2dcce41f3a8bf373bf
Michael Brewer <michael.brewer@gyft.com>
2021-03-10 12:56:17 -0800

    fix(logging): Correct paths for AppSync

--- aws_lambda_powertools/logging/correlation_paths.py
+++ aws_lambda_powertools/logging/correlation_paths.py
@@ -2,6 +2,6 @@
 
 API_GATEWAY_REST = "requestContext.requestId"
 API_GATEWAY_HTTP = API_GATEWAY_REST
-APPSYNC_RESOLVER = "request.headers.x-amzn-trace-id"
-APPLICATION_LOAD_BALANCER = "headers.x-amzn-trace-id"
+APPSYNC_RESOLVER = 'request.headers."x-amzn-trace-id"'
+APPLICATION_LOAD_BALANCER = 'headers."x-amzn-trace-id"'
 EVENT_BRIDGE = "id"
