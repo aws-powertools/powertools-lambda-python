fb6839aa94903950d47b883a10624587ac3c6507
Michael Brewer <michael.brewer@gyft.com>
2021-03-24 22:21:49 -0700

    fix(parser): Check python version_info for typing Changes: - Use sys.version_info to determine where we need typing_extensions
    Reference: https://mypy.readthedocs.io/en/stable/runtime_troubles.html#using-new-additions-to-the-typing-module

--- aws_lambda_powertools/utilities/parser/types.py
+++ aws_lambda_powertools/utilities/parser/types.py
@@ -1,13 +1,14 @@
 """Generics and other shared types used across parser"""
 
+import sys
 from typing import TypeVar
 
 from pydantic import BaseModel
 
 # We only need typing_extensions for python versions <3.8
-try:
+if sys.version_info >= (3, 8):
     from typing import Literal  # noqa: F401
-except ImportError:
+else:
     from typing_extensions import Literal  # noqa: F401
 
 Model = TypeVar("Model", bound=BaseModel)
