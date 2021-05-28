18d664d6b2bc3c7bdf4f0eaaeddfbdd595675c1f
Michael Brewer <michael.brewer@gyft.com>
2021-05-28 12:45:17 -0700

    tests(data-classes): make the mock persistence layer reusable

--- tests/functional/idempotency/test_idempotency.py
+++ tests/functional/idempotency/test_idempotency.py
@@ -21,6 +21,7 @@ from aws_lambda_powertools.utilities.idempotency.exceptions import (
 from aws_lambda_powertools.utilities.idempotency.idempotency import idempotent
 from aws_lambda_powertools.utilities.idempotency.persistence.base import BasePersistenceLayer, DataRecord
 from aws_lambda_powertools.utilities.validation import envelopes, validator
+from tests.functional.utils import load_event
 
 TABLE_NAME = "TEST_TABLE"
 
@@ -849,36 +850,39 @@ def test_handler_raise_idempotency_key_error(persistence_store: DynamoDBPersiste
     assert "No data found to create a hashed idempotency_key" == e.value.args[0]
 
 
+class MockPersistenceLayer(BasePersistenceLayer):
+    def __init__(self, expected_idempotency_key: str):
+        self.expected_idempotency_key = expected_idempotency_key
+        super(MockPersistenceLayer, self).__init__()
+
+    def _put_record(self, data_record: DataRecord) -> None:
+        assert data_record.idempotency_key == self.expected_idempotency_key
+
+    def _update_record(self, data_record: DataRecord) -> None:
+        assert data_record.idempotency_key == self.expected_idempotency_key
+
+    def _get_record(self, idempotency_key) -> DataRecord:
+        ...
+
+    def _delete_record(self, data_record: DataRecord) -> None:
+        ...
+
+
 def test_idempotent_lambda_event_source(lambda_context):
-    # Scenario to validate that we can use the event_source before the idempotent decorator
-    expected_idempotency_key = "test-func#" + hashlib.md5(json.dumps({}).encode()).hexdigest()
+    # Scenario to validate that we can use the event_source decorator before or after the idempotent decorator
+    mock_event = load_event("apiGatewayProxyV2Event.json")
+    persistence_layer = MockPersistenceLayer("test-func#" + hashlib.md5(json.dumps(mock_event).encode()).hexdigest())
     expected_result = {"message": "Foo"}
 
-    class MockPersistenceLayer(BasePersistenceLayer):
-        def __init__(self):
-            super(MockPersistenceLayer, self).__init__()
-
-        def _put_record(self, data_record: DataRecord) -> None:
-            assert data_record.idempotency_key == expected_idempotency_key
-
-        def _update_record(self, data_record: DataRecord) -> None:
-            assert data_record.idempotency_key == expected_idempotency_key
-
-        def _get_record(self, idempotency_key) -> DataRecord:
-            ...
-
-        def _delete_record(self, data_record: DataRecord) -> None:
-            ...
-
-    # GIVEN an event_source with a parsed in data class
-    # AND idempotent decorator
+    # GIVEN an event_source decorator
+    # AND then an idempotent decorator
     @event_source(data_class=APIGatewayProxyEventV2)
-    @idempotent(persistence_store=MockPersistenceLayer())
+    @idempotent(persistence_store=persistence_layer)
     def lambda_handler(event, _):
         assert isinstance(event, APIGatewayProxyEventV2)
         return expected_result
 
     # WHEN calling the lambda handler
-    result = lambda_handler({}, lambda_context)
-    # THEN we expect the handler to execute succesfully
+    result = lambda_handler(mock_event, lambda_context)
+    # THEN we expect the handler to execute successfully
     assert result == expected_result
