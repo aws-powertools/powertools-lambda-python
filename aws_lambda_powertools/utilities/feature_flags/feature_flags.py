8246b2ec4132e07f8af5cbbe55f5ae3a57fb73cf
Ran Isenberg <ran.isenberg@cyberark.com>
2021-08-05 10:52:36 +0300

    lint

--- aws_lambda_powertools/utilities/feature_flags/feature_flags.py
+++ aws_lambda_powertools/utilities/feature_flags/feature_flags.py
@@ -66,12 +66,10 @@ class FeatureFlags:
 
         for condition in conditions:
             context_value = context.get(str(condition.get(schema.CONDITION_KEY)))
-            cond_action = condition.get(schema.CONDITION_ACTION, "")
+            cond_value = condition.get(schema.CONDITION_ACTION, "")
             cond_value = condition.get(schema.CONDITION_VALUE)
 
-            if not self._match_by_action(
-                action=condition_action, condition_value=condition_value, context_value=context_value
-            ):
+            if not self._match_by_action(action=cond_value, condition_value=cond_value, context_value=context_value):
                 logger.debug(
                     f"rule did not match action, rule_name={rule_name}, rule_value={rule_match_value}, "
                     f"name={feature_name}, context_value={str(context_value)} "
