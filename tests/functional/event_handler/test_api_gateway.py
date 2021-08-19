35058b60bf67283c47730319afbfb2fcafdab787
Michael Brewer <michael.brewer@gyft.com>
2021-08-19 08:18:11 -0700

    fix(api-gateway): strip stage name from request path

--- tests/functional/event_handler/test_api_gateway.py
+++ tests/functional/event_handler/test_api_gateway.py
@@ -769,3 +769,22 @@ def test_custom_serializer():
     body = response["body"]
     expected = '{"color": 1, "variations": ["dark", "light"]}'
     assert expected == body
+
+
+def test_api_gateway_v2_raw_path():
+    # GIVEN a Http API V2 proxy type event
+    # AND a custom stage name "dev" and raw path "/dev/foo"
+    app = ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEventV2)
+    event = {"rawPath": "/dev/foo", "requestContext": {"http": {"method": "GET"}, "stage": "dev"}}
+
+    @app.get("/foo")
+    def foo():
+        return {}
+
+    # WHEN calling the event handler
+    # WITH a route "/foo"
+    result = app(event, {})
+
+    # THEN process event correctly
+    assert result["statusCode"] == 200
+    assert result["headers"]["Content-Type"] == content_types.APPLICATION_JSON
