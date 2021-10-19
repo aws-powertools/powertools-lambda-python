40a13a5f0553d3e4351b01d043b1673c2d618e93
Michael Brewer <michael.brewer@gyft.com>
2021-10-19 15:05:46 -0700

    chore: add rabbitmq data_class

--- tests/functional/data_classes/test_amazon_mq.py
+++ tests/functional/data_classes/test_amazon_mq.py
@@ -1,8 +1,11 @@
+from typing import Dict
+
 from aws_lambda_powertools.utilities.data_classes.active_mq_event import ActiveMQEvent, ActiveMQMessage
+from aws_lambda_powertools.utilities.data_classes.rabbit_mq_event import BasicProperties, RabbitMessage, RabbitMQEvent
 from tests.functional.utils import load_event
 
 
-def test_activate_mq_event():
+def test_active_mq_event():
     event = ActiveMQEvent(load_event("activeMQEvent.json"))
 
     assert event.event_source == "aws:amq"
@@ -30,4 +33,28 @@ def test_activate_mq_event():
 
     messages = list(event.messages)
     message = messages[1]
+    assert message.json_data["timeout"] == 0
+
+
+def test_rabbit_mq_event():
+    event = RabbitMQEvent(load_event("rabbitMQEvent.json"))
+
+    assert event.event_source == "aws:rmq"
+    assert event.event_source_arn is not None
+
+    message = event.rmq_messages_by_queue["pizzaQueue::/"][0]
+    assert isinstance(message, RabbitMessage)
+    properties = message.basic_properties
+    assert isinstance(properties, BasicProperties)
+    assert properties.content_type == "text/plain"
+    assert properties.content_encoding is None
+    assert isinstance(properties.headers, Dict)
+    assert properties.headers["header1"] is not None
+    assert properties.delivery_mode == 1
+    assert properties.priority == 34
+    assert properties.correlation_id is None
+
+    assert message.redelivered is False
+    assert message.data is not None
+    assert message.decoded_data is not None
     assert message.json_data["timeout"] == 0
