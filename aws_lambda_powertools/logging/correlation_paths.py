b92074c659dc02de7c957c24a5e7c96d25ea9a72
heitorlessa <lessa@amazon.co.uk>
2021-11-12 18:52:19 +0100

    feat(logger): add ALB correlation ID support

--- aws_lambda_powertools/logging/correlation_paths.py
+++ aws_lambda_powertools/logging/correlation_paths.py
@@ -2,6 +2,7 @@
 
 API_GATEWAY_REST = "requestContext.requestId"
 API_GATEWAY_HTTP = API_GATEWAY_REST
+ALB = "headers.x-amzn-trace-id"
 APPSYNC_AUTHORIZER = "requestContext.requestId"
 APPSYNC_RESOLVER = 'request.headers."x-amzn-trace-id"'
 APPLICATION_LOAD_BALANCER = 'headers."x-amzn-trace-id"'
