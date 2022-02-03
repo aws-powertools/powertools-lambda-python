168ec482502fb4a1a22038e3a2041459926e6591
Michael Brewer <michael.brewer@gyft.com>
2022-02-03 02:57:11 -0800

    feat(event-handler): new resolvers to fix current_event typing (#978)
    Co-authored-by: Heitor Lessa <heitor.lessa@hotmail.com>
    Co-authored-by: heitorlessa <lessa@amazon.co.uk>

--- tests/functional/test_metrics.py
+++ tests/functional/test_metrics.py
@@ -496,7 +496,8 @@ def test_log_metrics_capture_cold_start_metric(capsys, namespace, service):
     # THEN ColdStart metric and function_name and service dimension should be logged
     assert output["ColdStart"] == [1.0]
     assert output["function_name"] == "example_fn"
-    assert output['service'] == service
+    assert output["service"] == service
+
 
 def test_log_metrics_capture_cold_start_metric_no_service(capsys, namespace):
     # GIVEN Metrics is initialized without service
@@ -515,7 +516,7 @@ def test_log_metrics_capture_cold_start_metric_no_service(capsys, namespace):
     # THEN ColdStart metric and function_name dimension should be logged
     assert output["ColdStart"] == [1.0]
     assert output["function_name"] == "example_fn"
-    assert output.get('service') is None
+    assert output.get("service") is None
 
 
 def test_emit_cold_start_metric_only_once(capsys, namespace, service, metric):
