e74a516f1f6fb9939627664a5cb7b7a0f48792bf
Michael Brewer <michael.brewer@gyft.com>
2022-02-14 18:18:32 -0800

    chore: ignore false positives

--- aws_lambda_powertools/utilities/data_classes/secret_manager.py
+++ aws_lambda_powertools/utilities/data_classes/secret_manager.py
@@ -4,17 +4,17 @@ from aws_lambda_powertools.utilities.data_classes.common import DictWrapper
 
 
 class SecretsManagerRotationEventStep(Enum):
-    CREATE_SECRET = "createSecret"
+    CREATE_SECRET = "createSecret"  # nosec
     """The first step of rotation is to create a new version of the secret. Depending on your rotation strategy,
     the new version can contain a new password, a new username and password, or more secret information. Secrets
     Manager labels the new version with the staging label `AWSPENDING`."""
-    SET_SECRET = "setSecret"
+    SET_SECRET = "setSecret"  # nosec
     """Second step, rotation changes the credentials in the database or service to match the new credentials in the
     `AWSPENDING` version of the secret. """
-    TEST_SECRET = "testSecret"
+    TEST_SECRET = "testSecret"  # nosec
     """Third step, rotation tests the `AWSPENDING` version of the secret by using it to access the database or
     service."""
-    FINISH_SECRET = "finishSecret"
+    FINISH_SECRET = "finishSecret"  # nosec
     """Final step, rotation moves the label `AWSCURRENT` from the previous secret version to this version.
     Secrets Manager adds the `AWSPREVIOUS` staging label to the previous version, so that you retain the last known
     good version of the secret."""
