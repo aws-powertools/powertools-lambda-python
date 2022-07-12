db1b756127c3957c2b7e11d9738fb6df4dd6ae59
heitorlessa <lessa@amazon.co.uk>
2022-07-12 14:35:41 +0200

    fix(ci): move conditionals from yaml to code

--- .github/scripts/label_missing_related_issue.js
+++ .github/scripts/label_missing_related_issue.js
@@ -1,12 +1,29 @@
+const {
+  PR_ACTION,
+  PR_AUTHOR,
+  PR_BODY,
+  PR_NUMBER,
+  IGNORE_AUTHORS,
+  BLOCK_LABEL,
+  BLOCK_REASON_LABEL
+} = require("./constants")
+
 module.exports = async ({github, context, core}) => {
-    const prBody = process.env.PR_BODY;
-    const prNumber = process.env.PR_NUMBER;
-    const blockLabel = process.env.BLOCK_LABEL;
-    const blockReasonLabel = process.env.BLOCK_REASON_LABEL;
+    core.debug(`Number: ${PR_BODY}`);
+    core.debug(`Action: ${PR_ACTION}`);
+    core.debug(`Author: ${PR_AUTHOR}`);
+    core.debug(`Body: ${PR_BODY}`);
+
+    if (IGNORE_AUTHORS.includes(PR_AUTHOR)) {
+      return core.notice("Author in IGNORE_AUTHORS list; skipping...")
+    }
+
+    if (PR_ACTION != "opened") {
+      return core.notice("Only newly open PRs are labelled to avoid spam; skipping")
+    }
 
     const RELATED_ISSUE_REGEX = /Issue number:[^\d\r\n]+(?<issue>\d+)/;
-
-    const isMatch = RELATED_ISSUE_REGEX.exec(prBody);
+    const isMatch = RELATED_ISSUE_REGEX.exec(PR_BODY);
     if (isMatch == null) {
         core.info(`No related issue found, maybe the author didn't use the template but there is one.`)
 
@@ -15,14 +32,14 @@ module.exports = async ({github, context, core}) => {
           owner: context.repo.owner,
           repo: context.repo.repo,
           body: msg,
-          issue_number: prNumber,
+          issue_number: PR_NUMBER,
         });
 
         return await github.rest.issues.addLabels({
-          issue_number: prNumber,
+          issue_number: PR_NUMBER,
           owner: context.repo.owner,
           repo: context.repo.repo,
-          labels: [blockLabel, blockReasonLabel]
+          labels: [BLOCK_LABEL, BLOCK_REASON_LABEL]
         })
     }
 }
