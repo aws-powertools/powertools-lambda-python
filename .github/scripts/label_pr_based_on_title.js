3105db4008596a3c8a9170a9cb447c89707dcef5
Rúben Fonseca <fonseka@gmail.com>
2022-07-14 15:31:06 +0200

    chore(ci): automatically add area label based on title

--- .github/scripts/label_pr_based_on_title.js
+++ .github/scripts/label_pr_based_on_title.js
@@ -17,6 +17,24 @@ module.exports = async ({github, context, core}) => {
         "deprecated": DEPRECATED_REGEX,
     }
 
+	  const areas = [
+	  	"tracer",
+	  	"metric",
+	  	"utilities",
+	  	"logger",
+	  	"event_handlers",
+	  	"middleware_factory",
+	  	"idempotency",
+	  	"event_sources",
+	  	"feature_flags",
+	  	"parameters",
+	  	"batch",
+	  	"parser",
+	  	"validator",
+	  	"jmespath_util",
+	  	"lambda-layers",
+	  ];
+
     // Maintenance: We should keep track of modified PRs in case their titles change
     let miss = 0;
     try {
@@ -26,12 +44,27 @@ module.exports = async ({github, context, core}) => {
             if (isMatch != null) {
                 core.info(`Auto-labeling PR ${PR_NUMBER} with ${label}`)
 
-                return await github.rest.issues.addLabels({
-                issue_number: PR_NUMBER,
-                owner: context.repo.owner,
-                repo: context.repo.repo,
-                labels: [label]
+                await github.rest.issues.addLabels({
+                    issue_number: PR_NUMBER,
+                    owner: context.repo.owner,
+                    repo: context.repo.repo,
+                    labels: [label]
                 })
+
+				        const area = matches[2]; // second capture group contains the area
+                if (areas.indexOf(area) > -1) {
+                    core.info(`Auto-labeling PR ${PR_NUMBER} with area ${area}`);
+                    await github.rest.issues.addLabels({
+                        issue_number: PR_NUMBER,
+                        owner: context.repo.owner,
+                        repo: context.repo.repo,
+                        labels: [`area/${area}`],
+                    });
+                } else {
+                    core.debug(`'${PR_TITLE}' didn't match any known area.`);
+                }
+
+				        return;
             } else {
                 core.debug(`'${PR_TITLE}' didn't match '${label}' semantic.`)
                 miss += 1
