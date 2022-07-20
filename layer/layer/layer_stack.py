cad96a6740c0e06193fa6616f6f1af5758b4b16a
Heitor Lessa <lessa@amazon.com>
2022-07-20 10:52:15 +0200

    chore(layers): expand to all aws commercial regions (#1324)

--- layer/layer/layer_stack.py
+++ layer/layer/layer_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import RemovalPolicy, Stack
+from aws_cdk import CfnOutput, RemovalPolicy, Stack
 from aws_cdk.aws_lambda import CfnLayerVersionPermission
 from aws_cdk.aws_ssm import StringParameter
 from cdk_lambda_powertools_python_layer import LambdaPowertoolsLayer
@@ -27,3 +27,5 @@ class LayerStack(Stack):
         layer.apply_removal_policy(RemovalPolicy.RETAIN)
 
         StringParameter(self, "VersionArn", parameter_name=ssm_paramter_layer_arn, string_value=layer.layer_version_arn)
+
+        CfnOutput(self, "LatestLayerArn", value=layer.layer_version_arn)
