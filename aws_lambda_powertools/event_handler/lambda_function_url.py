febe483326f0dc269f114a7fa770a0290c100eb5
Rúben Fonseca <fonseka@gmail.com>
2022-08-03 14:12:44 +0200

    chore(docs): updated links

--- aws_lambda_powertools/event_handler/lambda_function_url.py
+++ aws_lambda_powertools/event_handler/lambda_function_url.py
@@ -6,6 +6,41 @@ from aws_lambda_powertools.utilities.data_classes import LambdaFunctionUrlEvent
 
 
 class LambdaFunctionUrlResolver(ApiGatewayResolver):
+    """AWS Lambda Function URL resolver
+
+    Notes:
+    -----
+    For now, this seems to work the same way as API Gateway HTTP APIs Payload Format Version 2.0.
+
+    Documentation:
+    - https://docs.aws.amazon.com/lambda/latest/dg/urls-configuration.html
+    - https://docs.aws.amazon.com/lambda/latest/dg/urls-invocation.html#urls-payloads
+
+    Examples
+    --------
+    Simple example with a custom lambda handler using the Tracer capture_lambda_handler decorator
+
+    ```python
+    from aws_lambda_powertools import Tracer
+    from aws_lambda_powertools.event_handler import LambdaFunctoinUrlResolver
+
+    tracer = Tracer()
+    app = LambdaFunctoinUrlResolver()
+
+    @app.get("/get-call")
+    def simple_get():
+        return {"message": "Foo"}
+
+    @app.post("/post-call")
+    def simple_post():
+        post_data: dict = app.current_event.json_body
+        return {"message": post_data["value"]}
+
+    @tracer.capture_lambda_handler
+    def lambda_handler(event, context):
+        return app.resolve(event, context)
+    """
+
     current_event: LambdaFunctionUrlEvent
 
     def __init__(
--- aws_lambda_powertools/utilities/data_classes/lambda_function_url_event.py
+++ aws_lambda_powertools/utilities/data_classes/lambda_function_url_event.py
@@ -6,14 +6,13 @@ class LambdaFunctionUrlEvent(APIGatewayProxyEventV2):
 
     Notes:
     -----
-    For now, this seems to follow the exact payload as HTTP APIs Payload Format Version 2.0.
-    Certain keys in this payload format don't make sense for function urls (e.g: `routeKey`).
-    These keys will always be null.
+    For now, this seems to follow the exact same payload as HTTP APIs Payload Format Version 2.0.
+    Certain keys in this payload format don't make sense for function urls (e.g: `routeKey`, `stage`).
+    These keys will have default values that come on the payload, but they are not useful since they can't be changed.
 
     Documentation:
+    - https://docs.aws.amazon.com/lambda/latest/dg/urls-configuration.html
     - https://docs.aws.amazon.com/lambda/latest/dg/urls-invocation.html#urls-payloads
-    - https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-develop-integrations-lambda.html
-
     """
 
     pass
