b9889dc021788dc56a14842499570cb66c19227a
Leandro Damascena <leandro.damascena@gmail.com>
2022-08-06 20:40:22 +0100

    docs(jmespath_function): Refactoring codes for demonstration purposes - generic envelope - changing to more clean and userful code

--- examples/jmespath_functions/src/powertools_json_jmespath_function.py
+++ examples/jmespath_functions/src/powertools_json_jmespath_function.py
@@ -1,10 +1,30 @@
-import random
-from json import JSONDecodeError
+import json
+from dataclasses import asdict, dataclass, is_dataclass
+from uuid import uuid4
 
 import powertools_json_jmespath_schema as schemas
 
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.utilities.validation import SchemaValidationError, validate
+
+
+@dataclass
+class Order:
+    order_id: str
+    user_id: int
+    product_id: int
+    quantity: int
+    price: float
+    currency: str
+
+
+class DataclassCustomEncoder(json.JSONEncoder):
+    """A custom JSON encoder to serialize dataclass obj"""
+
+    def default(self, obj):
+        # Only called for values that aren't JSON serializable
+        # where `obj` will be an instance of Todo in this example
+        return asdict(obj) if is_dataclass(obj) else super().default(obj)
 
 
 def lambda_handler(event, context: LambdaContext) -> dict:
@@ -13,10 +33,14 @@ def lambda_handler(event, context: LambdaContext) -> dict:
     try:
         validate(event=event, schema=schemas.INPUT, envelope="powertools_json(payload)")
 
-        order = create_order(event["payload"])
-        return {"order_id": order.get("id"), "message": order.get("message"), "statusCode": 200}
-    except JSONDecodeError:
-        return return_error_message("Payload must be valid JSON (plain text).")
+        order = create_order(json.loads(event["payload"]))
+        return {
+            "order": json.dumps(order.get("order"), cls=DataclassCustomEncoder),
+            "message": order.get("message"),
+            "success": True,
+        }
+    except json.JSONDecodeError:
+        return return_error_message("Payload must be valid JSON (base64 encoded).")
     except SchemaValidationError as exception:
 
         # if validation fails, a SchemaValidationError will be raised with the wrong fields
@@ -24,9 +48,18 @@ def lambda_handler(event, context: LambdaContext) -> dict:
 
 
 def create_order(event: dict) -> dict:
-    # Add your logic here. For demostration purposes, this return a random order number
-    return {"id": random.randint(1, 100), "message": "order created"}
+    # This return an Order dataclass
+    order_id = str(uuid4())
+    order = Order(
+        order_id=order_id,
+        user_id=event.get("user_id"),
+        product_id=event.get("product_id"),
+        quantity=event.get("quantity"),
+        price=event.get("price"),
+        currency=event.get("currency"),
+    )
+    return {"order": order, "message": "order created"}
 
 
 def return_error_message(message: str) -> dict:
-    return {"order_id": None, "message": message, "statusCode": 400}
+    return {"order": None, "message": message, "success": False}
