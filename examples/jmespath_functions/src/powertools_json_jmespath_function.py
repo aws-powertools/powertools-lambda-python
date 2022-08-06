9f96bd8ea4e790f0d236127ce894ace0cb857aab
Leandro Damascena <leandro.damascena@gmail.com>
2022-08-06 18:50:27 +0100

    docs(jmespath_function): Refactoring codes for demonstration purposes - generic envelope

--- examples/jmespath_functions/src/powertools_json_jmespath_function.py
+++ examples/jmespath_functions/src/powertools_json_jmespath_function.py
@@ -1,7 +1,25 @@
+import random
+
 import powertools_json_jmespath_schema as schemas
 
-from aws_lambda_powertools.utilities.validation import validate
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.utilities.validation import SchemaValidationError, validate
 
-sample_event = {"data": '{"payload": {"message": "hello hello", "username": "blah blah"}}'}
 
-validate(event=sample_event, schema=schemas.INPUT, envelope="powertools_json(data)")
+def lambda_handler(event, context: LambdaContext) -> dict:
+
+    # Let's validate the schema
+    try:
+        validate(event=event, schema=schemas.INPUT, envelope="powertools_json(payload)")
+
+        order = create_order(event["payload"])
+        return {"order_id": order.get("id"), "message": order.get("message"), "statusCode": 200}
+    except SchemaValidationError as exception:
+
+        # if validation fails, a SchemaValidationError will be raised with the wrong fields
+        return {"order_id": None, "statusCode": 400, "message": str(exception)}
+
+
+def create_order(event: dict) -> dict:
+    # Add your logic here. For demostration purposes, this return a random order number
+    return {"id": random.randint(1, 100), "message": "order created"}
