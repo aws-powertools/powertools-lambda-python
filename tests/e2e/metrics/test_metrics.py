0280d9f82e23e33e31a6a078b6e6a1627d8285db
heitorlessa <lessa@amazon.co.uk>
2022-08-08 09:21:49 +0200

    refactor: update test_metrics to use new verbose infra

--- tests/e2e/metrics/test_metrics.py
+++ tests/e2e/metrics/test_metrics.py
@@ -1,25 +1,11 @@
 import datetime
 import json
-import uuid
 from typing import Dict, Type
 
 import pytest
-from e2e import conftest
 from e2e.utils import helpers
 
 from tests.e2e.metrics.infrastructure import MetricsStack
-
-
-@pytest.fixture(scope="module")
-def config() -> conftest.LambdaConfig:
-    return {
-        "parameters": {},
-        "environment_variables": {
-            "POWERTOOLS_METRICS_NAMESPACE": "powertools-e2e-metric",
-            "POWERTOOLS_SERVICE_NAME": "test-powertools-service",
-            "METRIC_NAME": f"business-metric-{str(uuid.uuid4()).replace('-','_')}",
-        },
-    }
 
 
 @pytest.fixture
@@ -28,17 +14,19 @@ def infra_outputs(infrastructure: Type[MetricsStack]):
 
 
 def test_basic_lambda_metric_is_visible(infra_outputs: Dict[str, str]):
-    # sourcery skip: aware-datetime-for-utc
-    execution_time = datetime.datetime.utcnow()
-    metric_name = "test"
+    # GIVEN
+    metric_name = "test-two"
     service = "test-metric-is-visible"
     namespace = "powertools-e2e-metric"
     event = json.dumps({"metric_name": metric_name, "service": service, "namespace": namespace})
+
+    # NOTE: Need to try creating a dynamic enum/dataclass w/ Literal types to make discovery easier
+    # it might not be possible
+
+    # WHEN
+    execution_time = datetime.datetime.utcnow()
     ret = helpers.trigger_lambda(lambda_arn=infra_outputs.get("basichandlerarn"), payload=event)
 
-    assert ret is None  # we could test in the actual response now
-
-    # NOTE: find out why we're getting empty metrics
     metrics = helpers.get_metrics(
         start_date=execution_time,
         end_date=execution_time + datetime.timedelta(minutes=2),
@@ -46,13 +34,22 @@ def test_basic_lambda_metric_is_visible(infra_outputs: Dict[str, str]):
         service_name=service,
         metric_name=metric_name,
     )
-    assert metrics is not None
+
+    # THEN
+    assert len(metrics.get("Timestamps", [])) == 1
+    metric_data = metrics.get("Values", [])
+    assert metric_data and metric_data[0] == 1
+
+    # for later... we could break the test early if the function failed
+    # we could extend `trigger_lambda` with a default param to check on that
+    # making the test less verbose. For explicit invoke failure, we could override
+    assert ret is not None
 
 
-# helpers: create client on the fly if not passed
-#          accept payload to be sent as part of invocation
 # helpers: adjust retries and wait to be much smaller
+# helpers: make retry config adjustable
 # Infra: Create dynamic Enum/DataClass to reduce guessing on outputs
 # Infra: Fix outputs
 # Infra: Add temporary Powertools Layer
 # Powertools: should have a method to set namespace at runtime
+# Create separate Infra class so they can live side by side
