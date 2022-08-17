70516cab39b5eeec843185c94a6faf529c8709bc
heitorlessa <lessa@amazon.co.uk>
2022-08-17 15:34:33 +0200

    chore: address import feedback lint

--- tests/e2e/utils/data_builder/__init__.py
+++ tests/e2e/utils/data_builder/__init__.py
@@ -9,16 +9,3 @@ from tests.e2e.utils.data_builder.metrics import (
     build_put_metadata_input,
 )
 from tests.e2e.utils.data_builder.traces import build_trace_default_query
-
-__all__ = [
-    "build_metric_query_data",
-    "build_metric_name",
-    "build_add_metric_input",
-    "build_multiple_add_metric_input",
-    "build_put_metadata_input",
-    "build_add_dimensions_input",
-    "build_put_annotations_input",
-    "build_random_value",
-    "build_service_name",
-    "build_trace_default_query",
-]
