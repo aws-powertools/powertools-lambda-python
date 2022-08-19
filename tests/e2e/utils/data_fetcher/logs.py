d429be61d757efc54960713280b6a58264ad3f1f
heitorlessa <lessa@amazon.co.uk>
2022-08-19 13:35:57 +0200

    feat: accept filter_expression to reduce wanted logs

--- tests/e2e/utils/data_fetcher/logs.py
+++ tests/e2e/utils/data_fetcher/logs.py
@@ -25,12 +25,18 @@ class Log(BaseModel):
 
 @retry(ValueError, delay=2, jitter=1.5, tries=10)
 def get_logs(
-    lambda_function_name: str, start_time: datetime, log_client: Optional[CloudWatchLogsClient] = None
+    lambda_function_name: str,
+    start_time: datetime,
+    log_client: Optional[CloudWatchLogsClient] = None,
+    filter_expression: Optional[str] = None,
 ) -> List[Log]:
     log_client = log_client or boto3.client("logs")
+    filter_expression = filter_expression or "message"  # Logger message key
 
     response = log_client.filter_log_events(
-        logGroupName=f"/aws/lambda/{lambda_function_name}", startTime=int(start_time.timestamp())
+        logGroupName=f"/aws/lambda/{lambda_function_name}",
+        startTime=int(start_time.timestamp()),
+        filterPattern=filter_expression,
     )
 
     if not response["events"]:
