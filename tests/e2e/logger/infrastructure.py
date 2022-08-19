fc5e8a64418801f93aa74b67f8b8a1a6dc826453
heitorlessa <lessa@amazon.co.uk>
2022-08-19 14:05:25 +0200

    refactor: make logger e2e dynamic

--- tests/e2e/logger/infrastructure.py
+++ tests/e2e/logger/infrastructure.py
@@ -4,16 +4,8 @@ from tests.e2e.utils.infrastructure import BaseInfrastructureV2
 
 
 class LoggerStack(BaseInfrastructureV2):
-    LOG_MESSAGE: str = "logger message test"
-    LOG_LEVEL: str = "INFO"
-
     def __init__(self, handlers_dir: Path, feature_name: str = "logger") -> None:
         super().__init__(feature_name, handlers_dir)
 
     def create_resources(self):
-        env_vars = {
-            "MESSAGE": self.LOG_MESSAGE,
-            "LOG_LEVEL": self.LOG_LEVEL,
-            "ADDITIONAL_KEY": "extra_info",
-        }
-        self.create_lambda_functions(function_props={"environment": env_vars})
+        self.create_lambda_functions()
--- tests/e2e/logger/test_logger.py
+++ tests/e2e/logger/test_logger.py
@@ -1,3 +1,6 @@
+import json
+from uuid import uuid4
+
 import pytest
 
 from tests.e2e.utils import data_fetcher
@@ -23,10 +26,14 @@ def test_basic_lambda_logs_visible(basic_handler_fn, basic_handler_fn_arn):
         "function_name",
         "cold_start",
     )
+    message = "logs should be visible with default settings"
+    additional_keys = {"order_id": f"{uuid4()}"}
+    payload = json.dumps({"message": message, "append_keys": additional_keys})
 
     # WHEN
-    _, execution_time = data_fetcher.get_lambda_response(lambda_arn=basic_handler_fn_arn)
+    _, execution_time = data_fetcher.get_lambda_response(lambda_arn=basic_handler_fn_arn, payload=payload)
     filtered_logs = data_fetcher.get_logs(function_name=basic_handler_fn, start_time=execution_time)
 
     # THEN
     assert all(keys in logs.dict(exclude_unset=True) for logs in filtered_logs for keys in required_keys)
+    assert any(getattr(logs, "order_id", False) for logs in filtered_logs)
