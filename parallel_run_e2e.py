060bb6cf1665fa41d2adeda7e9c1be09438256a7
heitorlessa <lessa@amazon.co.uk>
2022-08-24 14:13:24 +0200

    fix(ci): dynamically calculate workers based on infra jobs

---
+++ parallel_run_e2e.py
@@ -0,0 +1,16 @@
+""" Calculate how many parallel workers are needed to complete E2E infrastructure jobs across available CPU Cores """
+import subprocess
+from pathlib import Path
+
+
+def main():
+    features = Path("tests/e2e").rglob("infrastructure.py")
+    workers = len(list(features)) - 1
+
+    command = f"poetry run pytest -n {workers} --dist loadfile -o log_cli=true tests/e2e"
+    print(f"Running E2E tests with: {command}")
+    subprocess.run(command.split(), shell=False)
+
+
+if __name__ == "__main__":
+    main()
