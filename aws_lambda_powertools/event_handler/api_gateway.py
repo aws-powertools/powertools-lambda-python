56b0d2cfae5536557d1ce002365c6566b48d927c
Rúben Fonseca <fonseka@gmail.com>
2022-08-25 12:25:03 +0200

    chore: re-added sorting of CORS headers

--- aws_lambda_powertools/event_handler/api_gateway.py
+++ aws_lambda_powertools/event_handler/api_gateway.py
@@ -610,7 +610,7 @@ class ApiGatewayResolver(BaseRouter):
 
             if method == "OPTIONS":
                 logger.debug("Pre-flight request detected. Returning CORS with null response")
-                headers["Access-Control-Allow-Methods"].append(",".join(self._cors_methods))
+                headers["Access-Control-Allow-Methods"].append(",".join(sorted(self._cors_methods)))
                 return ResponseBuilder(Response(status_code=204, content_type=None, headers=headers, body=""))
 
         handler = self._lookup_exception_handler(NotFoundError)
