75d2ff9c3104ffae43eb9024e4c16b26cf7bbdf1
Leandro Damascena <leandro.damascena@gmail.com>
2022-08-25 09:10:04 +0100

    docs(middleware-factory): snippets split, improved, and lint (#1451)
    Co-authored-by: heitorlessa <lessa@amazon.co.uk>

---
+++ examples/middleware_factory/src/combining_powertools_utilities_schema.py
@@ -0,0 +1,25 @@
+INPUT = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "$id": "https://example.com/object1661012141.json",
+    "title": "Root",
+    "type": "object",
+    "required": ["headers"],
+    "properties": {
+        "headers": {
+            "$id": "#root/headers",
+            "title": "Headers",
+            "type": "object",
+            "required": ["X-Customer-Id"],
+            "properties": {
+                "X-Customer-Id": {
+                    "$id": "#root/headers/X-Customer-Id",
+                    "title": "X-customer-id",
+                    "type": "string",
+                    "default": "",
+                    "examples": ["1"],
+                    "pattern": "^.*$",
+                }
+            },
+        }
+    },
+}
