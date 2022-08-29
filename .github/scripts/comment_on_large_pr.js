5dadc43e49f72993197388ad611a5fa86691ee1a
heitorlessa <lessa@amazon.co.uk>
2022-08-29 17:00:23 +0200

    fix(ci): on_label permissioning model & workflow execution

--- .github/scripts/comment_on_large_pr.js
+++ .github/scripts/comment_on_large_pr.js
@@ -1,14 +1,16 @@
 const {
   PR_NUMBER,
+  PR_ACTION,
   PR_AUTHOR,
   IGNORE_AUTHORS,
 } = require("./constants")
 
-module.exports = async ({github, context, core}) => {
-    if (IGNORE_AUTHORS.includes(PR_AUTHOR)) {
-      return core.notice("Author in IGNORE_AUTHORS list; skipping...")
-    }
 
+const notifyAuthor = async ({
+  gh_client,
+  owner,
+  repository,
+}) => {
     core.info(`Commenting on PR ${PR_NUMBER}`)
 
     let msg = `
@@ -17,10 +19,46 @@ module.exports = async ({github, context, core}) => {
     Please consider breaking into smaller PRs to avoid significant review delays. Ignore if this PR has naturally grown to this size after reviews.
     `;
 
-    await github.rest.issues.createComment({
+    try {
+      await gh_client.rest.issues.createComment({
+        owner: owner,
+        repo: repository,
+        body: msg,
+        issue_number: PR_NUMBER,
+      });
+    } catch (error) {
+      core.setFailed("Failed to notify PR author to split large PR");
+      console.error(err);
+    }
+}
+
+module.exports = async ({github, context, core}) => {
+    if (IGNORE_AUTHORS.includes(PR_AUTHOR)) {
+      return core.notice("Author in IGNORE_AUTHORS list; skipping...")
+    }
+
+    if (PR_ACTION != "labeled") {
+      return core.notice("Only run on PRs labeling actions; skipping")
+    }
+
+
+    /** @type {string[]} */
+    const labels = await github.rest.issues.listLabelsOnIssue({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      body: msg,
       issue_number: PR_NUMBER,
-    });
+    })
+
+    // Schema: https://docs.github.com/en/rest/issues/labels#list-labels-for-an-issue
+    for (const label of labels) {
+      core.info(`Label: ${label}`)
+      if (label.name == "size/XXL") {
+        await notifyAuthor({
+          gh_client: github,
+          owner: context.repo.owner,
+          repository: context.repo.repo,
+        })
+        break;
+      }
+    }
 }
