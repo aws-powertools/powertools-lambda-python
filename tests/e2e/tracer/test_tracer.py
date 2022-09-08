8cfa3b30e989e6b2f5bd8755ac804cb5c009eb3c
Leandro Damascena <leandro.damascena@gmail.com>
2022-09-08 14:32:47 +0100

    feat(tracer): support methods with the same name (ABCs) by including fully qualified name in v2 (#1486)
    Co-authored-by: Heitor Lessa <lessa@amazon.co.uk>
    Co-authored-by: Ruben Fonseca <fonseka@gmail.com>

--- tests/e2e/tracer/test_tracer.py
+++ tests/e2e/tracer/test_tracer.py
@@ -16,6 +16,16 @@ def basic_handler_fn(infrastructure: dict) -> str:
 
 
 @pytest.fixture
+def same_function_name_fn(infrastructure: dict) -> str:
+    return infrastructure.get("SameFunctionName", "")
+
+
+@pytest.fixture
+def same_function_name_arn(infrastructure: dict) -> str:
+    return infrastructure.get("SameFunctionNameArn", "")
+
+
+@pytest.fixture
 def async_fn_arn(infrastructure: dict) -> str:
     return infrastructure.get("AsyncCaptureArn", "")
 
@@ -31,9 +41,9 @@ def test_lambda_handler_trace_is_visible(basic_handler_fn_arn: str, basic_handle
     handler_subsegment = f"## {handler_name}"
     handler_metadata_key = f"{handler_name} response"
 
-    method_name = basic_handler.get_todos.__name__
+    method_name = f"basic_handler.{basic_handler.get_todos.__name__}"
     method_subsegment = f"## {method_name}"
-    handler_metadata_key = f"{method_name} response"
+    method_metadata_key = f"{method_name} response"
 
     trace_query = data_builder.build_trace_default_query(function_name=basic_handler_fn)
 
@@ -46,14 +56,38 @@ def test_lambda_handler_trace_is_visible(basic_handler_fn_arn: str, basic_handle
 
     assert len(trace.get_annotation(key="ColdStart", value=True)) == 1
     assert len(trace.get_metadata(key=handler_metadata_key, namespace=TracerStack.SERVICE_NAME)) == 2
-    assert len(trace.get_metadata(key=handler_metadata_key, namespace=TracerStack.SERVICE_NAME)) == 2
+    assert len(trace.get_metadata(key=method_metadata_key, namespace=TracerStack.SERVICE_NAME)) == 2
     assert len(trace.get_subsegment(name=handler_subsegment)) == 2
     assert len(trace.get_subsegment(name=method_subsegment)) == 2
 
 
+def test_lambda_handler_trace_multiple_functions_same_name(same_function_name_arn: str, same_function_name_fn: str):
+    # GIVEN
+    method_name_todos = "same_function_name.Todos.get_all"
+    method_subsegment_todos = f"## {method_name_todos}"
+    method_metadata_key_todos = f"{method_name_todos} response"
+
+    method_name_comments = "same_function_name.Comments.get_all"
+    method_subsegment_comments = f"## {method_name_comments}"
+    method_metadata_key_comments = f"{method_name_comments} response"
+
+    trace_query = data_builder.build_trace_default_query(function_name=same_function_name_fn)
+
+    # WHEN
+    _, execution_time = data_fetcher.get_lambda_response(lambda_arn=same_function_name_arn)
+
+    # THEN
+    trace = data_fetcher.get_traces(start_date=execution_time, filter_expression=trace_query)
+
+    assert len(trace.get_metadata(key=method_metadata_key_todos, namespace=TracerStack.SERVICE_NAME)) == 1
+    assert len(trace.get_metadata(key=method_metadata_key_comments, namespace=TracerStack.SERVICE_NAME)) == 1
+    assert len(trace.get_subsegment(name=method_subsegment_todos)) == 1
+    assert len(trace.get_subsegment(name=method_subsegment_comments)) == 1
+
+
 def test_async_trace_is_visible(async_fn_arn: str, async_fn: str):
     # GIVEN
-    async_fn_name = async_capture.async_get_users.__name__
+    async_fn_name = f"async_capture.{async_capture.async_get_users.__name__}"
     async_fn_name_subsegment = f"## {async_fn_name}"
     async_fn_name_metadata_key = f"{async_fn_name} response"
 
