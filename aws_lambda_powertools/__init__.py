0777fc96afb65c01422e293a0fffb007a272a141
kt-hr <25603933+kt-hr@users.noreply.github.com>
2022-09-15 21:44:19 +0900

    chore(core): expose modules in the Top-level package (#1517)

--- aws_lambda_powertools/__init__.py
+++ aws_lambda_powertools/__init__.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 
+"""Top-level package for Lambda Python Powertools."""
+
 from pathlib import Path
 
-"""Top-level package for Lambda Python Powertools."""
-from .logging import Logger  # noqa: F401
-from .metrics import Metrics, single_metric  # noqa: F401
+from .logging import Logger
+from .metrics import Metrics, single_metric
 from .package_logger import set_package_logger_handler
-from .tracing import Tracer  # noqa: F401
+from .tracing import Tracer
 
 __author__ = """Amazon Web Services"""
+__all__ = [
+    "Logger",
+    "Metrics",
+    "single_metric",
+    "Tracer",
+]
 
 PACKAGE_PATH = Path(__file__).parent
 
