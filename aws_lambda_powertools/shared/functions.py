364c537e565f15a256612c96939e973c52f54673
heitorlessa <lessa@amazon.co.uk>
2022-10-05 10:45:20 +0200

    feat(logger): set package logger to debug mode when POWERTOOLS_DEV is set

--- aws_lambda_powertools/shared/functions.py
+++ aws_lambda_powertools/shared/functions.py
@@ -91,3 +91,12 @@ def powertools_dev_is_set() -> bool:
         return True
 
     return False
+
+
+def powertools_debug_is_set() -> bool:
+    is_on = strtobool(os.getenv(constants.POWERTOOLS_DEBUG_ENV, "0"))
+    if is_on:
+        warnings.warn("POWERTOOLS_DEBUG environment variable is enabled. Setting logging level to DEBUG")
+        return True
+
+    return False
