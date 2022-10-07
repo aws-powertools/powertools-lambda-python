2e5d0cbf4c12fa143e15a82300181233a7fd099d
Ruben Fonseca <fonseka@gmail.com>
2022-10-07 14:47:34 +0200

    feat(layers): add support for publishing v2 layer (#1558)
    Co-authored-by: Heitor Lessa <lessa@amazon.com>

--- tests/e2e/utils/lambda_layer/powertools_layer.py
+++ tests/e2e/utils/lambda_layer/powertools_layer.py
@@ -2,6 +2,7 @@ import logging
 import subprocess
 from pathlib import Path
 
+from aws_cdk.aws_lambda import Architecture
 from checksumdir import dirhash
 
 from aws_lambda_powertools import PACKAGE_PATH
@@ -14,11 +15,21 @@ logger = logging.getLogger(__name__)
 class LocalLambdaPowertoolsLayer(BaseLocalLambdaLayer):
     IGNORE_EXTENSIONS = ["pyc"]
 
-    def __init__(self, output_dir: Path = CDK_OUT_PATH):
+    def __init__(self, output_dir: Path = CDK_OUT_PATH, architecture: Architecture = Architecture.X86_64):
         super().__init__(output_dir)
-        self.package = f"{SOURCE_CODE_ROOT_PATH}[pydantic]"
-        self.build_args = "--platform manylinux1_x86_64 --only-binary=:all: --upgrade"
+        self.package = f"{SOURCE_CODE_ROOT_PATH}[all]"
+
+        platform_name = self._resolve_platform(architecture)
+        self.build_args = f"--platform {platform_name} --only-binary=:all: --upgrade"
         self.build_command = f"python -m pip install {self.package} {self.build_args} --target {self.target_dir}"
+        self.cleanup_command = (
+            f"rm -rf {self.target_dir}/boto* {self.target_dir}/s3transfer* && "
+            f"rm -rf {self.target_dir}/*dateutil* {self.target_dir}/urllib3* {self.target_dir}/six* && "
+            f"rm -rf {self.target_dir}/jmespath* && "
+            f"find {self.target_dir} -name '*.so' -type f -exec strip '{{}}' \\; && "
+            f"find {self.target_dir} -wholename '*/tests/*' -type f -delete && "
+            f"find {self.target_dir} -regex '^.*\\(__pycache__\\|\\.py[co]\\)$' -delete"
+        )
         self.source_diff_file: Path = CDK_OUT_PATH / "layer_build.diff"
 
     def build(self) -> str:
@@ -30,6 +41,9 @@ class LocalLambdaPowertoolsLayer(BaseLocalLambdaLayer):
         self.after_build()
 
         return str(self.output_dir)
+
+    def after_build(self):
+        subprocess.run(self.cleanup_command, shell=True)
 
     def _has_source_changed(self) -> bool:
         """Hashes source code and
@@ -46,3 +60,18 @@ class LocalLambdaPowertoolsLayer(BaseLocalLambdaLayer):
             return True
 
         return False
+
+    def _resolve_platform(self, architecture: Architecture) -> str:
+        """Returns the correct plaform name for the manylinux project (see PEP 599)
+
+        Returns
+        -------
+        platform_name : str
+            The platform tag
+        """
+        if architecture.name == Architecture.X86_64.name:
+            return "manylinux1_x86_64"
+        elif architecture.name == Architecture.ARM_64.name:
+            return "manylinux2014_aarch64"
+        else:
+            raise ValueError(f"unknown architecture {architecture.name}")
