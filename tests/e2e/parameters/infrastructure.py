92f8fd6e0eb78d4090687ce11b34252e29f0e608
Heitor Lessa <heitor.lessa@hotmail.com>
2022-10-14 18:31:05 +0200

    chore: make resource naming traceable

--- tests/e2e/parameters/infrastructure.py
+++ tests/e2e/parameters/infrastructure.py
@@ -18,7 +18,7 @@ class ParametersStack(BaseInfrastructure):
         service_name = build_service_name()
 
         cfn_application = appconfig.CfnApplication(
-            self.stack, id="appconfig-app", name=f"appe2e{service_name}", description="appe2e"
+            self.stack, id="appconfig-app", name=f"powertools-e2e-{service_name}", description="Lambda Powertools End-to-End testing for AppConfig"
         )
         CfnOutput(self.stack, "AppConfigApplication", value=cfn_application.name)
 
