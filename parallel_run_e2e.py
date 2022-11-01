7a9cfc9e08c579081dfc6bf9ab2d464c144d611d
gokhan721 <33860737+gokhan721@users.noreply.github.com>
2022-11-01 11:07:07 +0300

    Update parallel_run_e2e.py
    Signed-off-by: gokhan721 <33860737+gokhan721@users.noreply.github.com>

--- parallel_run_e2e.py
+++ parallel_run_e2e.py
@@ -7,7 +7,7 @@ def main():
     features = Path("tests/e2e").rglob("infrastructure.py")
     workers = len(list(features)) - 1
 
-    command = f"poetry run pytest -n {workers} --dist loadfile -o log_cli=true tests/e2e"
+    command = f"poetry run pytest --junitxml=./results/result.xml -n {workers} --dist loadfile -o log_cli=true tests/e2e"
     subprocess.run(command.split(), shell=False)
 
 
