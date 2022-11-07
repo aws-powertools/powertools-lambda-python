0778dcb502c31399a0110e7b56918b71e008fa95
Bruno Souza <bruno@komu.com.br>
2022-11-07 17:10:38 -0300

    docs: fix example for tests in api_gateway
    Signed-off-by: Bruno Souza <bruno@komu.com.br>

--- examples/event_handler_rest/src/assert_http_response.py
+++ examples/event_handler_rest/src/assert_http_response.py
@@ -18,9 +18,13 @@ def lambda_context():
 
 def test_lambda_handler(lambda_context):
     minimal_event = {
-        "path": "/todos",
-        "httpMethod": "GET",
-        "requestContext": {"requestId": "227b78aa-779d-47d4-a48e-ce62120393b8"},  # correlation ID
+        "rawPath": "/todos",
+        "requestContext": {
+            "http": {
+                "method": "GET",
+            },
+            "stage": "$default",
+        },
     }
 
     ret = assert_http_response_module.lambda_handler(minimal_event, lambda_context)
