31d265cc511cfcde5f269fa1fd3ed2380948b135
Bruno Souza <bruno@komu.com.br>
2022-11-08 17:22:32 -0300

    Adding AWS Docs URL for ALB
    Signed-off-by: Bruno Souza <bruno@komu.com.br>

--- examples/event_handler_rest/src/assert_alb_api_resolver_response.py
+++ examples/event_handler_rest/src/assert_alb_api_resolver_response.py
@@ -22,6 +22,8 @@ def test_lambda_handler(lambda_context):
         "httpMethod": "GET",
         "headers": {"x-amzn-trace-id": "b25827e5-0e30-4d52-85a8-4df449ee4c5a"},
     }
+    # Example of Application Load Balancer request event:
+    # https://docs.aws.amazon.com/lambda/latest/dg/services-alb.html
 
     ret = assert_alb_api_response_module.lambda_handler(minimal_event, lambda_context)
     assert ret["statusCode"] == 200
