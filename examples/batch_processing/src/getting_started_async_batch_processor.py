4d6c0f3de8b383fac732e2a1fde44e1ec4fbeb72
h4x0r <h4x0r>
2022-11-15 22:32:59 -0500

    docs(examples): add async and batch processor examples

---
+++ examples/batch_processing/src/getting_started_async_batch_processor.py
@@ -0,0 +1,23 @@
+from aws_lambda_powertools.asynchrony import async_lambda_handler
+from aws_lambda_powertools.utilities.batch import async_batch_processor, EventType, AsyncBatchProcessor
+from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSRecord
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+
+async def async_record_handler(record: SQSRecord):
+    """
+    Process here each record
+    """
+    payload: str = record.body
+    if not payload:
+        raise ValueError
+    # code code code
+
+
+processor = AsyncBatchProcessor(event_type=EventType.SQS)
+
+
+@async_lambda_handler
+@async_batch_processor(record_handler=async_record_handler, processor=processor)
+async def lambda_handler(event, context: LambdaContext):
+    return processor.response()
