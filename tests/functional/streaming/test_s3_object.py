639f38e55ef853882fb039cb8904b3b6b2187aea
Rúben Fonseca <fonseka@gmail.com>
2022-11-23 17:42:52 +0100

    fix: remove test

--- tests/functional/streaming/test_s3_object.py
+++ tests/functional/streaming/test_s3_object.py
@@ -1,11 +1,5 @@
-import io
 from csv import DictReader
 from gzip import GzipFile
-
-import boto3
-import pytest
-from botocore import stub
-from botocore.response import StreamingBody
 
 from aws_lambda_powertools.utilities.streaming import S3Object
 from aws_lambda_powertools.utilities.streaming._s3_seekable_io import _S3SeekableIO
@@ -44,24 +38,3 @@ def test_s3_transform_in_place():
 
     new_obj = obj.transform(GzipTransform(), in_place=True)
     assert new_obj is None
-
-
-def test_s3_transform_after_read():
-    # GIVEN a S3 Object with a "hello world" payload
-    payload = b"hello world"
-
-    s3_client = boto3.client("s3")
-    s3_stub = stub.Stubber(s3_client)
-    s3_stub.add_response(
-        "get_object", {"Body": StreamingBody(raw_stream=io.BytesIO(payload), content_length=len(payload))}
-    )
-    s3_stub.activate()
-    obj = S3Object(bucket="bucket", key="key", boto3_client=s3_client)
-
-    # WHEN you read some part of the object and then apply a transformation
-    assert obj.read(5) == b"hello"
-
-    # THEN it raises ValueError
-    with pytest.raises(ValueError) as exc:
-        obj.transform(GzipTransform())
-    assert str(exc.value) == "Cannot add transformations to a read object. Already read 5 bytes"
