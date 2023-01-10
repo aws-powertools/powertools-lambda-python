a2c071a2a67729c07f69019b536b69e5b1a60ffa
Rúben Fonseca <fonseka@gmail.com>
2023-01-10 12:06:18 +0100

    chore: export top level constants

--- aws_lambda_powertools/event_handler/__init__.py
+++ aws_lambda_powertools/event_handler/__init__.py
@@ -12,14 +12,26 @@ from .api_gateway import (
 )
 from .appsync import AppSyncResolver
 from .lambda_function_url import LambdaFunctionUrlResolver
+from .router import (
+    ALBRouter,
+    APIGatewayHttpRouter,
+    APIGatewayRouter,
+    LambdaFunctionUrlRouter,
+    Router,
+)
 
 __all__ = [
     "AppSyncResolver",
     "APIGatewayRestResolver",
+    "APIGatewayRouter",
     "APIGatewayHttpResolver",
+    "APIGatewayHttpRouter",
     "ALBResolver",
+    "ALBRouter",
     "ApiGatewayResolver",
     "CORSConfig",
     "LambdaFunctionUrlResolver",
+    "LambdaFunctionUrlRouter",
     "Response",
+    "Router",
 ]
