2bfc558f0646cf4d136d7df77755449d66dc3d9f
Rúben Fonseca <fonseka@gmail.com>
2023-01-27 19:32:11 +0100

    fix: simplified parallel lambdas

--- tests/e2e/utils/functions.py
+++ tests/e2e/utils/functions.py
@@ -6,18 +6,23 @@ from tests.e2e.utils import data_fetcher  # noqa F401
 
 
 def execute_lambdas_in_parallel(function_name: str, lambdas_arn: list, arguments: str):
-    def f(fname: str, farn: str, fargs: str):
-        return eval(fname)(farn, fargs)
-
     result_list = []
     with ThreadPoolExecutor() as executor:
         running_tasks: List[Future] = []
         for arn in lambdas_arn:
             # Sleep 0.5, 1, 1.5, ... seconds between each invocation. This way
             # we can guarantee that lambdas are executed in parallel, but they are
-            # called in the same "order" as they are passed in.
+            # called in the same "order" as they are passed in, thus guaranteeing that
+            # we can assert on the correct output.
             time.sleep(0.5 * len(running_tasks))
-            running_tasks.append(executor.submit(f, function_name, arn, arguments))
+            running_tasks.append(
+                executor.submit(
+                    lambda lname, larn, largs: eval(lname)(larn, largs),
+                    function_name,
+                    arn,
+                    arguments,
+                )
+            )
 
         executor.shutdown(wait=True)
 
