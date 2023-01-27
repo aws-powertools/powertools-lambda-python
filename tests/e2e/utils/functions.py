c70425ef256bc77119c2950c76f275fd5530672c
Rúben Fonseca <fonseka@gmail.com>
2023-01-27 11:30:35 +0100

    fix: parallel lambdas

--- tests/e2e/utils/functions.py
+++ tests/e2e/utils/functions.py
@@ -1,14 +1,24 @@
-from concurrent.futures import ThreadPoolExecutor
+import time
+from concurrent.futures import Future, ThreadPoolExecutor
+from typing import List
 
 from tests.e2e.utils import data_fetcher  # noqa F401
 
 
 def execute_lambdas_in_parallel(function_name: str, lambdas_arn: list, arguments: str):
+    def f(function_name, arn, arguments):
+        eval(function_name)(arn, arguments)
+
     result_list = []
     with ThreadPoolExecutor() as executor:
-        running_tasks = executor.map(lambda exec: eval(function_name)(*exec), [(arn, arguments) for arn in lambdas_arn])
+        running_tasks: List[Future] = []
+        for arn in lambdas_arn:
+            time.sleep(0.5 * len(running_tasks))
+            running_tasks.append(executor.submit(f, function_name, arn, arguments))
+
         executor.shutdown(wait=True)
+
         for running_task in running_tasks:
-            result_list.append(running_task)
+            result_list.append(running_task.result())
 
     return result_list
