b7e329496bd787566c7955ad5037806283cbe7c7
Vladimir Prudnikov <v.prudnikov@gmail.com>
2023-02-01 01:45:34 +0000

    feat(metrics): default dimensions in single_metric

---
+++ examples/metrics/src/single_metric_default_dimensions_inherit.py
@@ -0,0 +1,17 @@
+import os
+
+from aws_lambda_powertools import single_metric
+from aws_lambda_powertools.metrics import Metrics, MetricUnit
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+STAGE = os.getenv("STAGE", "dev")
+
+metrics = Metrics()
+metrics.set_default_dimensions(environment=STAGE)
+
+
+def lambda_handler(event: dict, context: LambdaContext):
+    with single_metric(
+        name="RecordsCount", unit=MetricUnit.Count, value=10, default_dimensions=metrics.default_dimensions
+    ) as metric:
+        metric.add_dimension(name="TableName", value="Users")
