4e28bb24cd24ef3b762a57f977e0f15e98ec87f3
heitorlessa <lessa@amazon.co.uk>
2023-02-07 15:38:44 +0100

    docs: remove sync for now, revamp async example

--- examples/batch_processing/src/getting_started_async_batch_processor.py
+++ examples/batch_processing/src/getting_started_async_batch_processor.py
@@ -1,21 +1,25 @@
-from aws_lambda_powertools.utilities.batch import async_batch_processor, EventType, AsyncBatchProcessor
+import httpx  # external dependency
+
+from aws_lambda_powertools.utilities.batch import (
+    AsyncBatchProcessor,
+    EventType,
+    async_batch_processor,
+)
 from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSRecord
 from aws_lambda_powertools.utilities.typing import LambdaContext
-
-
-async def async_record_handler(record: SQSRecord):
-    """
-    Process here each record
-    """
-    payload: str = record.body
-    if not payload:
-        raise ValueError
-    # code code code
-
 
 processor = AsyncBatchProcessor(event_type=EventType.SQS)
 
 
+async def async_record_handler(record: SQSRecord):
+    # Yield control back to the event loop to schedule other tasks
+    # while you await from a response from httpbin.org
+    async with httpx.AsyncClient() as client:
+        ret = await client.get("https://httpbin.org/get")
+
+    return ret.status_code
+
+
 @async_batch_processor(record_handler=async_record_handler, processor=processor)
-async def lambda_handler(event, context: LambdaContext):
+def lambda_handler(event, context: LambdaContext):
     return processor.response()
