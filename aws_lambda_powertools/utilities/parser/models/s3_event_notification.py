70e7b1c34b1c9d0f55b39f30f5ac5aa1b7f3b224
Alan Ip <theipster@users.noreply.github.com>
2023-04-14 03:04:52 +0100

    Fix formatting

--- aws_lambda_powertools/utilities/parser/models/s3_event_notification.py
+++ aws_lambda_powertools/utilities/parser/models/s3_event_notification.py
@@ -4,9 +4,11 @@ from pydantic import Json
 
 from aws_lambda_powertools.utilities.parser.models.s3 import S3Model
 from aws_lambda_powertools.utilities.parser.models.sqs import SqsRecordModel, SqsModel
-  
+
+
 class SqsS3EventNotificationRecordModel(SqsRecordModel):
-  body: Json[S3Model]
+    body: Json[S3Model]
+
 
 class SqsS3EventNotificationModel(SqsModel):
-  Records: List[SqsS3EventNotificationRecordModel]
+    Records: List[SqsS3EventNotificationRecordModel]
