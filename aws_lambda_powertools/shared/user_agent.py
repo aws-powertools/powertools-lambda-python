5c7a5a29f2f29f56647a8ca4471da2ab13fd3bc4
Leandro Damascena <leandro.damascena@gmail.com>
2023-05-17 23:12:18 +0100

    user-agent: fixing small things

--- aws_lambda_powertools/shared/user_agent.py
+++ aws_lambda_powertools/shared/user_agent.py
@@ -1,9 +1,15 @@
 import logging
 import os
+from sys import version_info
 
-from importlib_metadata import version
+# Since Python 3.8 there is a built-in. Remove this when support for Python3.7 is dropped
+# See https://docs.python.org/3/library/importlib.metadata.html
+if version_info.major == 3 and version_info.minor == 7:
+    from importlib_metadata import version
+else:
+    from importlib.metadata import version
 
-pt_version = version("aws-lambda-powertools")
+powertools_version = version("aws-lambda-powertools")
 
 try:
     from botocore import handlers
@@ -13,52 +19,54 @@ except ImportError:
 
 logger = logging.getLogger(__name__)
 
-ENV_KEY = "AWS_EXECUTION_ENV"
-EXEC_ENV = os.environ.get(ENV_KEY, "NA")
-target_sdk_event = "request-created"
+EXEC_ENV = os.environ.get("AWS_EXECUTION_ENV", "NA")
+TARGET_SDK_EVENT = "request-created"
+FEATURE_PREFIX = "PT"
 
 
-# add user-agent like powertools/2.14.1 EXEC-ENV/AWS_Lambda_python3.9
-def _add_pt_version(request, **kwargs):
-    headers = request.headers
-    # assume we always have a User-Agent.
-    # Q: do we need to check if field: User-Agent exist in request.headers?
-    # Q: do we care if User-Agent string exceed 256 characters?
-    headers["User-Agent"] = f"{headers['User-Agent']} PT/no-op/{pt_version} PTEnv/{EXEC_ENV}"
+# In case of no Powertools utility: PT/no-op/2.15.0 PTEnv/AWS_Lambda_python3.9
+def _add_powertools_version(request, **kwargs):
+    try:
+        headers = request.headers
+        headers["User-Agent"] = f"{headers['User-Agent']} {FEATURE_PREFIX}/no-op/{powertools_version} PTEnv/{EXEC_ENV}"
+    except Exception:
+        logger.debug("Missing header User-Agent")
 
 
 # creates the `add_feature_string` function with given feature parameter
 def _create_feature_function(feature):
-    def add_pt_feature(request, **kwargs):
+    def add_powertools_feature(request, **kwargs):
         headers = request.headers
         # Actually, only one handler can be registered, registering a new one will replace the prev handler
         # We don't need to replace/detect previous registered user-agent
-        headers["User-Agent"] = f"{headers['User-Agent']} PT/{feature}/{pt_version} PTEnv/{EXEC_ENV}"
+        headers[
+            "User-Agent"
+        ] = f"{headers['User-Agent']} {FEATURE_PREFIX}/{feature}/{powertools_version} PTEnv/{EXEC_ENV}"
 
     # return created function
-    return add_pt_feature
+    return add_powertools_feature
 
 
-# add feature user-agent to given sdk session
+# add feature user-agent to given sdk boto3.session
 def register_feature_to_session(session, feature):
     try:
-        session.events.register(target_sdk_event, _create_feature_function(feature))
+        session.events.register(TARGET_SDK_EVENT, _create_feature_function(feature))
     except AttributeError as e:
         logger.debug(f"session passed in doesn't have a event system:{e}")
 
 
-# add feature user-agent to given sdk client
+# add feature user-agent to given sdk boto3.client
 def register_feature_to_client(client, feature):
     try:
-        client.meta.events.register(target_sdk_event, _create_feature_function(feature))
+        client.meta.events.register(TARGET_SDK_EVENT, _create_feature_function(feature))
     except AttributeError as e:
         logger.debug(f"session passed in doesn't have a event system:{e}")
 
 
-# add feature user-agent to given sdk session.resource
+# add feature user-agent to given sdk boto3.resource
 def register_feature_to_resource(resource, feature):
     try:
-        resource.meta.client.meta.events.register(target_sdk_event, _create_feature_function(feature))
+        resource.meta.client.meta.events.register(TARGET_SDK_EVENT, _create_feature_function(feature))
     except AttributeError as e:
         logger.debug(f"resource passed in doesn't have a event system:{e}")
 
@@ -67,4 +75,4 @@ def register_feature_to_resource(resource, feature):
 def inject_user_agent():
     if handlers:
         # register add_user_agent to BUILTIN_HANDLERS so every aws sdk session will have this event registered
-        handlers.BUILTIN_HANDLERS.append((target_sdk_event, _add_pt_version))
+        handlers.BUILTIN_HANDLERS.append((TARGET_SDK_EVENT, _add_powertools_version))
