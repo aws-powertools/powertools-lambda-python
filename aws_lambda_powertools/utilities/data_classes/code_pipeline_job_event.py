719cdc0e3e9cf09da7592bc6a393554c1acd6349
Darnley Costa <darnley@outlook.com>
2023-06-01 07:20:52 -0300

    fix(event_source): change the import location of boto3 in CodePipelineJobEvent data class (#2353)
    Co-authored-by: Leandro Damascena <leandro.damascena@gmail.com>

--- aws_lambda_powertools/utilities/data_classes/code_pipeline_job_event.py
+++ aws_lambda_powertools/utilities/data_classes/code_pipeline_job_event.py
@@ -4,9 +4,6 @@ import zipfile
 from typing import Any, Dict, List, Optional
 from urllib.parse import unquote_plus
 
-import boto3
-
-from aws_lambda_powertools.shared import user_agent
 from aws_lambda_powertools.utilities.data_classes.common import DictWrapper
 
 
@@ -204,6 +201,12 @@ class CodePipelineJobEvent(DictWrapper):
         BaseClient
             An S3 client with the appropriate credentials
         """
+        # IMPORTING boto3 within the FUNCTION and not at the top level to get
+        # it only when we explicitly want it for better performance.
+        import boto3
+
+        from aws_lambda_powertools.shared import user_agent
+
         s3 = boto3.client(
             "s3",
             aws_access_key_id=self.data.artifact_credentials.access_key_id,
