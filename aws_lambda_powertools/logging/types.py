652ee8ab8e4aadc2e5d08b4f192094270adbb6ad
erikayao93 <erikayao@amazon.com>
2023-06-07 23:35:59 +0000

    Added additional typing options for TypedLog keys, updated bring_your_own_formatter example to utilize new class

--- aws_lambda_powertools/logging/types.py
+++ aws_lambda_powertools/logging/types.py
@@ -1,9 +1,10 @@
+from typing import Any, Dict, Iterable, Union
+
 from typing_extensions import TypedDict
 
-
 class TypedLog(TypedDict, total=False):
-    level: str
+    level: Union[str, int]
     location: str
-    timestamp: str
+    timestamp: Union[str, int]
     service: str
-    event: str
+    message: Union[Dict[str, Any], str, bool, Iterable]
--- examples/logger/src/bring_your_own_formatter.py
+++ examples/logger/src/bring_your_own_formatter.py
@@ -1,12 +1,13 @@
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.logging.formatter import LambdaPowertoolsFormatter
+from aws_lambda_powertools.logging.types import TypedLog
 
 
 class CustomFormatter(LambdaPowertoolsFormatter):
-    def serialize(self, log: dict) -> str:
+    def serialize(self, log: TypedLog) -> str: # type: ignore
         """Serialize final structured log dict to JSON str"""
-        log["event"] = log.pop("message")  # rename message key to event
-        return self.json_serializer(log)  # use configured json serializer
+        log["event"] = log.pop("message")  # type: ignore
+        return self.json_serializer(log)  # type: ignore
 
 
 logger = Logger(service="payment", logger_formatter=CustomFormatter())
