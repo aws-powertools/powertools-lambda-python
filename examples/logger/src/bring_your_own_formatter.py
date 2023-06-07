9289c4b05e58dd6feb315fb5683a071cd889407f
erikayao93 <erikayao@amazon.com>
2023-06-07 23:40:19 +0000

    Added missing space to satisfy linter

--- examples/logger/src/bring_your_own_formatter.py
+++ examples/logger/src/bring_your_own_formatter.py
@@ -4,7 +4,7 @@ from aws_lambda_powertools.logging.types import TypedLog
 
 
 class CustomFormatter(LambdaPowertoolsFormatter):
-    def serialize(self, log: TypedLog) -> str: # type: ignore
+    def serialize(self, log: TypedLog) -> str:  # type: ignore
         """Serialize final structured log dict to JSON str"""
         log["event"] = log.pop("message")  # type: ignore
         return self.json_serializer(log)  # type: ignore
