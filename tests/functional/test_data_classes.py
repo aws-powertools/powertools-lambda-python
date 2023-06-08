e416e1e9651ab0e6529d00f8a5c03e076c9d7603
Stephen Bawks <5246651+stephenbawks@users.noreply.github.com>
2023-06-08 08:50:16 -0400

    feat(event_sources): add support for VPC Lattice events (#2358)
    Co-authored-by: Leandro Damascena <leandro.damascena@gmail.com>
    Co-authored-by: Heitor Lessa <lessa@amazon.co.uk>

--- tests/functional/test_data_classes.py
+++ tests/functional/test_data_classes.py
@@ -25,6 +25,7 @@ from aws_lambda_powertools.utilities.data_classes import (
     SESEvent,
     SNSEvent,
     SQSEvent,
+    VPCLatticeEvent,
 )
 from aws_lambda_powertools.utilities.data_classes.api_gateway_authorizer_event import (
     APIGatewayAuthorizerEventV2,
@@ -2041,3 +2042,18 @@ def test_api_gateway_route_arn_parser():
     details = parse_api_gateway_arn(arn)
     assert details.resource == ""
     assert details.arn == arn + "/"
+
+
+def test_vpc_lattice_event():
+    event = VPCLatticeEvent(load_event("vpcLatticeEvent.json"))
+
+    assert event.raw_path == event["raw_path"]
+    assert event.get_query_string_value("order-id") == "1"
+    assert event.get_header_value("user_agent") == "curl/7.64.1"
+    assert event.decoded_body == '{"test": "event"}'
+    assert event.json_body == {"test": "event"}
+    assert event.method == event["method"]
+    assert event.headers == event["headers"]
+    assert event.query_string_parameters == event["query_string_parameters"]
+    assert event.body == event["body"]
+    assert event.is_base64_encoded == event["is_base64_encoded"]
