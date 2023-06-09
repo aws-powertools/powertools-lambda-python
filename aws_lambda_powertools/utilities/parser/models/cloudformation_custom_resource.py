926be8fbdcc672aebaecd0568f9b47032930fac1
heitorlessa <lessa@amazon.co.uk>
2023-06-09 17:35:40 +0200

    fix: resource_properties type to allow override

--- aws_lambda_powertools/utilities/parser/models/cloudformation_custom_resource.py
+++ aws_lambda_powertools/utilities/parser/models/cloudformation_custom_resource.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Type, Union
+from typing import Any, Dict, Union
 
 from pydantic import BaseModel, Field, HttpUrl
 
@@ -13,7 +13,7 @@ class CloudFormationCustomResourceBaseModel(BaseModel):
     request_id: str = Field(..., alias="RequestId")
     logical_resource_id: str = Field(..., alias="LogicalResourceId")
     resource_type: str = Field(..., alias="ResourceType")
-    resource_properties: Union[Dict[str, Any], Type[BaseModel], None] = Field(None, alias="ResourceProperties")
+    resource_properties: Union[Dict[str, Any], BaseModel, None] = Field(None, alias="ResourceProperties")
 
 
 class CloudFormationCustomResourceCreateModel(CloudFormationCustomResourceBaseModel):
@@ -26,4 +26,4 @@ class CloudFormationCustomResourceDeleteModel(CloudFormationCustomResourceBaseMo
 
 class CloudFormationCustomResourceUpdateModel(CloudFormationCustomResourceBaseModel):
     request_type: Literal["Update"] = Field(..., alias="RequestType")
-    old_resource_properties: Union[Dict[str, Any], Type[BaseModel], None] = Field(None, alias="OldResourceProperties")
+    old_resource_properties: Union[Dict[str, Any], BaseModel, None] = Field(None, alias="OldResourceProperties")
