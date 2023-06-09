caf218a63051dda9e0f108b206941958336ab1e7
heitorlessa <lessa@amazon.co.uk>
2023-06-09 17:31:00 +0200

    chore: refactor create test as unit test

--- tests/unit/parser/test_cloudformation_custom_resource.py
+++ tests/unit/parser/test_cloudformation_custom_resource.py
@@ -11,24 +11,6 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 from tests.functional.utils import load_event
 
 
-@event_parser(model=CloudFormationCustomResourceCreateModel)
-def handle_create_custom_resource(event: CloudFormationCustomResourceCreateModel, _: LambdaContext):
-    assert event.request_type == "Create"
-    assert event.request_id == "xxxxx-d2a0-4dfb-ab1f-xxxxxx"
-    assert event.service_token == "arn:aws:lambda:us-east-1:xxx:function:xxxx-CrbuiltinfunctionidProvi-2vKAalSppmKe"
-    assert (
-        str(event.response_url)
-        == "https://cloudformation-custom-resource-response-useast1.s3.amazonaws.com/7F%7Cb1f50fdfc25f3b"
-    )
-    assert event.stack_id == "arn:aws:cloudformation:us-east-1:xxxx:stack/xxxx/271845b0-f2e8-11ed-90ac-0eeb25b8ae21"
-    assert event.logical_resource_id == "xxxxxxxxx"
-    assert event.resource_type == "Custom::MyType"
-    assert event.resource_properties == {
-        "ServiceToken": "arn:aws:lambda:us-east-1:xxxxx:function:xxxxx",
-        "MyProps": "ss",
-    }
-
-
 @event_parser(model=CloudFormationCustomResourceUpdateModel)
 def handle_update_custom_resource(event: CloudFormationCustomResourceUpdateModel, _: LambdaContext):
     assert event.request_type == "Update"
@@ -69,11 +51,6 @@ def handle_delete_custom_resource(event: CloudFormationCustomResourceDeleteModel
     }
 
 
-def test_create_trigger_event():
-    event_dict = load_event("cloudformationCustomResourceCreate.json")
-    handle_create_custom_resource(event_dict, LambdaContext())
-
-
 def test_update_trigger_event():
     event_dict = load_event("cloudformationCustomResourceUpdate.json")
     handle_update_custom_resource(event_dict, LambdaContext())
@@ -82,12 +59,6 @@ def test_update_trigger_event():
 def test_delete_trigger_event():
     event_dict = load_event("cloudformationCustomResourceDelete.json")
     handle_delete_custom_resource(event_dict, LambdaContext())
-
-
-def test_validate_create_event_does_not_conform_with_model():
-    event = {"invalid": "event"}
-    with pytest.raises(ValidationError):
-        handle_create_custom_resource(event, LambdaContext())
 
 
 def test_validate_update_event_does_not_conform_with_model():
@@ -106,25 +77,40 @@ class MyModel(BaseModel):
     MyProps: str
 
 
-class MyCustomResource(CloudFormationCustomResourceCreateModel):
-    resource_properties: MyModel = Field(..., alias="ResourceProperties")
+def test_cloudformation_custom_resource_create_event_custom_model():
+    class MyCustomResource(CloudFormationCustomResourceCreateModel):
+        resource_properties: MyModel = Field(..., alias="ResourceProperties")
+
+    raw_event = load_event("cloudformationCustomResourceCreate.json")
+    model = MyCustomResource(**raw_event)
+
+    assert model.request_type == raw_event["RequestType"]
+    assert model.request_id == raw_event["RequestId"]
+    assert model.service_token == raw_event["ServiceToken"]
+    assert str(model.response_url) == raw_event["ResponseURL"]
+    assert model.stack_id == raw_event["StackId"]
+    assert model.logical_resource_id == raw_event["LogicalResourceId"]
+    assert model.resource_type == raw_event["ResourceType"]
+    assert model.resource_properties.MyProps == raw_event["ResourceProperties"].get("MyProps")
 
 
-@event_parser(model=MyCustomResource)
-def handle_create_custom_resource_extended_model(event: MyCustomResource, _: LambdaContext):
-    assert event.request_type == "Create"
-    assert event.request_id == "xxxxx-d2a0-4dfb-ab1f-xxxxxx"
-    assert event.service_token == "arn:aws:lambda:us-east-1:xxx:function:xxxx-CrbuiltinfunctionidProvi-2vKAalSppmKe"
-    assert (
-        str(event.response_url)
-        == "https://cloudformation-custom-resource-response-useast1.s3.amazonaws.com/7F%7Cb1f50fdfc25f3b"
-    )
-    assert event.stack_id == "arn:aws:cloudformation:us-east-1:xxxx:stack/xxxx/271845b0-f2e8-11ed-90ac-0eeb25b8ae21"
-    assert event.logical_resource_id == "xxxxxxxxx"
-    assert event.resource_type == "Custom::MyType"
-    assert event.resource_properties.MyProps == "ss"
+def test_cloudformation_custom_resource_create_event():
+    raw_event = load_event("cloudformationCustomResourceCreate.json")
+    model = CloudFormationCustomResourceCreateModel(**raw_event)
+
+    assert model.request_type == raw_event["RequestType"]
+    assert model.request_id == raw_event["RequestId"]
+    assert model.service_token == raw_event["ServiceToken"]
+    assert str(model.response_url) == raw_event["ResponseURL"]
+    assert model.stack_id == raw_event["StackId"]
+    assert model.logical_resource_id == raw_event["LogicalResourceId"]
+    assert model.resource_type == raw_event["ResourceType"]
+    assert model.resource_properties == raw_event["ResourceProperties"]
 
 
-def test_create_trigger_event_custom_model():
-    event_dict = load_event("cloudformationCustomResourceCreate.json")
-    handle_create_custom_resource_extended_model(event_dict, LambdaContext())
+def test_cloudformation_custom_resource_create_event_invalid():
+    raw_event = load_event("cloudformationCustomResourceCreate.json")
+    raw_event["ResourceProperties"] = ["some_data"]
+
+    with pytest.raises(ValidationError):
+        CloudFormationCustomResourceCreateModel(**raw_event)
