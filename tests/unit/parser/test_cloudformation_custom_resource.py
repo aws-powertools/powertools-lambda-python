6be00f2ab6043f573451bfd70d5035ae0468bfe0
heitorlessa <lessa@amazon.co.uk>
2023-06-09 17:48:41 +0200

    chore: cleanup any leftovers, remove duplicate logic

--- tests/unit/parser/test_cloudformation_custom_resource.py
+++ tests/unit/parser/test_cloudformation_custom_resource.py
@@ -10,29 +10,6 @@ from aws_lambda_powertools.utilities.parser.models import (
 from tests.functional.utils import load_event
 
 
-# NOTE: Leftover, move to create model test
-class MyModel(BaseModel):
-    MyProps: str
-
-
-# NOTE: Leftover, update to test only custom model per se
-def test_cloudformation_custom_resource_create_event_custom_model():
-    class MyCustomResource(CloudFormationCustomResourceCreateModel):
-        resource_properties: MyModel = Field(..., alias="ResourceProperties")
-
-    raw_event = load_event("cloudformationCustomResourceCreate.json")
-    model = MyCustomResource(**raw_event)
-
-    assert model.request_type == raw_event["RequestType"]
-    assert model.request_id == raw_event["RequestId"]
-    assert model.service_token == raw_event["ServiceToken"]
-    assert str(model.response_url) == raw_event["ResponseURL"]
-    assert model.stack_id == raw_event["StackId"]
-    assert model.logical_resource_id == raw_event["LogicalResourceId"]
-    assert model.resource_type == raw_event["ResourceType"]
-    assert model.resource_properties.MyProps == raw_event["ResourceProperties"].get("MyProps")
-
-
 def test_cloudformation_custom_resource_create_event():
     raw_event = load_event("cloudformationCustomResourceCreate.json")
     model = CloudFormationCustomResourceCreateModel(**raw_event)
@@ -47,33 +24,25 @@ def test_cloudformation_custom_resource_create_event():
     assert model.resource_properties == raw_event["ResourceProperties"]
 
 
+def test_cloudformation_custom_resource_create_event_custom_model():
+    class MyModel(BaseModel):
+        MyProps: str
+
+    class MyCustomResource(CloudFormationCustomResourceCreateModel):
+        resource_properties: MyModel = Field(..., alias="ResourceProperties")
+
+    raw_event = load_event("cloudformationCustomResourceCreate.json")
+    model = MyCustomResource(**raw_event)
+
+    assert model.resource_properties.MyProps == raw_event["ResourceProperties"].get("MyProps")
+
+
 def test_cloudformation_custom_resource_create_event_invalid():
     raw_event = load_event("cloudformationCustomResourceCreate.json")
     raw_event["ResourceProperties"] = ["some_data"]
 
     with pytest.raises(ValidationError):
         CloudFormationCustomResourceCreateModel(**raw_event)
-
-
-def test_cloudformation_custom_resource_delete_event():
-    raw_event = load_event("cloudformationCustomResourceDelete.json")
-    model = CloudFormationCustomResourceDeleteModel(**raw_event)
-
-    assert model.request_type == raw_event["RequestType"]
-    assert model.request_id == raw_event["RequestId"]
-    assert model.service_token == raw_event["ServiceToken"]
-    assert str(model.response_url) == raw_event["ResponseURL"]
-    assert model.stack_id == raw_event["StackId"]
-    assert model.logical_resource_id == raw_event["LogicalResourceId"]
-    assert model.resource_type == raw_event["ResourceType"]
-    assert model.resource_properties == raw_event["ResourceProperties"]
-
-
-def test_cloudformation_custom_resource_delete_event_invalid():
-    raw_event = load_event("cloudformationCustomResourceDelete.json")
-    raw_event["ResourceProperties"] = ["some_data"]
-    with pytest.raises(ValidationError):
-        CloudFormationCustomResourceDeleteModel(**raw_event)
 
 
 def test_cloudformation_custom_resource_update_event():
@@ -97,3 +66,24 @@ def test_cloudformation_custom_resource_update_event_invalid():
 
     with pytest.raises(ValidationError):
         CloudFormationCustomResourceUpdateModel(**raw_event)
+
+
+def test_cloudformation_custom_resource_delete_event():
+    raw_event = load_event("cloudformationCustomResourceDelete.json")
+    model = CloudFormationCustomResourceDeleteModel(**raw_event)
+
+    assert model.request_type == raw_event["RequestType"]
+    assert model.request_id == raw_event["RequestId"]
+    assert model.service_token == raw_event["ServiceToken"]
+    assert str(model.response_url) == raw_event["ResponseURL"]
+    assert model.stack_id == raw_event["StackId"]
+    assert model.logical_resource_id == raw_event["LogicalResourceId"]
+    assert model.resource_type == raw_event["ResourceType"]
+    assert model.resource_properties == raw_event["ResourceProperties"]
+
+
+def test_cloudformation_custom_resource_delete_event_invalid():
+    raw_event = load_event("cloudformationCustomResourceDelete.json")
+    raw_event["ResourceProperties"] = ["some_data"]
+    with pytest.raises(ValidationError):
+        CloudFormationCustomResourceDeleteModel(**raw_event)
