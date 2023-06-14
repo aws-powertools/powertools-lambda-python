77c08c49c1f2b6bcbf6499998ef6a130d96a4e25
heitorlessa <lessa@amazon.co.uk>
2023-06-14 12:33:44 +0200

    fix: ignore typeddict specific checks and explain why

--- examples/logger/src/bring_your_own_formatter.py
+++ examples/logger/src/bring_your_own_formatter.py
@@ -1,17 +1,16 @@
 from __future__ import annotations
 
-from typing import Mapping
-
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.logging.formatter import LambdaPowertoolsFormatter
-from aws_lambda_powertools.logging.types import PowertoolsLogRecord
+from aws_lambda_powertools.logging.types import LogRecord
 
 
 class CustomFormatter(LambdaPowertoolsFormatter):
-    def serialize(self, log: PowertoolsLogRecord | Mapping) -> str:  
+    def serialize(self, log: LogRecord) -> str:
         """Serialize final structured log dict to JSON str"""
-        log["event"] = log.pop("message")  # type: ignore
-        return self.json_serializer(log)  
+        # in this example, log["message"] is a required field but we want to remap and delete, hence mypy ignore checks
+        log["event"] = log.pop("message")  # type: ignore[typeddict-unknown-key,misc]
+        return self.json_serializer(log)
 
 
 logger = Logger(service="payment", logger_formatter=CustomFormatter())
