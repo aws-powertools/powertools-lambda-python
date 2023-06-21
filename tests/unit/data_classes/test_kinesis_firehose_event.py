98afae37f6aee0b6c0ed87ed8f264281cbe6378b
Leandro Damascena <lcdama@amazon.pt>
2023-06-21 21:14:47 +0100

    refactor: kinesisfirehose

--- tests/unit/data_classes/test_kinesis_firehose_event.py
+++ tests/unit/data_classes/test_kinesis_firehose_event.py
@@ -3,65 +3,77 @@ from tests.functional.utils import load_event
 
 
 def test_kinesis_firehose_kinesis_event():
-    event = KinesisFirehoseEvent(load_event("kinesisFirehoseKinesisEvent.json"))
+    raw_event = load_event("kinesisFirehoseKinesisEvent.json")
+    parsed_event = KinesisFirehoseEvent(raw_event)
 
-    assert event.region == "us-east-2"
-    assert event.invocation_id == "2b4d1ad9-2f48-94bd-a088-767c317e994a"
-    assert event.delivery_stream_arn == "arn:aws:firehose:us-east-2:123456789012:deliverystream/delivery-stream-name"
-    assert event.source_kinesis_stream_arn == "arn:aws:kinesis:us-east-1:123456789012:stream/kinesis-source"
+    assert parsed_event.region == raw_event["region"]
+    assert parsed_event.invocation_id == raw_event["invocationId"]
+    assert parsed_event.delivery_stream_arn == raw_event["deliveryStreamArn"]
+    assert parsed_event.source_kinesis_stream_arn == raw_event["sourceKinesisStreamArn"]
 
-    records = list(event.records)
+    records = list(parsed_event.records)
     assert len(records) == 2
     record_01, record_02 = records[:]
 
-    assert record_01.approximate_arrival_timestamp == 1664028820148
-    assert record_01.record_id == "record1"
-    assert record_01.data == "SGVsbG8gV29ybGQ="
+    record01_raw = raw_event["records"][0]
+    assert record_01.approximate_arrival_timestamp == record01_raw["approximateArrivalTimestamp"]
+    assert record_01.record_id == record01_raw["recordId"]
+    assert record_01.data == record01_raw["data"]
     assert record_01.data_as_bytes == b"Hello World"
     assert record_01.data_as_text == "Hello World"
 
-    assert record_01.metadata.shard_id == "shardId-000000000000"
-    assert record_01.metadata.partition_key == "4d1ad2b9-24f8-4b9d-a088-76e9947c317a"
-    assert record_01.metadata.approximate_arrival_timestamp == 1664028820148
-    assert record_01.metadata.sequence_number == "49546986683135544286507457936321625675700192471156785154"
-    assert record_01.metadata.subsequence_number == ""
+    assert record_01.metadata.shard_id == record01_raw["kinesisRecordMetadata"]["shardId"]
+    assert record_01.metadata.partition_key == record01_raw["kinesisRecordMetadata"]["partitionKey"]
+    assert (
+        record_01.metadata.approximate_arrival_timestamp
+        == record01_raw["kinesisRecordMetadata"]["approximateArrivalTimestamp"]
+    )
+    assert record_01.metadata.sequence_number == record01_raw["kinesisRecordMetadata"]["sequenceNumber"]
+    assert record_01.metadata.subsequence_number == record01_raw["kinesisRecordMetadata"]["subsequenceNumber"]
 
-    assert record_02.approximate_arrival_timestamp == 1664028793294
-    assert record_02.record_id == "record2"
-    assert record_02.data == "eyJIZWxsbyI6ICJXb3JsZCJ9"
+    record02_raw = raw_event["records"][1]
+    assert record_02.approximate_arrival_timestamp == record02_raw["approximateArrivalTimestamp"]
+    assert record_02.record_id == record02_raw["recordId"]
+    assert record_02.data == record02_raw["data"]
     assert record_02.data_as_bytes == b'{"Hello": "World"}'
     assert record_02.data_as_text == '{"Hello": "World"}'
     assert record_02.data_as_json == {"Hello": "World"}
 
-    assert record_02.metadata.shard_id == "shardId-000000000001"
-    assert record_02.metadata.partition_key == "4d1ad2b9-24f8-4b9d-a088-76e9947c318a"
-    assert record_02.metadata.approximate_arrival_timestamp == 1664028793294
-    assert record_02.metadata.sequence_number == "49546986683135544286507457936321625675700192471156785155"
-    assert record_02.metadata.subsequence_number == ""
+    assert record_02.metadata.shard_id == record02_raw["kinesisRecordMetadata"]["shardId"]
+    assert record_02.metadata.partition_key == record02_raw["kinesisRecordMetadata"]["partitionKey"]
+    assert (
+        record_02.metadata.approximate_arrival_timestamp
+        == record02_raw["kinesisRecordMetadata"]["approximateArrivalTimestamp"]
+    )
+    assert record_02.metadata.sequence_number == record02_raw["kinesisRecordMetadata"]["sequenceNumber"]
+    assert record_02.metadata.subsequence_number == record02_raw["kinesisRecordMetadata"]["subsequenceNumber"]
 
 
 def test_kinesis_firehose_put_event():
-    event = KinesisFirehoseEvent(load_event("kinesisFirehosePutEvent.json"))
+    raw_event = load_event("kinesisFirehosePutEvent.json")
+    parsed_event = KinesisFirehoseEvent(raw_event)
 
-    assert event.region == "us-east-2"
-    assert event.invocation_id == "2b4d1ad9-2f48-94bd-a088-767c317e994a"
-    assert event.delivery_stream_arn == "arn:aws:firehose:us-east-2:123456789012:deliverystream/delivery-stream-name"
-    assert event.source_kinesis_stream_arn is None
+    assert parsed_event.region == raw_event["region"]
+    assert parsed_event.invocation_id == raw_event["invocationId"]
+    assert parsed_event.delivery_stream_arn == raw_event["deliveryStreamArn"]
+    assert parsed_event.source_kinesis_stream_arn is None
 
-    records = list(event.records)
+    records = list(parsed_event.records)
     assert len(records) == 2
     record_01, record_02 = records[:]
 
-    assert record_01.approximate_arrival_timestamp == 1664029185290
-    assert record_01.record_id == "record1"
-    assert record_01.data == "SGVsbG8gV29ybGQ="
+    record01_raw = raw_event["records"][0]
+    assert record_01.approximate_arrival_timestamp == record01_raw["approximateArrivalTimestamp"]
+    assert record_01.record_id == record01_raw["recordId"]
+    assert record_01.data == record01_raw["data"]
     assert record_01.data_as_bytes == b"Hello World"
     assert record_01.data_as_text == "Hello World"
     assert record_01.metadata is None
 
-    assert record_02.approximate_arrival_timestamp == 1664029186945
-    assert record_02.record_id == "record2"
-    assert record_02.data == "eyJIZWxsbyI6ICJXb3JsZCJ9"
+    record02_raw = raw_event["records"][1]
+    assert record_02.approximate_arrival_timestamp == record02_raw["approximateArrivalTimestamp"]
+    assert record_02.record_id == record02_raw["recordId"]
+    assert record_02.data == record02_raw["data"]
     assert record_02.data_as_bytes == b'{"Hello": "World"}'
     assert record_02.data_as_text == '{"Hello": "World"}'
     assert record_02.data_as_json == {"Hello": "World"}
