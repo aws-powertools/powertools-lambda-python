279a5f6bb8a2fa9d7bed749b52cc0d9f708b7c0e
Leandro Damascena <lcdama@amazon.pt>
2023-06-21 18:09:56 +0100

    refactor: alb event

--- tests/unit/data_classes/test_alb_event.py
+++ tests/unit/data_classes/test_alb_event.py
@@ -3,13 +3,15 @@ from tests.functional.utils import load_event
 
 
 def test_alb_event():
-    event = ALBEvent(load_event("albEvent.json"))
-    assert event.request_context.elb_target_group_arn == event["requestContext"]["elb"]["targetGroupArn"]
-    assert event.http_method == event["httpMethod"]
-    assert event.path == event["path"]
-    assert event.query_string_parameters == event["queryStringParameters"]
-    assert event.headers == event["headers"]
-    assert event.multi_value_query_string_parameters == event.get("multiValueQueryStringParameters")
-    assert event.multi_value_headers == event.get("multiValueHeaders")
-    assert event.body == event["body"]
-    assert event.is_base64_encoded == event["isBase64Encoded"]
+    raw_event = load_event("albEvent.json")
+    parsed_event = ALBEvent(raw_event)
+
+    assert parsed_event.request_context.elb_target_group_arn == raw_event["requestContext"]["elb"]["targetGroupArn"]
+    assert parsed_event.http_method == raw_event["httpMethod"]
+    assert parsed_event.path == raw_event["path"]
+    assert parsed_event.query_string_parameters == raw_event["queryStringParameters"]
+    assert parsed_event.headers == raw_event["headers"]
+    assert parsed_event.multi_value_query_string_parameters == raw_event.get("multiValueQueryStringParameters")
+    assert parsed_event.multi_value_headers == raw_event.get("multiValueHeaders")
+    assert parsed_event.body == raw_event["body"]
+    assert parsed_event.is_base64_encoded == raw_event["isBase64Encoded"]
