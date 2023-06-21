09b7b5a96cac19bdb51fd9d0908bb2d39988d085
Leandro Damascena <lcdama@amazon.pt>
2023-06-22 00:15:19 +0100

    refactor: lambda function url

--- tests/unit/data_classes/test_lambda_function_url.py
+++ tests/unit/data_classes/test_lambda_function_url.py
@@ -3,96 +3,102 @@ from tests.functional.utils import load_event
 
 
 def test_lambda_function_url_event():
-    event = LambdaFunctionUrlEvent(load_event("lambdaFunctionUrlEvent.json"))
+    raw_event = load_event("lambdaFunctionUrlEvent.json")
+    parsed_event = LambdaFunctionUrlEvent(raw_event)
 
-    assert event.version == "2.0"
-    assert event.route_key == "$default"
+    assert parsed_event.version == raw_event["version"]
+    assert parsed_event.route_key == raw_event["routeKey"]
 
-    assert event.path == "/"
-    assert event.raw_query_string == ""
+    assert parsed_event.path == raw_event["rawPath"]
+    assert parsed_event.raw_query_string == raw_event["rawQueryString"]
 
-    assert event.cookies is None
+    assert parsed_event.cookies is None
 
-    headers = event.headers
+    headers = parsed_event.headers
     assert len(headers) == 20
 
-    assert event.query_string_parameters is None
+    assert parsed_event.query_string_parameters is None
 
-    assert event.is_base64_encoded is False
-    assert event.body is None
-    assert event.path_parameters is None
-    assert event.stage_variables is None
-    assert event.http_method == "GET"
+    assert parsed_event.is_base64_encoded is False
+    assert parsed_event.body is None
+    assert parsed_event.path_parameters is None
+    assert parsed_event.stage_variables is None
+    assert parsed_event.http_method == raw_event["requestContext"]["http"]["method"]
 
-    request_context = event.request_context
+    request_context = parsed_event.request_context
+    request_context_raw = raw_event["requestContext"]
 
-    assert request_context.account_id == "anonymous"
-    assert request_context.api_id is not None
-    assert request_context.domain_name == "<url-id>.lambda-url.us-east-1.on.aws"
-    assert request_context.domain_prefix == "<url-id>"
-    assert request_context.request_id == "id"
-    assert request_context.route_key == "$default"
-    assert request_context.stage == "$default"
-    assert request_context.time is not None
-    assert request_context.time_epoch == 1659687279885
+    assert request_context.account_id == request_context_raw["accountId"]
+    assert request_context.api_id == request_context_raw["apiId"]
+    assert request_context.domain_name == request_context_raw["domainName"]
+    assert request_context.domain_prefix == request_context_raw["domainPrefix"]
+    assert request_context.request_id == request_context_raw["requestId"]
+    assert request_context.route_key == request_context_raw["routeKey"]
+    assert request_context.stage == request_context_raw["stage"]
+    assert request_context.time == request_context_raw["time"]
+    assert request_context.time_epoch == request_context_raw["timeEpoch"]
     assert request_context.authentication is None
 
     http = request_context.http
-    assert http.method == "GET"
-    assert http.path == "/"
-    assert http.protocol == "HTTP/1.1"
-    assert http.source_ip == "123.123.123.123"
-    assert http.user_agent == "agent"
+    http_raw = raw_event["requestContext"]["http"]
+    assert http.method == http_raw["method"]
+    assert http.path == http_raw["path"]
+    assert http.protocol == http_raw["protocol"]
+    assert http.source_ip == http_raw["sourceIp"]
+    assert http.user_agent == http_raw["userAgent"]
 
     assert request_context.authorizer is None
 
 
 def test_lambda_function_url_event_iam():
-    event = LambdaFunctionUrlEvent(load_event("lambdaFunctionUrlIAMEvent.json"))
+    raw_event = load_event("lambdaFunctionUrlIAMEvent.json")
+    parsed_event = LambdaFunctionUrlEvent(raw_event)
 
-    assert event.version == "2.0"
-    assert event.route_key == "$default"
+    assert parsed_event.version == raw_event["version"]
+    assert parsed_event.route_key == raw_event["routeKey"]
 
-    assert event.path == "/my/path"
-    assert event.raw_query_string == "parameter1=value1&parameter1=value2&parameter2=value"
+    assert parsed_event.path == raw_event["rawPath"]
+    assert parsed_event.raw_query_string == raw_event["rawQueryString"]
 
-    cookies = event.cookies
+    headers = parsed_event.headers
+    assert len(headers) == 2
+
+    cookies = parsed_event.cookies
     assert len(cookies) == 2
     assert cookies[0] == "cookie1"
 
-    headers = event.headers
-    assert len(headers) == 2
-
-    query_string_parameters = event.query_string_parameters
+    query_string_parameters = parsed_event.query_string_parameters
     assert len(query_string_parameters) == 2
-    assert query_string_parameters.get("parameter2") == "value"
+    assert query_string_parameters.get("parameter2") == raw_event["queryStringParameters"]["parameter2"]
 
-    assert event.is_base64_encoded is False
-    assert event.body == "Hello from client!"
-    assert event.decoded_body == event.body
-    assert event.path_parameters is None
-    assert event.stage_variables is None
-    assert event.http_method == "POST"
+    assert parsed_event.is_base64_encoded is False
+    assert parsed_event.body == raw_event["body"]
+    assert parsed_event.decoded_body == raw_event["body"]
+    assert parsed_event.path_parameters is None
+    assert parsed_event.stage_variables is None
+    assert parsed_event.http_method == raw_event["requestContext"]["http"]["method"]
 
-    request_context = event.request_context
+    request_context = parsed_event.request_context
+    request_context_raw = raw_event["requestContext"]
 
-    assert request_context.account_id == "123456789012"
-    assert request_context.api_id is not None
-    assert request_context.domain_name == "<url-id>.lambda-url.us-west-2.on.aws"
-    assert request_context.domain_prefix == "<url-id>"
-    assert request_context.request_id == "id"
-    assert request_context.route_key == "$default"
-    assert request_context.stage == "$default"
-    assert request_context.time is not None
-    assert request_context.time_epoch == 1583348638390
+    assert request_context.account_id == request_context_raw["accountId"]
+    assert request_context.api_id == request_context_raw["apiId"]
+    assert request_context.domain_name == request_context_raw["domainName"]
+    assert request_context.domain_prefix == request_context_raw["domainPrefix"]
+    assert request_context.request_id == request_context_raw["requestId"]
+    assert request_context.route_key == request_context_raw["routeKey"]
+    assert request_context.stage == request_context_raw["stage"]
+    assert request_context.time == request_context_raw["time"]
+    assert request_context.time_epoch == request_context_raw["timeEpoch"]
     assert request_context.authentication is None
 
     http = request_context.http
-    assert http.method == "POST"
-    assert http.path == "/my/path"
-    assert http.protocol == "HTTP/1.1"
-    assert http.source_ip == "123.123.123.123"
-    assert http.user_agent == "agent"
+    http_raw = raw_event["requestContext"]["http"]
+    assert http.method == http_raw["method"]
+    assert http.path == http_raw["path"]
+    assert http.protocol == http_raw["protocol"]
+    assert http.source_ip == http_raw["sourceIp"]
+    assert http.user_agent == http_raw["userAgent"]
 
     authorizer = request_context.authorizer
     assert authorizer is not None
@@ -101,13 +107,14 @@ def test_lambda_function_url_event_iam():
     assert authorizer.get_lambda is None
 
     iam = authorizer.iam
+    iam_raw = raw_event["requestContext"]["authorizer"]["iam"]
     assert iam is not None
-    assert iam.access_key is not None
-    assert iam.account_id == "111122223333"
-    assert iam.caller_id is not None
+    assert iam.access_key == iam_raw["accessKey"]
+    assert iam.account_id == iam_raw["accountId"]
+    assert iam.caller_id == iam_raw["callerId"]
     assert iam.cognito_amr is None
     assert iam.cognito_identity_id is None
     assert iam.cognito_identity_pool_id is None
     assert iam.principal_org_id is None
-    assert iam.user_id is not None
-    assert iam.user_arn == "arn:aws:iam::111122223333:user/example-user"
+    assert iam.user_id == iam_raw["userId"]
+    assert iam.user_arn == iam_raw["userArn"]
