4c990d6c3c72b50b6caa173dc96603055ea0d8e2
Leandro Damascena <lcdama@amazon.pt>
2023-06-21 19:54:02 +0100

    refactor: cloudwatch widget

--- tests/unit/data_classes/test_cloud_watch_custom_widget_event.py
+++ tests/unit/data_classes/test_cloud_watch_custom_widget_event.py
@@ -5,34 +5,37 @@ from tests.functional.utils import load_event
 
 
 def test_cloud_watch_dashboard_event():
-    event = CloudWatchDashboardCustomWidgetEvent(load_event("cloudWatchDashboardEvent.json"))
-    assert event.describe is False
-    assert event.widget_context.account_id == "123456789123"
-    assert event.widget_context.domain == "https://us-east-1.console.aws.amazon.com"
-    assert event.widget_context.dashboard_name == "Name-of-current-dashboard"
-    assert event.widget_context.widget_id == "widget-16"
-    assert event.widget_context.locale == "en"
-    assert event.widget_context.timezone.label == "UTC"
-    assert event.widget_context.timezone.offset_iso == "+00:00"
-    assert event.widget_context.timezone.offset_in_minutes == 0
-    assert event.widget_context.period == 300
-    assert event.widget_context.is_auto_period is True
-    assert event.widget_context.time_range.mode == "relative"
-    assert event.widget_context.time_range.start == 1627236199729
-    assert event.widget_context.time_range.end == 1627322599729
-    assert event.widget_context.time_range.relative_start == 86400012
-    assert event.widget_context.time_range.zoom_start == 1627276030434
-    assert event.widget_context.time_range.zoom_end == 1627282956521
-    assert event.widget_context.theme == "light"
-    assert event.widget_context.link_charts is True
-    assert event.widget_context.title == "Tweets for Amazon website problem"
-    assert event.widget_context.forms == {}
-    assert event.widget_context.params == {"original": "param-to-widget"}
-    assert event.widget_context.width == 588
-    assert event.widget_context.height == 369
-    assert event.widget_context.params["original"] == "param-to-widget"
-    assert event["original"] == "param-to-widget"
-    assert event.raw_event["original"] == "param-to-widget"
+    raw_event = load_event("cloudWatchDashboardEvent.json")
+    parsed_event = CloudWatchDashboardCustomWidgetEvent(raw_event)
+
+    assert parsed_event.describe is False
+    widget_context_raw = raw_event["widgetContext"]
+    assert parsed_event.widget_context.account_id == widget_context_raw["accountId"]
+    assert parsed_event.widget_context.domain == widget_context_raw["domain"]
+    assert parsed_event.widget_context.dashboard_name == widget_context_raw["dashboardName"]
+    assert parsed_event.widget_context.widget_id == widget_context_raw["widgetId"]
+    assert parsed_event.widget_context.locale == widget_context_raw["locale"]
+    assert parsed_event.widget_context.timezone.label == widget_context_raw["timezone"]["label"]
+    assert parsed_event.widget_context.timezone.offset_iso == widget_context_raw["timezone"]["offsetISO"]
+    assert parsed_event.widget_context.timezone.offset_in_minutes == widget_context_raw["timezone"]["offsetInMinutes"]
+    assert parsed_event.widget_context.period == widget_context_raw["period"]
+    assert parsed_event.widget_context.is_auto_period is True
+    assert parsed_event.widget_context.time_range.mode == widget_context_raw["timeRange"]["mode"]
+    assert parsed_event.widget_context.time_range.start == widget_context_raw["timeRange"]["start"]
+    assert parsed_event.widget_context.time_range.end == widget_context_raw["timeRange"]["end"]
+    assert parsed_event.widget_context.time_range.relative_start == widget_context_raw["timeRange"]["relativeStart"]
+    assert parsed_event.widget_context.time_range.zoom_start == widget_context_raw["timeRange"]["zoom"]["start"]
+    assert parsed_event.widget_context.time_range.zoom_end == widget_context_raw["timeRange"]["zoom"]["end"]
+    assert parsed_event.widget_context.theme == widget_context_raw["theme"]
+    assert parsed_event.widget_context.link_charts is True
+    assert parsed_event.widget_context.title == widget_context_raw["title"]
+    assert parsed_event.widget_context.forms == {}
+    assert parsed_event.widget_context.params == {"original": "param-to-widget"}
+    assert parsed_event.widget_context.width == widget_context_raw["width"]
+    assert parsed_event.widget_context.height == widget_context_raw["height"]
+    assert parsed_event.widget_context.params["original"] == "param-to-widget"
+    assert parsed_event["original"] == "param-to-widget"
+    assert parsed_event.raw_event["original"] == "param-to-widget"
 
 
 def test_cloud_watch_dashboard_describe_event():
