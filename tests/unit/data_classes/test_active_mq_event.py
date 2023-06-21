3031acba6c60dcd4660be5f4f83d05c7f85ac742
Leandro Damascena <lcdama@amazon.pt>
2023-06-21 18:08:42 +0100

    refactor: active mq

--- tests/unit/data_classes/test_active_mq_event.py
+++ tests/unit/data_classes/test_active_mq_event.py
@@ -6,25 +6,26 @@ from tests.functional.utils import load_event
 
 
 def test_active_mq_event():
-    event = ActiveMQEvent(load_event("activeMQEvent.json"))
+    raw_event = load_event("activeMQEvent.json")
+    parsed_event = ActiveMQEvent(raw_event)
 
-    assert event.event_source == "aws:amq"
-    assert event.event_source_arn is not None
-    assert len(list(event.messages)) == 3
+    assert parsed_event.event_source == raw_event["eventSource"]
+    assert parsed_event.event_source_arn == raw_event["eventSourceArn"]
+    assert len(list(parsed_event.messages)) == 3
 
-    message = event.message
+    message = parsed_event.message
     assert isinstance(message, ActiveMQMessage)
-    assert message.message_id is not None
-    assert message.message_type is not None
-    assert message.data is not None
-    assert message.decoded_data is not None
-    assert message.connection_id is not None
+    assert message.message_id == raw_event["messages"][0]["messageID"]
+    assert message.message_type == raw_event["messages"][0]["messageType"]
+    assert message.data == raw_event["messages"][0]["data"]
+    assert message.decoded_data == "ABC:AAAA"
+    assert message.connection_id == raw_event["messages"][0]["connectionId"]
     assert message.redelivered is False
-    assert message.timestamp is not None
-    assert message.broker_in_time is not None
-    assert message.broker_out_time is not None
-    assert message.properties["testKey"] == "testValue"
-    assert message.destination_physicalname is not None
+    assert message.timestamp == raw_event["messages"][0]["timestamp"]
+    assert message.broker_in_time == raw_event["messages"][0]["brokerInTime"]
+    assert message.broker_out_time == raw_event["messages"][0]["brokerOutTime"]
+    assert message.properties.get("testKey") == raw_event["messages"][0]["properties"]["testKey"]
+    assert message.destination_physicalname == raw_event["messages"][0]["destination"]["physicalname"]
     assert message.delivery_mode is None
     assert message.correlation_id is None
     assert message.reply_to is None
@@ -32,7 +33,7 @@ def test_active_mq_event():
     assert message.expiration is None
     assert message.priority is None
 
-    messages = list(event.messages)
+    messages = list(parsed_event.messages)
     message = messages[1]
     assert message.json_data["timeout"] == 0
     assert message.json_data["data"] == "CZrmf0Gw8Ov4bqLQxD4E"
