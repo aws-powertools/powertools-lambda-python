c8449715c19492d47f97869d98f17ea7ab652d6a
Rafael Ramos <rgsramos@amazon.de>
2023-06-30 12:20:38 +0200

    Fixing unit tests for SQS Events to assert against raw_event instead of hardcoded values

--- tests/unit/data_classes/test_sqs_event.py
+++ tests/unit/data_classes/test_sqs_event.py
@@ -43,7 +43,7 @@ def test_seq_trigger_event():
     assert record_2.json_body == {"message": "foo1"}
 
 
-def test_decode_nested_s3_event(raw_event):
+def test_decode_nested_s3_event():
     raw_event = load_event("s3SqsEvent.json")
     event = SQSEvent(raw_event)
 
@@ -52,47 +52,51 @@ def test_decode_nested_s3_event(raw_event):
     attributes = record.attributes
 
     assert len(records) == 1
-    assert record.message_id == "ca3e7a89-c358-40e5-8aa0-5da01403c267"
+    assert record.message_id == raw_event["Records"][0]["messageId"]
     assert attributes.aws_trace_header is None
-    assert attributes.approximate_receive_count == "1"
-    assert attributes.sent_timestamp == "1681332219270"
-    assert attributes.sender_id == "AIDAJHIPRHEMV73VRJEBU"
-    assert attributes.approximate_first_receive_timestamp == "1681332239270"
+    raw_attributes = raw_event["Records"][0]["attributes"]
+    assert attributes.approximate_receive_count == raw_attributes["ApproximateReceiveCount"]
+    assert attributes.sent_timestamp == raw_attributes["SentTimestamp"]
+    assert attributes.sender_id == raw_attributes["SenderId"]
+    assert attributes.approximate_first_receive_timestamp == raw_attributes["ApproximateFirstReceiveTimestamp"]
     assert attributes.sequence_number is None
     assert attributes.message_group_id is None
     assert attributes.message_deduplication_id is None
-    assert record.md5_of_body == "16f4460f4477d8d693a5abe94fdbbd73"
-    assert record.event_source == "aws:sqs"
-    assert record.event_source_arn == "arn:aws:sqs:us-east-1:123456789012:SQS"
-    assert record.aws_region == "us-east-1"
+    assert record.md5_of_body == raw_event["Records"][0]["md5OfBody"]
+    assert record.event_source == raw_event["Records"][0]["eventSource"]
+    assert record.event_source_arn == raw_event["Records"][0]["eventSourceARN"]
+    assert record.aws_region == raw_event["Records"][0]["awsRegion"]
 
     s3_event: S3Event = record.decode_nested_s3_event
     s3_record = s3_event.record
+    raw_body = json.loads(raw_event["Records"][0]["body"])
 
-    assert s3_event.bucket_name == "xxx"
-    assert s3_event.object_key == "test.pdf"
-    assert s3_record.aws_region == "us-east-1"
-    assert s3_record.event_name == "ObjectCreated:Put"
-    assert s3_record.event_source == "aws:s3"
-    assert s3_record.event_time == "2023-04-12T20:43:38.021Z"
-    assert s3_record.event_version == "2.1"
+    assert s3_event.bucket_name == raw_body["Records"][0]["s3"]["bucket"]["name"]
+    assert s3_event.object_key == raw_body["Records"][0]["s3"]["object"]["key"]
+    raw_s3_record = raw_body["Records"][0]
+    assert s3_record.aws_region == raw_s3_record["awsRegion"]
+    assert s3_record.event_name == raw_s3_record["eventName"]
+    assert s3_record.event_source == raw_s3_record["eventSource"]
+    assert s3_record.event_time == raw_s3_record["eventTime"]
+    assert s3_record.event_version == raw_s3_record["eventVersion"]
     assert s3_record.glacier_event_data is None
-    assert s3_record.request_parameters.source_ip_address == "93.108.161.96"
-    assert s3_record.response_elements["x-amz-request-id"] == "YMSSR8BZJ2Y99K6P"
-    assert s3_record.s3.s3_schema_version == "1.0"
-    assert s3_record.s3.bucket.arn == "arn:aws:s3:::xxx"
-    assert s3_record.s3.bucket.name == "xxx"
-    assert s3_record.s3.bucket.owner_identity.principal_id == "A1YQ72UWCM96UF"
-    assert s3_record.s3.configuration_id == "SNS"
-    assert s3_record.s3.get_object.etag == "2e3ad1e983318bbd8e73b080e2997980"
-    assert s3_record.s3.get_object.key == "test.pdf"
-    assert s3_record.s3.get_object.sequencer == "00643717F9F8B85354"
-    assert s3_record.s3.get_object.size == 104681
-    assert s3_record.s3.get_object.version_id == "yd3d4HaWOT2zguDLvIQLU6ptDTwKBnQV"
-    assert s3_record.user_identity.principal_id == "A1YQ72UWCM96UF"
+    assert s3_record.request_parameters.source_ip_address == raw_s3_record["requestParameters"]["sourceIPAddress"]
+    assert s3_record.response_elements["x-amz-request-id"] == raw_s3_record["responseElements"]["x-amz-request-id"]
+    assert s3_record.s3.s3_schema_version == raw_s3_record["s3"]["s3SchemaVersion"]
+    assert s3_record.s3.bucket.arn == raw_s3_record["s3"]["bucket"]["arn"]
+    assert s3_record.s3.bucket.name == raw_s3_record["s3"]["bucket"]["name"]
+    assert (
+        s3_record.s3.bucket.owner_identity.principal_id == raw_s3_record["s3"]["bucket"]["ownerIdentity"]["principalId"]
+    )
+    assert s3_record.s3.configuration_id == raw_s3_record["s3"]["configurationId"]
+    assert s3_record.s3.get_object.etag == raw_s3_record["s3"]["object"]["eTag"]
+    assert s3_record.s3.get_object.key == raw_s3_record["s3"]["object"]["key"]
+    assert s3_record.s3.get_object.sequencer == raw_s3_record["s3"]["object"]["sequencer"]
+    assert s3_record.s3.get_object.size == raw_s3_record["s3"]["object"]["size"]
+    assert s3_record.s3.get_object.version_id == raw_s3_record["s3"]["object"]["versionId"]
 
 
-def test_decode_nested_sns_event(raw_event):
+def test_decode_nested_sns_event():
     raw_event = load_event("snsSqsEvent.json")
     event = SQSEvent(raw_event)
 
@@ -101,27 +105,30 @@ def test_decode_nested_sns_event(raw_event):
     attributes = record.attributes
 
     assert len(records) == 1
-    assert record.message_id == "79406a00-bf15-46ca-978c-22c3613fcb30"
+    assert record.message_id == raw_event["Records"][0]["messageId"]
+    raw_attributes = raw_event["Records"][0]["attributes"]
     assert attributes.aws_trace_header is None
-    assert attributes.approximate_receive_count == "1"
-    assert attributes.sent_timestamp == "1611050827340"
-    assert attributes.sender_id == "AIDAISMY7JYY5F7RTT6AO"
-    assert attributes.approximate_first_receive_timestamp == "1611050827344"
+    assert attributes.approximate_receive_count == raw_attributes["ApproximateReceiveCount"]
+    assert attributes.sent_timestamp == raw_attributes["SentTimestamp"]
+    assert attributes.sender_id == raw_attributes["SenderId"]
+    assert attributes.approximate_first_receive_timestamp == raw_attributes["ApproximateFirstReceiveTimestamp"]
     assert attributes.sequence_number is None
     assert attributes.message_group_id is None
     assert attributes.message_deduplication_id is None
-    assert record.md5_of_body == "8910bdaaf9a30a607f7891037d4af0b0"
-    assert record.event_source == "aws:sqs"
-    assert record.event_source_arn == "arn:aws:sqs:eu-west-1:231436140809:powertools265"
-    assert record.aws_region == "eu-west-1"
+    assert record.md5_of_body == raw_event["Records"][0]["md5OfBody"]
+    assert record.event_source == raw_event["Records"][0]["eventSource"]
+    assert record.event_source_arn == raw_event["Records"][0]["eventSourceARN"]
+    assert record.aws_region == raw_event["Records"][0]["awsRegion"]
 
     sns_message: SNSMessage = record.decode_nested_sns_event
+    raw_body = json.loads(raw_event["Records"][0]["body"])
     message = json.loads(sns_message.message)
 
-    assert sns_message.get_type == "Notification"
-    assert sns_message.message_id == "d88d4479-6ec0-54fe-b63f-1cf9df4bb16e"
-    assert sns_message.topic_arn == "arn:aws:sns:eu-west-1:231436140809:powertools265"
-    assert sns_message.timestamp == "2021-01-19T10:07:07.287Z"
-    assert sns_message.signature_version == "1"
-    assert message["message"] == "hello world"
-    assert message["username"] == "lessa"
+    assert sns_message.get_type == raw_body["Type"]
+    assert sns_message.message_id == raw_body["MessageId"]
+    assert sns_message.topic_arn == raw_body["TopicArn"]
+    assert sns_message.timestamp == raw_body["Timestamp"]
+    assert sns_message.signature_version == raw_body["SignatureVersion"]
+    raw_message = json.loads(raw_body["Message"])
+    assert message["message"] == raw_message["message"]
+    assert message["username"] == raw_message["username"]
