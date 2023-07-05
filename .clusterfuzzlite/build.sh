2f2082d8c20039cf92bf650bfb3f71d25e4b670f
heitorlessa <lessa@amazon.co.uk>
2023-07-05 14:26:05 +0200

    chore: try with minimal config

---
+++ .clusterfuzzlite/build.sh
@@ -0,0 +1,6 @@
+# Build and install project (using current CFLAGS, CXXFLAGS).
+pip3 install --upgrade pip
+pip3 install .
+for fuzzer in $(find $SRC -name 'fuzz_*.py'); do
+    compile_python_fuzzer $fuzzer
+done
