3673c4b79daf8ed9b63edbd62accb0680bcb7fd5
Roger Zhang <ruojiazh@amazon.com>
2023-07-13 11:31:47 -0700

    fix docstring polish datadog_provider add flush_to_log parameter remove OTEL provider draft

--- aws_lambda_powertools/metrics/provider/__init__.py
+++ aws_lambda_powertools/metrics/provider/__init__.py
@@ -5,13 +5,9 @@ from aws_lambda_powertools.metrics.provider.amazon_cloudwatch_emf import (
     Metrics,
 )
 from aws_lambda_powertools.metrics.provider.base import MetricsBase, MetricsProviderBase
-from aws_lambda_powertools.metrics.provider.datadog_provider_draft import (
+from aws_lambda_powertools.metrics.provider.datadog_provider import (
     DataDogMetrics,
     DataDogProvider,
-)
-from aws_lambda_powertools.metrics.provider.opentelemetry_provider_draft import (
-    OTLPMetrics,
-    OTLPProvider,
 )
 
 __all__ = [
@@ -23,7 +19,4 @@ __all__ = [
     "AmazonCloudWatchEMF",
     "EphemeralAmazonCloudWatchEMF",
     "EphemeralMetrics",
-    "CloudWatchEMF",
-    "OTLPProvider",
-    "OTLPMetrics",
 ]
