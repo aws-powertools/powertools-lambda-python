49561b22f0fcd138219c0eed8acb0e8226f38166
Cavalcante Damascena <lcdama@b0be8355743f.ant.amazon.com>
2023-07-21 12:34:58 +0100

    Adding cache to disable pydantic warnings
    Signed-off-by: Cavalcante Damascena <lcdama@b0be8355743f.ant.amazon.com>

--- aws_lambda_powertools/utilities/parser/compat.py
+++ aws_lambda_powertools/utilities/parser/compat.py
@@ -1,3 +1,7 @@
+import functools
+
+
+@functools.lru_cache(maxsize=None)
 def disable_pydantic_v2_warning():
     """
     Disables the Pydantic version 2 warning by filtering out the related warnings.
@@ -6,7 +10,9 @@ def disable_pydantic_v2_warning():
     it filters out the PydanticDeprecationWarning and PydanticDeprecatedSince20 warnings
     to suppress them.
 
-    Note: This function assumes that Pydantic is already imported.
+    Since we only need to run the code once, we are using lru_cache to improve performance.
+
+    Note: This function assumes that Pydantic is installed.
 
     Usage:
         disable_pydantic_v2_warning()
