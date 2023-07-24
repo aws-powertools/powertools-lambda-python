dfb989288a431cd5bb3c01903e833341310335ae
Ruben Fonseca <fonseka@gmail.com>
2023-07-24 14:18:04 +0200

    feat(general): add support for Python 3.11 (#2820)
    * feat: add support for Python 3.11
    
    * fix: remove python-snappY
    
    * fastjsonschema: test change due to behavior change in fastjsonschem
    
    * LRU: test
    
    * Removing snappy since it doesnt offer to Python3.11
    
    * Adding support to Python3.11 in the layer for test
    
    * Skipping test
    
    * Skipping test for Python 3.11+
    
    ---------
    
    Co-authored-by: Leandro Damascena <lcdama@amazon.pt>

--- tests/e2e/utils/infrastructure.py
+++ tests/e2e/utils/infrastructure.py
@@ -97,12 +97,12 @@ class BaseInfrastructure(InfrastructureProvider):
         self.create_lambda_functions()
         ```
 
-        Creating Lambda functions and override runtime to Python 3.10
+        Creating Lambda functions and override runtime to Python 3.11
 
         ```python
         from aws_cdk.aws_lambda import Runtime
 
-        self.create_lambda_functions(function_props={"runtime": Runtime.PYTHON_3_10)
+        self.create_lambda_functions(function_props={"runtime": Runtime.PYTHON_3_11)
         ```
         """
         if not self._handlers_dir.exists():
@@ -118,6 +118,7 @@ class BaseInfrastructure(InfrastructureProvider):
                 Runtime.PYTHON_3_8,
                 Runtime.PYTHON_3_9,
                 Runtime.PYTHON_3_10,
+                Runtime.PYTHON_3_11,
             ],
             compatible_architectures=[architecture],
             code=Code.from_asset(path=layer_build),
@@ -257,6 +258,8 @@ class BaseInfrastructure(InfrastructureProvider):
             return Runtime.PYTHON_3_9
         elif version.major == 3 and version.minor == 10:
             return Runtime.PYTHON_3_10
+        elif version.major == 3 and version.minor == 11:
+            return Runtime.PYTHON_3_11
         else:
             raise Exception(f"Unsupported Python version: {version}")
 
