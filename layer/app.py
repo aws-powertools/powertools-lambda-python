7901e990db945c7e955378a96115116b78bfb10d
ivica-k <ivica.kolenkas@gmail.com>
2023-07-26 21:03:04 +0200

    chore(ci): splits layer building into a stack per supported architecture

--- layer/app.py
+++ layer/app.py
@@ -4,10 +4,12 @@ import aws_cdk as cdk
 
 from layer.canary_stack import CanaryStack
 from layer.layer_stack import LayerStack
+from aws_cdk.aws_lambda import Architecture
 
 app = cdk.App()
 
 POWERTOOLS_VERSION: str = app.node.try_get_context("version")
+REGION_HAS_ARM64_SUPPORT: str = app.node.try_get_context("HasARM64Support")
 SSM_PARAM_LAYER_ARN: str = "/layers/powertools-layer-v2-arn"
 SSM_PARAM_LAYER_ARM64_ARN: str = "/layers/powertools-layer-v2-arm64-arn"
 
@@ -17,13 +19,23 @@ if not POWERTOOLS_VERSION:
         "synth step."
     )
 
+
 LayerStack(
     app,
     "LayerV2Stack",
     powertools_version=POWERTOOLS_VERSION,
     ssm_parameter_layer_arn=SSM_PARAM_LAYER_ARN,
-    ssm_parameter_layer_arm64_arn=SSM_PARAM_LAYER_ARM64_ARN,
+    architecture=Architecture.X86_64
 )
+
+if REGION_HAS_ARM64_SUPPORT and REGION_HAS_ARM64_SUPPORT.lower() == "true":
+    LayerStack(
+        app,
+        "LayerV2ARM64Stack",
+        powertools_version=POWERTOOLS_VERSION,
+        ssm_parameter_layer_arn=SSM_PARAM_LAYER_ARM64_ARN,
+        architecture=Architecture.ARM_64
+    )
 
 CanaryStack(
     app,
--- layer/layer/layer_stack.py
+++ layer/layer/layer_stack.py
@@ -1,33 +1,14 @@
 from typing import Optional
 
-import jsii
 from aws_cdk import (
-    Aspects,
-    CfnCondition,
     CfnOutput,
-    CfnParameter,
-    CfnResource,
-    Fn,
-    IAspect,
     RemovalPolicy,
     Stack,
 )
-from aws_cdk.aws_lambda import Architecture, CfnLayerVersionPermission
 from aws_cdk.aws_ssm import StringParameter
+from aws_cdk.aws_lambda import Architecture, CfnLayerVersionPermission
 from cdk_aws_lambda_powertools_layer import LambdaPowertoolsLayer
 from constructs import Construct
-
-
-@jsii.implements(IAspect)
-class ApplyCondition:
-    def __init__(self, condition: CfnCondition):
-        self.condition = condition
-
-    def visit(self, node):
-        if isinstance(node, CfnResource):
-            node.cfn_options.condition = self.condition
-        if isinstance(node, CfnOutput):
-            node.condition = self.condition
 
 
 class Layer(Construct):
@@ -71,105 +52,30 @@ class LayerStack(Stack):
         construct_id: str,
         powertools_version: str,
         ssm_parameter_layer_arn: str,
-        ssm_parameter_layer_arm64_arn: str,
+        architecture: Architecture,
         **kwargs
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        has_arm64_support = CfnParameter(
-            self,
-            "HasARM64Support",
-            description="Has ARM64 Support Condition",
-            type="String",
-            allowed_values=["true", "false"],
+        layer_name = (
+            "AWSLambdaPowertoolsPythonV2"
+            if architecture.to_string() == "x86_64"
+            else "AWSLambdaPowertoolsPythonV2-Arm64"
         )
-
-        has_arm64_condition = CfnCondition(
-            self,
-            "HasARM64SupportCondition",
-            expression=Fn.condition_equals(has_arm64_support, "true"),
-        )
-        has_no_arm64_condition = CfnCondition(
-            self,
-            "HasNOArm64SupportCondition",
-            expression=Fn.condition_equals(has_arm64_support, "false"),
-        )
-
-        # The following code is used when the region does not support ARM64 Lambdas. We make sure to only create the
-        # X86_64 Layer without specifying any compatible architecture, which would result in a CloudFormation error.
-
-        layer_single = Layer(
-            self,
-            "LayerSingle",
-            layer_version_name="AWSLambdaPowertoolsPythonV2",
-            powertools_version=powertools_version,
-        )
-        Aspects.of(layer_single).add(ApplyCondition(has_no_arm64_condition))
-
-        Aspects.of(
-            StringParameter(
-                self,
-                "SingleVersionArn",
-                parameter_name=ssm_parameter_layer_arn,
-                string_value=layer_single.layer_version_arn,
-            )
-        ).add(ApplyCondition(has_no_arm64_condition))
-
-        # The following code is used when the region has support for ARM64 Lambdas. In this case, we explicitly
-        # create a Layer for both X86_64 and ARM64, specifying the compatible architectures.
-
-        # X86_64 layer
 
         layer = Layer(
             self,
             "Layer",
-            layer_version_name="AWSLambdaPowertoolsPythonV2",
+            layer_version_name=layer_name,
             powertools_version=powertools_version,
-            architecture=Architecture.X86_64,
+            architecture=architecture,
         )
-        Aspects.of(layer).add(ApplyCondition(has_arm64_condition))
-
-        Aspects.of(
-            StringParameter(
-                self,
-                "VersionArn",
-                parameter_name=ssm_parameter_layer_arn,
-                string_value=layer.layer_version_arn,
-            )
-        ).add(ApplyCondition(has_arm64_condition))
-
-        CfnOutput(
-            self,
-            "LatestLayerArn",
-            value=Fn.condition_if(
-                has_arm64_condition.logical_id,
-                layer.layer_version_arn,
-                layer_single.layer_version_arn,
-            ).to_string(),
-        )
-
-        # ARM64 layer
-
-        layer_arm64 = Layer(
-            self,
-            "Layer-ARM64",
-            layer_version_name="AWSLambdaPowertoolsPythonV2-Arm64",
-            powertools_version=powertools_version,
-            architecture=Architecture.ARM_64,
-        )
-        Aspects.of(layer_arm64).add(ApplyCondition(has_arm64_condition))
 
         StringParameter(
             self,
-            "Arm64VersionArn",
-            parameter_name=ssm_parameter_layer_arm64_arn,
-            string_value=Fn.condition_if(
-                has_arm64_condition.logical_id,
-                layer_arm64.layer_version_arn,
-                "none",
-            ).to_string(),
+            "VersionArn",
+            parameter_name=ssm_parameter_layer_arn,
+            string_value=layer.layer_version_arn,
         )
 
-        Aspects.of(
-            CfnOutput(self, "LatestLayerArm64Arn", value=layer_arm64.layer_version_arn)
-        ).add(ApplyCondition(has_arm64_condition))
+        CfnOutput(self, "LatestLayerArn", value=layer.layer_version_arn)
