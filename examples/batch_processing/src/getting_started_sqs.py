b6463a95acc5e4d1e796cd7b5dc4cbbb63ba07bc
Heitor Lessa <lessa@amazon.co.uk>
2023-07-27 18:10:48 +0200

    docs(batch): new visuals and error handling section (#2857)
    * fix(parameters): make cache aware of single vs multiple calls
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * chore: cleanup, add test for single and nested
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * docs(batch): simplify background with diagram and wording
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * docs: recommend sqs new record.json_body
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * docs: add code annotation for more details
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * docs: add initial error handling section
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * docs: add visual for partial failure mechanics
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * docs: add fifo and stream sequence numbers
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * docs: revamp ctx manager processing
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * docs: highlight use of model in BatchProcessor
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * docs: diagram for BYOP
    
    ---------
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- examples/batch_processing/src/getting_started_sqs.py
+++ examples/batch_processing/src/getting_started_sqs.py
@@ -1,5 +1,3 @@
-import json
-
 from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.utilities.batch import (
     BatchProcessor,
@@ -9,20 +7,23 @@ from aws_lambda_powertools.utilities.batch import (
 from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSRecord
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
-processor = BatchProcessor(event_type=EventType.SQS)
+processor = BatchProcessor(event_type=EventType.SQS)  # (1)!
 tracer = Tracer()
 logger = Logger()
 
 
 @tracer.capture_method
-def record_handler(record: SQSRecord):
-    payload: str = record.body
-    if payload:
-        item: dict = json.loads(payload)
-        logger.info(item)
+def record_handler(record: SQSRecord):  # (2)!
+    payload: str = record.json_body  # if json string data, otherwise record.body for str
+    logger.info(payload)
 
 
 @logger.inject_lambda_context
 @tracer.capture_lambda_handler
 def lambda_handler(event, context: LambdaContext):
-    return process_partial_response(event=event, record_handler=record_handler, processor=processor, context=context)
+    return process_partial_response(  # (3)!
+        event=event,
+        record_handler=record_handler,
+        processor=processor,
+        context=context,
+    )
