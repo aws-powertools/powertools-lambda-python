e8fdaa1a0902a2dbaf87507c1ee9c2e53efe936a
Leandro Damascena <lcdama@amazon.pt>
2023-08-08 17:35:21 +0100

    Refactoring tests

--- tests/unit/metrics/test_metrics.py
+++
@@ -1,8 +0,0 @@
-from aws_lambda_powertools.metrics.provider import (
-    BaseProvider,
-)
-from aws_lambda_powertools.metrics.provider.cloudwatch_emf.cloudwatch import AmazonCloudWatchEMFProvider
-
-
-def test_amazoncloudwatchemf_is_subclass_of_metricsproviderbase():
-    assert issubclass(AmazonCloudWatchEMFProvider, BaseProvider)
