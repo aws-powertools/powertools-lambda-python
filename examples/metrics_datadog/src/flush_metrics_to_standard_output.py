340b4466b44881e15c3bf6b92c30f60a6678d428
heitorlessa <lessa@amazon.co.uk>
2023-08-14 17:51:17 +0200

    docs(datadog): simplify code snippet
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- examples/metrics_datadog/src/flush_metrics_to_standard_output.py
+++ examples/metrics_datadog/src/flush_metrics_to_standard_output.py
@@ -1,8 +1,7 @@
-from aws_lambda_powertools.metrics.provider.datadog import DatadogMetrics, DatadogProvider
+from aws_lambda_powertools.metrics.provider.datadog import DatadogMetrics
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
-provider = DatadogProvider(flush_to_log=True)
-metrics = DatadogMetrics(provider=provider)
+metrics = DatadogMetrics(flush_to_log=True)
 
 
 @metrics.log_metrics  # ensures metrics are flushed upon request completion/failure
