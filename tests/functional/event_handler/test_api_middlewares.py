388592fb7b39f4bae31fa201ae51cd2c076b77fa
walmsles <2704782+walmsles@users.noreply.github.com>
2023-08-30 21:44:31 +1000

    feat(middlewwares): Expand tests to incdlue core gateway types

--- tests/functional/event_handler/test_api_middlewares.py
+++ tests/functional/event_handler/test_api_middlewares.py
@@ -1,24 +1,31 @@
+import pytest
+
 from aws_lambda_powertools.event_handler import content_types
 from aws_lambda_powertools.event_handler.api_gateway import (
+    APIGatewayHttpResolver,
     ApiGatewayResolver,
+    APIGatewayRestResolver,
     ProxyEventType,
     Response,
     Router,
 )
 from aws_lambda_powertools.event_handler.exceptions import BadRequestError
 from aws_lambda_powertools.event_handler.middlewares import SchemaValidationMiddleware
-from aws_lambda_powertools.utilities.data_classes import (
-    APIGatewayProxyEvent,
-)
 from tests.functional.utils import load_event
 
-LOAD_GW_EVENT = load_event("apiGatewayProxyEvent.json")
+API_REST_EVENT = load_event("apiGatewayProxyEvent.json")
+API_RESTV2_EVENT = load_event("apiGatewayProxyV2Event_GET.json")
 
 
-def test_route_with_middleware():
-    # GIVEN a Rest API Gateway proxy type event
-    app = ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEvent)
-
+@pytest.mark.parametrize(
+    "app, event",
+    [
+        (ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEvent), API_REST_EVENT),
+        (APIGatewayRestResolver(), API_REST_EVENT),
+        (APIGatewayHttpResolver(), API_RESTV2_EVENT),
+    ],
+)
+def test_route_with_middleware(app, event):
     # define custom middleware to inject new argument - "custom"
     def middleware_1(app, get_response, **kwargs):
         # inject a variable into the kwargs
@@ -35,25 +42,28 @@ def test_route_with_middleware():
 
     @app.get("/my/path", middlewares=[middleware_1, middleware_2])
     def get_lambda(another_one: int, custom: str) -> Response:
-        assert isinstance(app.current_event, APIGatewayProxyEvent)
         assert another_one == 6
         assert custom == "custom"
         return Response(200, content_types.TEXT_HTML, "foo")
 
     # WHEN calling the event handler
-    result = app(LOAD_GW_EVENT, {})
+    result = app(event, {})
 
     # THEN process event correctly
     # AND set the current_event type as APIGatewayProxyEvent
     assert result["statusCode"] == 200
-    assert result["multiValueHeaders"]["Content-Type"] == [content_types.TEXT_HTML]
     assert result["body"] == "foo"
 
 
-def test_with_router_middleware():
-    # GIVEN a Rest API Gateway proxy type event
-    app = ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEvent)
-
+@pytest.mark.parametrize(
+    "app, event",
+    [
+        (ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEvent), API_REST_EVENT),
+        (APIGatewayRestResolver(), API_REST_EVENT),
+        (APIGatewayHttpResolver(), API_RESTV2_EVENT),
+    ],
+)
+def test_with_router_middleware(app, event):
     # define custom middleware to inject new argument - "custom"
     def middleware_1(app, get_response, **kwargs):
         # inject a variable into the kwargs
@@ -72,25 +82,28 @@ def test_with_router_middleware():
 
     @app.get("/my/path", middlewares=[middleware_2])
     def get_lambda(another_one: int, custom: str) -> Response:
-        assert isinstance(app.current_event, APIGatewayProxyEvent)
         assert another_one == 6
         assert custom == "custom"
         return Response(200, content_types.TEXT_HTML, "foo")
 
     # WHEN calling the event handler
-    result = app(LOAD_GW_EVENT, {})
+    result = app(event, {})
 
     # THEN process event correctly
     # AND set the current_event type as APIGatewayProxyEvent
     assert result["statusCode"] == 200
-    assert result["multiValueHeaders"]["Content-Type"] == [content_types.TEXT_HTML]
     assert result["body"] == "foo"
 
 
-def test_dynamic_route_with_middleware():
-    # GIVEN
-    app = ApiGatewayResolver()
-
+@pytest.mark.parametrize(
+    "app, event",
+    [
+        (ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEvent), API_REST_EVENT),
+        (APIGatewayRestResolver(), API_REST_EVENT),
+        (APIGatewayHttpResolver(), API_RESTV2_EVENT),
+    ],
+)
+def test_dynamic_route_with_middleware(app, event):
     def middleware_one(app, get_response, **kwargs):
         # inject a variable into the kwargs
         response = get_response(app, injected="injected_value", **kwargs)
@@ -105,18 +118,22 @@ def test_dynamic_route_with_middleware():
         return Response(200, content_types.TEXT_HTML, my_id)
 
     # WHEN calling the event handler
-    result = app(LOAD_GW_EVENT, {})
+    result = app(event, {})
 
     # THEN
     assert result["statusCode"] == 200
-    assert result["multiValueHeaders"]["Content-Type"] == [content_types.TEXT_HTML]
     assert result["body"] == "path"
 
 
-def test_middleware_early_return():
-    # GIVEN
-    app = ApiGatewayResolver()
-
+@pytest.mark.parametrize(
+    "app, event",
+    [
+        (ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEvent), API_REST_EVENT),
+        (APIGatewayRestResolver(), API_REST_EVENT),
+        (APIGatewayHttpResolver(), API_RESTV2_EVENT),
+    ],
+)
+def test_middleware_early_return(app, event):
     def middleware_one(app, get_response, **context):
         # inject a variable into the kwargs
         response = get_response(app, injected="injected_value", **context)
@@ -140,74 +157,117 @@ def test_middleware_early_return():
         return Response(200, content_types.TEXT_HTML, my_id)
 
     # WHEN calling the event handler
-    result = app(LOAD_GW_EVENT, {})
+    result = app(event, {})
 
     # THEN
     assert result["statusCode"] == 400
-    assert result["multiValueHeaders"]["Content-Type"] == [content_types.TEXT_HTML]
     assert result["body"] == "bad_response"
 
 
-def test_pass_schema_validation(validation_schema):
-    # GIVEN
-    app = ApiGatewayResolver()
-
+@pytest.mark.parametrize(
+    "app, event",
+    [
+        (
+            ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEvent),
+            load_event("apigatewayeSchemaMiddlwareValidEvent.json"),
+        ),
+        (
+            APIGatewayRestResolver(),
+            load_event("apigatewayeSchemaMiddlwareValidEvent.json"),
+        ),
+        (
+            APIGatewayHttpResolver(),
+            load_event("apiGatewayProxyV2SchemaMiddlwareValidEvent.json"),
+        ),
+    ],
+)
+def test_pass_schema_validation(app, event, validation_schema):
     @app.post("/my/path", middlewares=[SchemaValidationMiddleware(validation_schema)])
     def post_lambda() -> Response:
         return Response(200, content_types.TEXT_HTML, "path")
 
     # WHEN calling the event handler
-    result = app(load_event("apigatewayeSchemaMiddlwareValidEvent.json"), {})
+    result = app(event, {})
 
     # THEN
     assert result["statusCode"] == 200
-    assert result["multiValueHeaders"]["Content-Type"] == [content_types.TEXT_HTML]
     assert result["body"] == "path"
 
 
-def test_fail_schema_validation(validation_schema):
-    # GIVEN
-    app = ApiGatewayResolver()
-
+@pytest.mark.parametrize(
+    "app, event",
+    [
+        (
+            ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEvent),
+            load_event("apigatewayeSchemaMiddlwareInvalidEvent.json"),
+        ),
+        (
+            APIGatewayRestResolver(),
+            load_event("apigatewayeSchemaMiddlwareInvalidEvent.json"),
+        ),
+        (
+            APIGatewayHttpResolver(),
+            load_event("apiGatewayProxyV2SchemaMiddlwareInvalidEvent.json"),
+        ),
+    ],
+)
+def test_fail_schema_validation(app, event, validation_schema):
     @app.post("/my/path", middlewares=[SchemaValidationMiddleware(validation_schema)])
     def post_lambda() -> Response:
         return Response(200, content_types.TEXT_HTML, "Should not be returned")
 
     # WHEN calling the event handler
-    result = app(load_event("apigatewayeSchemaMiddlwareInvalidEvent.json"), {})
+    result = app(event, {})
     print(f"\nRESULT:::{result}")
 
     # THEN
     assert result["statusCode"] == 400
-    assert result["multiValueHeaders"]["Content-Type"] == [content_types.APPLICATION_JSON]
     assert (
         result["body"]
         == "{\"statusCode\":400,\"message\":\"Bad Request: Failed schema validation. Error: data must contain ['message'] properties, Path: ['data'], Data: {'username': 'lessa'}\"}"  # noqa: E501
     )
 
 
-def test_invalid_schema_validation():
-    # GIVEN
-    app = ApiGatewayResolver()
-
+@pytest.mark.parametrize(
+    "app, event",
+    [
+        (
+            ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEvent),
+            load_event("apigatewayeSchemaMiddlwareValidEvent.json"),
+        ),
+        (
+            APIGatewayRestResolver(),
+            load_event("apigatewayeSchemaMiddlwareValidEvent.json"),
+        ),
+        (
+            APIGatewayHttpResolver(),
+            load_event("apiGatewayProxyV2SchemaMiddlwareInvalidEvent.json"),
+        ),
+    ],
+)
+def test_invalid_schema_validation(app, event):
     @app.post("/my/path", middlewares=[SchemaValidationMiddleware(inbound_schema="schema.json")])
     def post_lambda() -> Response:
         return Response(200, content_types.TEXT_HTML, "Should not be returned")
 
     # WHEN calling the event handler
-    result = app(load_event("apigatewayeSchemaMiddlwareValidEvent.json"), {})
+    result = app(event, {})
 
     print(f"\nRESULT:::{result}")
     # THEN
     assert result["statusCode"] == 500
-    assert result["multiValueHeaders"]["Content-Type"] == [content_types.APPLICATION_JSON]
     assert result["body"] == '{"statusCode":500,"message":"Internal Server Error"}'
 
 
-def test_middleware_short_circuit_via_httperrors():
-    # GIVEN
-    app = ApiGatewayResolver()
-
+@pytest.mark.parametrize(
+    "app, event",
+    [
+        (ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEvent), API_REST_EVENT),
+        (APIGatewayRestResolver(), API_REST_EVENT),
+        (APIGatewayHttpResolver(), API_RESTV2_EVENT),
+    ],
+)
+def test_middleware_short_circuit_via_httperrors(app, event):
     def middleware_one(app, get_response, **context):
         # inject a variable into the kwargs of the middleware chain
         response = get_response(app, injected="injected_value", **context)
@@ -231,17 +291,23 @@ def test_middleware_short_circuit_via_httperrors():
         return Response(200, content_types.TEXT_HTML, my_id)
 
     # WHEN calling the event handler
-    result = app(LOAD_GW_EVENT, {})
+    result = app(event, {})
 
     # THEN
     assert result["statusCode"] == 400
-    assert result["multiValueHeaders"]["Content-Type"] == [content_types.APPLICATION_JSON]
     assert result["body"] == '{"statusCode":400,"message":"bad_response"}'
 
 
-def test_api_gateway_app_router_with_middlewares():
+@pytest.mark.parametrize(
+    "app, event",
+    [
+        (ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEvent), API_REST_EVENT),
+        (APIGatewayRestResolver(), API_REST_EVENT),
+        (APIGatewayHttpResolver(), API_RESTV2_EVENT),
+    ],
+)
+def test_api_gateway_app_router_with_middlewares(app, event):
     # GIVEN a Router with registered routes
-    app = ApiGatewayResolver()
     router = Router()
 
     def app_middleware(app, get_response, **context):
@@ -279,9 +345,8 @@ def test_api_gateway_app_router_with_middlewares():
 
     app.include_router(router)
     # WHEN calling the event handler after applying routes from router object
-    result = app(LOAD_GW_EVENT, {})
+    result = app(event, {})
 
     # THEN process event correctly
     assert result["statusCode"] == 200
-    assert result["multiValueHeaders"]["Content-Type"] == [content_types.TEXT_HTML]
     assert result["body"] == to_inject
