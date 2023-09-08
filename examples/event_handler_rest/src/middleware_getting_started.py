b523583718260519dae3e3963054ae56a969e588
Heitor Lessa <lessa@amazon.com>
2023-09-08 12:59:11 +0200

    docs(event_handler): demonstrate how to combine logger correlation ID and middleware (#3064)
    * fix(parameters): make cache aware of single vs multiple calls
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * chore: cleanup, add test for single and nested
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * docs(event_handler): use correct correlation_id in logger.set_correlation_id example
    
    * docs(event_handler): combine correlation id with logger for a more powerful example
    
    * fix(parameters): make cache aware of single vs multiple calls
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * chore: cleanup, add test for single and nested
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    * docs: remove Logger dependency on retrieval to address Simon's feedback
    
    * docs: fix racing condition, more notes based on Simon's feedback
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>
    
    ---------
    
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- examples/event_handler_rest/src/middleware_getting_started.py
+++ examples/event_handler_rest/src/middleware_getting_started.py
@@ -12,21 +12,21 @@ def inject_correlation_id(app: APIGatewayRestResolver, next_middleware: NextMidd
     request_id = app.current_event.request_context.request_id  # (1)!
 
     # Use API Gateway REST API request ID if caller didn't include a correlation ID
-    correlation_id = app.current_event.headers.get("x-correlation-id", request_id)
+    correlation_id = logger.get_correlation_id() or request_id  # (2)!
 
     # Inject correlation ID in shared context and Logger
-    app.append_context(correlation_id=correlation_id)  # (2)!
+    app.append_context(correlation_id=correlation_id)  # (3)!
     logger.set_correlation_id(correlation_id)
 
     # Get response from next middleware OR /todos route
-    result = next_middleware(app)  # (3)!
+    result = next_middleware(app)  # (4)!
 
     # Include Correlation ID in the response back to caller
-    result.headers["x-correlation-id"] = correlation_id  # (4)!
+    result.headers["x-correlation-id"] = correlation_id  # (5)!
     return result
 
 
-@app.get("/todos", middlewares=[inject_correlation_id])  # (5)!
+@app.get("/todos", middlewares=[inject_correlation_id])  # (6)!
 def get_todos():
     todos: Response = requests.get("https://jsonplaceholder.typicode.com/todos")
     todos.raise_for_status()
@@ -35,6 +35,6 @@ def get_todos():
     return {"todos": todos.json()[:10]}
 
 
-@logger.inject_lambda_context
+@logger.inject_lambda_context(correlation_id_path='headers."x-correlation-id"')  # (7)!
 def lambda_handler(event, context):
     return app.resolve(event, context)
