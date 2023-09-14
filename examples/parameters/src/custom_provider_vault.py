86619e5ae4f29d68abfc6c20e708eedb8a9a6ca4
Leandro Damascena <lcdama@amazon.pt>
2023-09-14 15:57:30 +0100

    refactor(parameters): BaseProvider._get to also support Dict (#3090)

--- examples/parameters/src/custom_provider_vault.py
+++ examples/parameters/src/custom_provider_vault.py
@@ -1,5 +1,4 @@
-import json
-from typing import Dict
+from typing import Any, Dict
 
 from hvac import Client
 
@@ -8,21 +7,18 @@ from aws_lambda_powertools.utilities.parameters import BaseProvider
 
 class VaultProvider(BaseProvider):
     def __init__(self, vault_url: str, vault_token: str) -> None:
-
         super().__init__()
 
         self.vault_client = Client(url=vault_url, verify=False, timeout=10)
         self.vault_client.token = vault_token
 
-    def _get(self, name: str, **sdk_options) -> str:
-
+    def _get(self, name: str, **sdk_options) -> Dict[str, Any]:
         # for example proposal, the mountpoint is always /secret
         kv_configuration = self.vault_client.secrets.kv.v2.read_secret(path=name)
 
-        return json.dumps(kv_configuration["data"]["data"])
+        return kv_configuration["data"]["data"]
 
     def _get_multiple(self, path: str, **sdk_options) -> Dict[str, str]:
-
         list_secrets = {}
         all_secrets = self.vault_client.secrets.kv.v2.list_secrets(path=path)
 
