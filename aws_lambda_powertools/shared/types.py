3d7b7784fd0656c8a925637f5436bf26ef3afd85
heitorlessa <lessa@amazon.co.uk>
2023-09-14 18:17:36 +0200

    fix: still 3.7
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- aws_lambda_powertools/shared/types.py
+++ aws_lambda_powertools/shared/types.py
@@ -1,13 +1,10 @@
 import sys
 from typing import Any, Callable, Dict, List, TypeVar, Union
 
-if sys.version_info == (3, 7):
-    from typing_extensions import Literal, NotRequired, Protocol, TypeAlias, TypedDict
-
 if sys.version_info >= (3, 8):
     from typing import Literal, Protocol, TypedDict
 else:
-    from typing_extensions import Literal, Protocol
+    from typing_extensions import Literal, Protocol, TypedDict
 
 
 if sys.version_info >= (3, 11):
