7a488bc432e8f64cfe6e2ecf41c0fc09aeb0e946
stephenbawks <stephen@bawks.com>
2023-09-30 10:56:03 -0400

    creating "realistic" example

--- examples/parameters/src/getting_started_setting_secret.py
+++ examples/parameters/src/getting_started_setting_secret.py
@@ -6,20 +6,25 @@ from aws_lambda_powertools.utilities import parameters
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 
+def access_token(client_id: str, client_secret: str, audience: str) -> str:
+    # example function that returns a JWT access token
+    ...
+
+    return access_token
+
 def lambda_handler(event: dict, context: LambdaContext):
 
     try:
         # Usually an endpoint is not sensitive data, so we store it in SSM Parameters
-        endpoint_comments: Any = parameters.get_parameter("/lambda-powertools/endpoint_comments")
+        client_id: Any = parameters.get_parameter("/lambda-powertools/client_id")
+        client_secret: Any = parameters.get_parameter("/lambda-powertools/client_secret")
+        audience: Any = parameters.get_parameter("/lambda-powertools/audience")
 
-        # An API-KEY is a sensitive data and should be stored in SecretsManager
+        jwt_token = access_token(client_id=client_id, client_secret=client_secret, audience=audience)
+
         # set-secret will create a new secret if it doesn't exist and return the version id
-        update_secret = parameters.set_secret(name="/lambda-powertools/api-key", value="3884c335-25b0-4267-8531-561777eb2078")
+        update_secret = parameters.set_secret(name="/lambda-powertools/api-key", value=jwt_token)
 
-        headers: dict = {"X-API-Key": api_key}
-
-        comments: requests.Response = requests.get(endpoint_comments, headers=headers)
-
-        return {"comments": comments.json()[:10], "statusCode": 200}
+        return {"access_token": "updated", "statusCode": 200}
     except parameters.exceptions.GetParameterError as error:
-        return {"comments": None, "message": str(error), "statusCode": 400}
+        return {"access_token": "updated", "statusCode": 400}
