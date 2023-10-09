a559ed6b7e2087c60de66a847397336107095272
Ruben Fonseca <rubefons@amazon.com>
2023-10-09 20:57:37 +0200

    fix: remove assert

--- aws_lambda_powertools/event_handler/openapi/compat.py
+++ aws_lambda_powertools/event_handler/openapi/compat.py
@@ -198,7 +198,8 @@ if PYDANTIC_V2:
 
     def serialize_sequence_value(*, field: ModelField, value: Any) -> Sequence[Any]:
         origin_type = get_origin(field.field_info.annotation) or field.field_info.annotation
-        assert issubclass(origin_type, sequence_types)  # type: ignore[arg-type]
+        if not issubclass(origin_type, sequence_types):  # type: ignore[arg-type]
+            raise AssertionError(f"Expected sequence type, got {origin_type}")
         return sequence_annotation_to_type[origin_type](value)  # type: ignore[no-any-return]
 
     def _normalize_errors(errors: Sequence[Any]) -> List[Dict[str, Any]]:
