e6315815b1878819dee2641ba4c8df9c7fada4b5
Seshu Brahma <seshub@amazon.com>
2023-11-14 11:35:19 -0800

    Remove itsdangerous

--- examples/data_masking/src/custom_data_masking_provider.py
+++ examples/data_masking/src/custom_data_masking_provider.py
@@ -1,19 +1,18 @@
-from itsdangerous.url_safe import URLSafeSerializer
-
+import json
 from aws_lambda_powertools.utilities._data_masking.provider import BaseProvider
 
 
 class MyCustomEncryption(BaseProvider):
     def __init__(self, secret):
         super().__init__()
-        self.secret = URLSafeSerializer(secret)
+        self.secret = secret
 
     def encrypt(self, data: str) -> str:
         if data is None:
             return data
-        return self.secret.dumps(data)
+        return json.dumps(data)
 
     def decrypt(self, data: str) -> str:
         if data is None:
             return data
-        return self.secret.loads(data)
+        return json.loads(data)
