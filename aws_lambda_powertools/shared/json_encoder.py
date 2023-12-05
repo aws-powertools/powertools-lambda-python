8376a6b3426367056595fdba4dff050aea190e4c
heitorlessa <lessa@amazon.co.uk>
2023-12-05 17:19:26 +0100

    feat(event_handler): support pydantic/dataclass exception handler response

--- aws_lambda_powertools/shared/json_encoder.py
+++ aws_lambda_powertools/shared/json_encoder.py
@@ -4,8 +4,9 @@ import math
 
 
 class Encoder(json.JSONEncoder):
-    """
-    Custom JSON encoder to allow for serialization of Decimals, similar to the serializer used by Lambda internally.
+    """Custom JSON encoder to allow for serialization of Decimals, Pydantic and Dataclasses.
+
+    It's similar to the serializer used by Lambda internally.
     """
 
     def default(self, obj):
@@ -13,4 +14,17 @@ class Encoder(json.JSONEncoder):
             if obj.is_nan():
                 return math.nan
             return str(obj)
+
+        # Pydantic model (v1/v2)
+        if hasattr(obj, "json"):
+            from aws_lambda_powertools.event_handler.openapi.compat import _model_dump
+
+            return _model_dump(obj)
+
+        # Standard dataclass
+        if hasattr(obj, "__dataclass_fields__"):
+            import dataclasses
+
+            return dataclasses.asdict(obj)
+
         return super().default(obj)
