2c7ca54daf5fc2efa24637fa49676720319de1ad
Ruben Fonseca <rubefons@amazon.com>
2023-12-15 14:29:47 +0100

    chore: add maintenance note

--- aws_lambda_powertools/event_handler/util.py
+++ aws_lambda_powertools/event_handler/util.py
@@ -5,6 +5,8 @@ class _FrozenDict(dict):
     This is needed because the default dict implementation is not hashable.
     The only usage for this right now is to store dicts as part of the Router key.
     The implementation only takes into consideration the keys of the dictionary.
+
+    MAINTENANCE: this is a temporary solution until we refactor the route key into a class.
     """
 
     def __hash__(self):
