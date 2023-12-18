8573c2864ab0f0b192710fe3c69ea2a63034d36f
Ruben Fonseca <rubefons@amazon.com>
2023-12-18 13:47:43 +0100

    fix(event-handler): set default OpenAPI version to 3.0.0 (#3527)
    Co-authored-by: Leandro Damascena <lcdama@amazon.pt>

--- aws_lambda_powertools/event_handler/openapi/models.py
+++ aws_lambda_powertools/event_handler/openapi/models.py
@@ -207,7 +207,7 @@ class Schema(BaseModel):
     readOnly: Optional[bool] = None
     writeOnly: Optional[bool] = None
     examples: Optional[List["Example"]] = None
-    # Ref: OpenAPI 3.1.0: https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.0.md#schema-object
+    # Ref: OpenAPI 3.0.0: https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.0.0.md#schema-object
     # Schema Object
     discriminator: Optional[Discriminator] = None
     xml: Optional[XML] = None
