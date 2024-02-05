0d9d7b53cb8d47d82e92a9b790abae7bcbb4de76
Leandro Damascena <lcdama@amazon.pt>
2024-02-05 15:46:46 +0000

    Addressing Heitor's feedback

--- aws_lambda_powertools/utilities/streaming/compat.py
+++ aws_lambda_powertools/utilities/streaming/compat.py
@@ -1,3 +1,4 @@
 from botocore.response import StreamingBody
 
+# aliasing as 3.7 is no longer supported but unsure if anyone took a dependency on it (hyrum's law)
 PowertoolsStreamingBody = StreamingBody
