4a6deb7fcc1830edd34ac1b42077e7bdc7359f0b
Leandro Damascena <lcdama@amazon.pt>
2024-02-05 11:54:17 +0000

    Reverting

--- aws_lambda_powertools/shared/types.py
+++ aws_lambda_powertools/shared/types.py
@@ -1,18 +1,15 @@
 import sys
+from typing import Any, Callable, Dict, List, Literal, Protocol, TypedDict, TypeVar, Union
 
-from typing_extensions import (
-    Annotated,
-    Any,
-    Callable,
-    Dict,
-    List,
-    Literal,
-    NotRequired,
-    Protocol,
-    TypedDict,
-    TypeVar,
-    Union,
-)
+if sys.version_info >= (3, 9):
+    from typing import Annotated
+else:
+    from typing_extensions import Annotated
+
+if sys.version_info >= (3, 11):
+    from typing import NotRequired
+else:
+    from typing_extensions import NotRequired
 
 # Even though `get_args` and `get_origin` were added in Python 3.8, they only handle Annotated correctly on 3.10.
 # So for python < 3.10 we use the backport from typing_extensions.
