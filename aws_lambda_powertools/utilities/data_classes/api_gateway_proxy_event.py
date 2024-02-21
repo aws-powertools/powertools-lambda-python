e5daeb34a330131dfcd1cd33083fd44d448dfe1a
heitorlessa <lessa@amazon.co.uk>
2024-02-21 19:22:29 +0100

    chore: default dict for multi query strings too
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- aws_lambda_powertools/utilities/data_classes/api_gateway_proxy_event.py
+++ aws_lambda_powertools/utilities/data_classes/api_gateway_proxy_event.py
@@ -115,8 +115,8 @@ class APIGatewayProxyEvent(BaseProxyEvent):
         return self.get("multiValueHeaders") or {}
 
     @property
-    def multi_value_query_string_parameters(self) -> Optional[Dict[str, List[str]]]:
-        return self.get("multiValueQueryStringParameters")
+    def multi_value_query_string_parameters(self) -> Dict[str, List[str]]:
+        return self.get("multiValueQueryStringParameters") or {}
 
     @property
     def resolved_query_string_parameters(self) -> Dict[str, List[str]]:
