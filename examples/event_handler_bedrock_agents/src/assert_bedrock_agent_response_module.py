6430262e279f937525137dfa6bc146bdb4eb1f65
Ruben Fonseca <rubefons@amazon.com>
2024-02-27 16:54:12 +0100

    fix: types

--- examples/event_handler_bedrock_agents/src/assert_bedrock_agent_response_module.py
+++ examples/event_handler_bedrock_agents/src/assert_bedrock_agent_response_module.py
@@ -1,9 +1,9 @@
 import time
-from typing import Annotated
 
 from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.event_handler import BedrockAgentResolver
 from aws_lambda_powertools.event_handler.openapi.params import Body
+from aws_lambda_powertools.shared.types import Annotated
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 tracer = Tracer()
