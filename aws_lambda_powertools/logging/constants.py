26ac439314829ec6293bc5fcf2443266d88d37d2
Heitor Lessa <lessa@amazon.nl>
2024-05-13 15:46:57 +0200

    Update constants.py
    Signed-off-by: Heitor Lessa <lessa@amazon.nl>

--- aws_lambda_powertools/logging/constants.py
+++ aws_lambda_powertools/logging/constants.py
@@ -1,3 +1,4 @@
 LOGGER_ATTRIBUTE_POWERTOOLS_HANDLER = "powertools_handler"
+# logger.init attribute is set when Logger has been configured 
 LOGGER_ATTRIBUTE_PRECONFIGURED = "init"
 LOGGER_ATTRIBUTE_HANDLER = "logger_handler"
