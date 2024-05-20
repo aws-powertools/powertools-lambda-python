005fbab440cc8a470a0cdff1710273295bdbf813
Keith Rozario <keithjosephrozario@gmail.com>
2024-05-20 11:34:49 +0800

    moved code to examples

---
+++ examples/install/arm64/pulumi.py
@@ -0,0 +1,31 @@
+import json
+import pulumi
+import pulumi_aws as aws
+
+role = aws.iam.Role("role",
+    assume_role_policy=json.dumps({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+            "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow"
+        }
+    ]
+    }),
+    managed_policy_arns=[aws.iam.ManagedPolicy.AWS_LAMBDA_BASIC_EXECUTION_ROLE]
+)
+
+lambda_function = aws.lambda_.Function("function",
+    layers=[pulumi.Output.concat("arn:aws:lambda:",aws.get_region_output().name,":017000801446:layer:AWSLambdaPowertoolsPythonV2-Arm64:11")],
+    tracing_config={
+        "mode": "Active"
+    },
+    runtime=aws.lambda_.Runtime.PYTHON3D9,
+    handler="index.handler",
+    role=role.arn,
+    architectures=["arm64"],
+    code=pulumi.FileArchive("lambda_function_payload.zip")
+)
