10d551cfe0cbc34196ad1a59f1f4d0bf158159f8
Adrián Tomás <22175056+adriantomas@users.noreply.github.com>
2024-07-10 14:39:07 +0100

    feat(feature_flags): allow customers to bring their own boto3 client and session (#4717)
    * feat: add `sdk_client` argument to AppConfigStore `__init__`
    
    Signed-off-by: Adrián Tomás <22175056+adriantomas@users.noreply.github.com>
    
    * style: fix typing
    
    Signed-off-by: Adrián Tomás <22175056+adriantomas@users.noreply.github.com>
    
    * test: modify tests including sdk_client
    
    Signed-off-by: Adrián Tomás <22175056+adriantomas@users.noreply.github.com>
    
    * docs: add sdk_client to feature flags docs
    
    Signed-off-by: Adrián Tomás <22175056+adriantomas@users.noreply.github.com>
    
    * Adding some small changes
    
    * Addressing Adrian's feedback
    
    ---------
    
    Signed-off-by: Adrián Tomás <22175056+adriantomas@users.noreply.github.com>
    Co-authored-by: Leandro Damascena <lcdama@amazon.pt>

--- aws_lambda_powertools/utilities/feature_flags/appconfig.py
+++ aws_lambda_powertools/utilities/feature_flags/appconfig.py
@@ -1,7 +1,8 @@
 import logging
 import traceback
-from typing import Any, Dict, Optional, Union, cast
+from typing import TYPE_CHECKING, Any, Dict, Optional, Union, cast
 
+import boto3
 from botocore.config import Config
 
 from aws_lambda_powertools.utilities import jmespath_utils
@@ -15,6 +16,9 @@ from ... import Logger
 from .base import StoreProvider
 from .exceptions import ConfigurationStoreError, StoreClientError
 
+if TYPE_CHECKING:
+    from mypy_boto3_appconfigdata import AppConfigDataClient
+
 
 class AppConfigStore(StoreProvider):
     def __init__(
@@ -27,6 +31,9 @@ class AppConfigStore(StoreProvider):
         envelope: Optional[str] = "",
         jmespath_options: Optional[Dict] = None,
         logger: Optional[Union[logging.Logger, Logger]] = None,
+        boto_config: Optional[Config] = None,
+        boto3_session: Optional[boto3.session.Session] = None,
+        boto3_client: Optional["AppConfigDataClient"] = None,
     ):
         """This class fetches JSON schemas from AWS AppConfig
 
@@ -48,6 +55,12 @@ class AppConfigStore(StoreProvider):
             Alternative JMESPath options to be included when filtering expr
         logger: A logging object
             Used to log messages. If None is supplied, one will be created.
+        boto_config: botocore.config.Config, optional
+            Botocore configuration to pass during client initialization
+        boto3_session : boto3.Session, optional
+            Boto3 session to use for AWS API communication
+        boto3_client : AppConfigDataClient, optional
+            Boto3 AppConfigDataClient Client to use, boto3_session and boto_config will be ignored if both are provided
         """
         super().__init__()
         self.logger = logger or logging.getLogger(__name__)
@@ -55,10 +68,16 @@ class AppConfigStore(StoreProvider):
         self.application = application
         self.name = name
         self.cache_seconds = max_age
-        self.config = sdk_config
+        self.config = sdk_config or boto_config
         self.envelope = envelope
         self.jmespath_options = jmespath_options
-        self._conf_store = AppConfigProvider(environment=environment, application=application, config=sdk_config)
+        self._conf_store = AppConfigProvider(
+            environment=environment,
+            application=application,
+            config=sdk_config or boto_config,
+            boto3_client=boto3_client,
+            boto3_session=boto3_session,
+        )
 
     @property
     def get_raw_configuration(self) -> Dict[str, Any]:
--- examples/feature_flags/src/custom_boto_client_feature_flags.py
+++ examples/feature_flags/src/custom_boto_client_feature_flags.py
@@ -1,33 +1,17 @@
 from typing import Any
 
-from botocore.config import Config
-from jmespath.functions import Functions, signature
+import boto3
 
 from aws_lambda_powertools.utilities.feature_flags import AppConfigStore, FeatureFlags
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
-boto_config = Config(read_timeout=10, retries={"total_max_attempts": 2})
-
-
-# Custom JMESPath functions
-class CustomFunctions(Functions):
-    @signature({"types": ["object"]})
-    def _func_special_decoder(self, features):
-        # You can add some logic here
-        return features
-
-
-custom_jmespath_options = {"custom_functions": CustomFunctions()}
-
+boto3_client = boto3.client("appconfigdata")
 
 app_config = AppConfigStore(
     environment="dev",
     application="product-catalogue",
     name="features",
-    max_age=120,
-    envelope="special_decoder(features)",  # using a custom function defined in CustomFunctions Class
-    boto_config=boto_config,
-    jmespath_options=custom_jmespath_options,
+    boto3_client=boto3_client,
 )
 
 feature_flags = FeatureFlags(store=app_config)
