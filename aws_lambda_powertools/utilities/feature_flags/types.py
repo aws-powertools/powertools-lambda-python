287736fb3eeab935dded0fac80618131022c062b
Eric Nielsen <4120606+ericbn@users.noreply.github.com>
2024-08-15 12:10:45 -0500

    Fix type alias in Python 3.8
    See https://bugs.python.org/issue45117

--- aws_lambda_powertools/utilities/feature_flags/types.py
+++ aws_lambda_powertools/utilities/feature_flags/types.py
@@ -1,10 +1,8 @@
-from __future__ import annotations
-
-from typing import Any, TypeVar
+from typing import Any, Dict, List, TypeVar, Union
 
 from typing_extensions import ParamSpec
 
 # JSON primitives only, mypy doesn't support recursive tho
-JSONType = str | int | float | bool | None | dict[str, Any] | list[Any]
+JSONType = Union[str, int, float, bool, None, Dict[str, Any], List[Any]]
 T = TypeVar("T")
 P = ParamSpec("P")
