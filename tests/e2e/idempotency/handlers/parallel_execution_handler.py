54ae4017d0945298f66471ffc8270e2d802ffa10
Leandro Damascena <lcdama@amazon.pt>
2024-09-12 18:16:26 +0100

    Final pipelines

--- tests/e2e/idempotency/handlers/parallel_execution_handler.py
+++ tests/e2e/idempotency/handlers/parallel_execution_handler.py
@@ -12,6 +12,6 @@ persistence_layer = DynamoDBPersistenceLayer(table_name=TABLE_NAME)
 
 @idempotent(persistence_store=persistence_layer)
 def lambda_handler(event, context):
-    time.sleep(12)
+    time.sleep(15)
 
     return event
