8329bf34dd64e22768353676e71ad05becc17940
Mike W <12434761+mw-root@users.noreply.github.com>
2024-09-24 13:36:01 -0400

    feat(event_source): add CodeDeploy Lifecycle Hook event (#5219)
    * feat: Add CodeDeploy Lifecycle Hook event definition
    
    * Add tests
    
    * Update aws_lambda_powertools/utilities/data_classes/code_deploy_lifecycle_hook_event.py
    
    Co-authored-by: Leandro Damascena <lcdama@amazon.pt>
    Signed-off-by: Mike W <12434761+mw-root@users.noreply.github.com>
    
    * Update aws_lambda_powertools/utilities/data_classes/__init__.py
    
    Co-authored-by: Leandro Damascena <lcdama@amazon.pt>
    Signed-off-by: Mike W <12434761+mw-root@users.noreply.github.com>
    
    * Update tests/unit/data_classes/required_dependencies/test_code_deploy_lifecycle_hook_event.py
    
    Co-authored-by: Leandro Damascena <lcdama@amazon.pt>
    Signed-off-by: Mike W <12434761+mw-root@users.noreply.github.com>
    
    * Update tests/unit/data_classes/required_dependencies/test_code_deploy_lifecycle_hook_event.py
    
    Co-authored-by: Leandro Damascena <lcdama@amazon.pt>
    Signed-off-by: Mike W <12434761+mw-root@users.noreply.github.com>
    
    * Update aws_lambda_powertools/utilities/data_classes/__init__.py
    
    Co-authored-by: Leandro Damascena <lcdama@amazon.pt>
    Signed-off-by: Mike W <12434761+mw-root@users.noreply.github.com>
    
    * Add docs
    
    * Update docs/utilities/data_classes.md
    
    Co-authored-by: Leandro Damascena <lcdama@amazon.pt>
    Signed-off-by: Mike W <12434761+mw-root@users.noreply.github.com>
    
    * Update docs/utilities/data_classes.md
    
    Co-authored-by: Leandro Damascena <lcdama@amazon.pt>
    Signed-off-by: Mike W <12434761+mw-root@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Mike W <12434761+mw-root@users.noreply.github.com>
    Co-authored-by: Leandro Damascena <lcdama@amazon.pt>

---
+++ tests/unit/data_classes/required_dependencies/test_code_deploy_lifecycle_hook_event.py
@@ -0,0 +1,20 @@
+import pytest
+
+from aws_lambda_powertools.utilities.data_classes import (
+    CodeDeployLifecycleHookEvent,
+)
+from tests.functional.utils import load_event
+
+
+@pytest.mark.parametrize(
+    "event_file",
+    [
+        "codeDeployLifecycleHookEvent.json",
+    ],
+)
+def test_code_deploy_lifecycle_hook_event(event_file):
+    raw_event = load_event(event_file)
+    parsed_event = CodeDeployLifecycleHookEvent(raw_event)
+
+    assert parsed_event.deployment_id == raw_event["DeploymentId"]
+    assert parsed_event.lifecycle_event_hook_execution_id == raw_event["LifecycleEventHookExecutionId"]
