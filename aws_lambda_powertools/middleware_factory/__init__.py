80c1a07deac5f0dad3e795e6f618a4c16af1d698
Leandro Damascena <lcdama@amazon.pt>
2025-02-04 09:31:49 +0000

    Mkdocstrings - Middleware Factory

--- aws_lambda_powertools/middleware_factory/__init__.py
+++ aws_lambda_powertools/middleware_factory/__init__.py
@@ -1,4 +1,7 @@
-"""Utilities to enhance middlewares"""
+"""Utilities to enhance middlewares
+!!! abstract "Usage Documentation"
+    [`Middleware Factory`](../utilities/middleware_factory.md)
+"""
 
 from .factory import lambda_handler_decorator
 
