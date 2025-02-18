0d6a615b9904b7665efb0bb6328ee9b5a32bad67
Bas van Driel <contact@basvandriel.nl>
2025-02-18 22:19:37 +0100

    work on tests

--- aws_lambda_powertools/utilities/data_classes/iot_registry_event.py
+++ aws_lambda_powertools/utilities/data_classes/iot_registry_event.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from datetime import datetime
 from typing import Any, Dict, List, Literal, Optional
 
@@ -6,33 +8,63 @@ from aws_lambda_powertools.utilities.data_classes.common import DictWrapper
 EVENT_CRUD_OPERATION = Literal["CREATED", "UPDATED", "DELETED"]
 EVENT_ADD_REMOVE_OPERATION = Literal["ADDED", "REMOVED"]
 
-# TODO convert these to event source models and inherit from DictWrapper
-
 
 class IoTCoreRegistryEventsBase(DictWrapper):
-    # event_id: str = Field(..., alias="eventId")
-    # timestamp: datetime
-    ...
+    @property
+    def event_id(self) -> str:
+        return self["eventId"]
+
+    @property
+    def timestamp(self) -> datetime:
+        """
+        A Unix timestamp can be in seconds or milliseconds. If the value is 10 digits long, it's in seconds.
+        If it's 13 digits long, it's in milliseconds and should be divided by 1000 to convert it to seconds.
+        """
+        ts = self["timestamp"]
+        return datetime.fromtimestamp(ts / 1000 if ts > 10**10 else ts)
 
 
-class IoTCoreThingEvent(DictWrapper):
+class IoTCoreThingEvent(IoTCoreRegistryEventsBase):
     """
     Thing Created/Updated/Deleted
 
     The registry publishes event messages when things are created, updated, or deleted.
     """
 
-    # event_type: Literal["THING_EVENT"] = Field(..., alias="eventType")
-    # operation: EVENT_CRUD_OPERATION
-    # thing_id: str = Field(..., alias="thingId")
-    # account_id: str = Field(..., alias="accountId")
-    # thing_name: str = Field(..., alias="thingName")
-    # version_number: int = Field(..., alias="versionNumber")
-    # thing_type_name: Optional[str] = Field(None, alias="thingTypeName")
-    # attributes: Dict[str, Any]
+    @property
+    def event_type(self) -> Literal["THING_EVENT"]:
+        return self["eventType"]
+
+    @property
+    def operation(self) -> str:
+        return self["operation"]
+
+    @property
+    def thing_id(self) -> str:
+        return self["thingId"]
+
+    @property
+    def account_id(self) -> str:
+        return self["accountId"]
+
+    @property
+    def thing_name(self) -> str:
+        return self["thingName"]
+
+    @property
+    def version_number(self) -> int:
+        return self["versionNumber"]
+
+    @property
+    def thing_type_name(self) -> Optional[str]:
+        return self.get("thingTypeName")
+
+    @property
+    def attributes(self) -> Dict[str, Any]:
+        return self["attributes"]
 
 
-class IoTCoreThingTypeEvent(DictWrapper):
+class IoTCoreThingTypeEvent(IoTCoreRegistryEventsBase):
     """
     Thing Type Created/Updated/Deprecated/Undeprecated/Deleted
     The registry publishes event messages when thing types are created, updated, deprecated, undeprecated, or deleted.
@@ -43,19 +75,48 @@ class IoTCoreThingTypeEvent(DictWrapper):
         $aws/events/thingType/thingTypeName/deleted
     """
 
-    # event_type: Literal["THING_TYPE_EVENT"] = Field(..., alias="eventType")
-    # operation: EVENT_CRUD_OPERATION
-    # account_id: str = Field(..., alias="accountId")
-    # thing_type_id: str = Field(..., alias="thingTypeId")
-    # thing_type_name: str = Field(..., alias="thingTypeName")
-    # is_deprecated: bool = Field(..., alias="isDeprecated")
-    # deprecation_date: Optional[datetime] = Field(None, alias="deprecationDate")
-    # searchable_attributes: List[str] = Field(..., alias="searchableAttributes")
-    # propagating_attributes: List[Dict[str, str]] = Field(..., alias="propagatingAttributes")
-    # description: str
+    @property
+    def event_type(self) -> str:
+        return self["eventType"]
+
+    @property
+    def operation(self) -> EVENT_CRUD_OPERATION:
+        return self["operation"]
+
+    @property
+    def account_id(self) -> str:
+        return self["accountId"]
+
+    @property
+    def thing_type_id(self) -> str:
+        return self["thingTypeId"]
+
+    @property
+    def thing_type_name(self) -> str:
+        return self["thingTypeName"]
+
+    @property
+    def is_deprecated(self) -> bool:
+        return self["isDeprecated"]
+
+    @property
+    def deprecation_date(self) -> Optional[datetime]:
+        return datetime.fromisoformat(self["deprecationDate"]) if self.get("deprecationDate") else None
+
+    @property
+    def searchable_attributes(self) -> List[str]:
+        return self["searchableAttributes"]
+
+    @property
+    def propagating_attributes(self) -> List[Dict[str, str]]:
+        return self["propagatingAttributes"]
+
+    @property
+    def description(self) -> str:
+        return self["description"]
 
 
-class IoTCoreThingTypeAssociationEvent(DictWrapper):
+class IoTCoreThingTypeAssociationEvent(IoTCoreRegistryEventsBase):
     """
     The registry publishes event messages when a thing type is associated or disassociated with a thing.
 
@@ -64,14 +125,28 @@ class IoTCoreThingTypeAssociationEvent(DictWrapper):
         $aws/events/thingTypeAssociation/thing/thingName/thingType/typeName/removed
     """
 
-    # event_type: Literal["THING_TYPE_ASSOCIATION_EVENT"] = Field(..., alias="eventType")
-    # operation: EVENT_ADD_REMOVE_OPERATION
-    # thing_id: str = Field(..., alias="thingId")
-    # thing_name: str = Field(..., alias="thingName")
-    # thing_type_name: str = Field(..., alias="thingTypeName")
+    @property
+    def event_type(self) -> str:
+        return self["eventType"]
+
+    @property
+    def operation(self) -> Literal["THING_TYPE_ASSOCIATION_EVENT"]:
+        return self["operation"]
+
+    @property
+    def thing_id(self) -> str:
+        return self["thingId"]
+
+    @property
+    def thing_name(self) -> str:
+        return self["thingName"]
+
+    @property
+    def thing_type_name(self) -> str:
+        return self["thingTypeName"]
 
 
-class IoTCoreThingGroupEvent(DictWrapper):
+class IoTCoreThingGroupEvent(IoTCoreRegistryEventsBase):
     """
     The registry publishes the following event messages when a thing group is created, updated, or deleted.
 
@@ -81,21 +156,56 @@ class IoTCoreThingGroupEvent(DictWrapper):
         $aws/events/thingGroup/groupName/deleted
     """
 
-    # event_type: Literal["THING_GROUP_EVENT"] = Field(..., alias="eventType")
-    # operation: EVENT_CRUD_OPERATION
-    # account_id: str = Field(..., alias="accountId")
-    # thing_group_id: str = Field(..., alias="thingGroupId")
-    # thing_group_name: str = Field(..., alias="thingGroupName")
-    # version_number: int = Field(..., alias="versionNumber")
-    # parent_group_name: Optional[str] = Field(None, alias="parentGroupName")
-    # parent_group_id: Optional[str] = Field(None, alias="parentGroupId")
-    # description: str
-    # root_to_parent_thing_groups: List[Dict[str, str]] = Field(..., alias="rootToParentThingGroups")
-    # attributes: Dict[str, Any]
-    # dynamic_group_mapping_id: Optional[str] = Field(None, alias="dynamicGroupMappingId")
+    @property
+    def event_type(self) -> str:
+        return self["eventType"]
+
+    @property
+    def operation(self) -> EVENT_CRUD_OPERATION:
+        return self["operation"]
+
+    @property
+    def account_id(self) -> str:
+        return self["accountId"]
+
+    @property
+    def thing_group_id(self) -> str:
+        return self["thingGroupId"]
+
+    @property
+    def thing_group_name(self) -> str:
+        return self["thingGroupName"]
+
+    @property
+    def version_number(self) -> int:
+        return self["versionNumber"]
+
+    @property
+    def parent_group_name(self) -> Optional[str]:
+        return self.get("parentGroupName")
+
+    @property
+    def parent_group_id(self) -> Optional[str]:
+        return self.get("parentGroupId")
+
+    @property
+    def description(self) -> str:
+        return self["description"]
+
+    @property
+    def root_to_parent_thing_groups(self) -> List[Dict[str, str]]:
+        return self["rootToParentThingGroups"]
+
+    @property
+    def attributes(self) -> Dict[str, Any]:
+        return self["attributes"]
+
+    @property
+    def dynamic_group_mapping_id(self) -> Optional[str]:
+        return self.get("dynamicGroupMappingId")
 
 
-class IoTCoreAddOrRemoveFromThingGroupEvent(DictWrapper):
+class IoTCoreAddOrRemoveFromThingGroupEvent(IoTCoreRegistryEventsBase):
     """
     The registry publishes event messages when a thing is added to or removed from a thing group.
 
@@ -104,29 +214,64 @@ class IoTCoreAddOrRemoveFromThingGroupEvent(DictWrapper):
         $aws/events/thingGroupMembership/thingGroup/thingGroupName/thing/thingName/removed
     """
 
-    # event_type: Literal["THING_GROUP_MEMBERSHIP_EVENT"] = Field(..., alias="eventType")
-    # operation: EVENT_ADD_REMOVE_OPERATION
-    # account_id: str = Field(..., alias="accountId")
-    # group_arn: str = Field(..., alias="groupArn")
-    # group_id: str = Field(..., alias="groupId")
-    # thing_arn: str = Field(..., alias="thingArn")
-    # thing_id: str = Field(..., alias="thingId")
-    # membership_id: str = Field(..., alias="membershipId")
+    @property
+    def event_type(self) -> str:
+        return self["eventType"]
+
+    @property
+    def operation(self) -> EVENT_ADD_REMOVE_OPERATION:
+        return self["operation"]
+
+    @property
+    def account_id(self) -> str:
+        return self["accountId"]
+
+    @property
+    def group_arn(self) -> str:
+        return self["groupArn"]
+
+    @property
+    def group_id(self) -> str:
+        return self["groupId"]
+
+    @property
+    def thing_arn(self) -> str:
+        return self["thingArn"]
+
+    @property
+    def thing_id(self) -> str:
+        return self["thingId"]
+
+    @property
+    def membership_id(self) -> str:
+        return self["membershipId"]
 
 
-class IoTCoreAddOrDeleteFromThingGroupEvent(DictWrapper):
-    """
-    The registry publishes event messages when a thing group is added to or removed from another thing group.
+class IoTCoreAddOrDeleteFromThingGroupEvent(IoTCoreRegistryEventsBase):
+    @property
+    def event_type(self) -> str:
+        return self["eventType"]
 
-    Format:
-        $aws/events/thingGroupHierarchy/thingGroup/parentThingGroupName/childThingGroup/childThingGroupName/added
-        $aws/events/thingGroupHierarchy/thingGroup/parentThingGroupName/childThingGroup/childThingGroupName/removed
-    """
+    @property
+    def operation(self) -> EVENT_ADD_REMOVE_OPERATION:
+        return self["operation"]
 
-    # event_type: Literal["THING_GROUP_HIERARCHY_EVENT"] = Field(..., alias="eventType")
-    # operation: EVENT_ADD_REMOVE_OPERATION
-    # account_id: str = Field(..., alias="accountId")
-    # thing_group_id: str = Field(..., alias="thingGroupId")
-    # thing_group_name: str = Field(..., alias="thingGroupName")
-    # child_group_id: str = Field(..., alias="childGroupId")
-    # child_group_name: str = Field(..., alias="childGroupName")
+    @property
+    def account_id(self) -> str:
+        return self["accountId"]
+
+    @property
+    def thing_group_id(self) -> str:
+        return self["thingGroupId"]
+
+    @property
+    def thing_group_name(self) -> str:
+        return self["thingGroupName"]
+
+    @property
+    def child_group_id(self) -> str:
+        return self["childGroupId"]
+
+    @property
+    def child_group_name(self) -> str:
+        return self["childGroupName"]
