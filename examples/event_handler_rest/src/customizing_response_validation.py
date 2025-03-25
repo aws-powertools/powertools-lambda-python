249554f97b20823e68a7f462d4f914b684826526
Amin Farjadi <amin.farjadi@outlook.com>
2025-03-25 07:46:30 +0000

    feat: updated docs.

--- examples/event_handler_rest/src/customizing_response_validation.py
+++ examples/event_handler_rest/src/customizing_response_validation.py
@@ -33,6 +33,18 @@ def get_todo_by_id(todo_id: int) -> Todo:
     return todo.json()["title"]  # (2)!
 
 
+@app.get(
+    "/todos_bad_response_with_custom_http_code/<todo_id>",
+    custom_response_validation_http_code=HTTPStatus.UNPROCESSABLE_ENTITY,  # (3)!
+)
+@tracer.capture_method
+def get_todo_by_id_custom(todo_id: int) -> Todo:
+    todo = requests.get(f"https://jsonplaceholder.typicode.com/todos/{todo_id}")
+    todo.raise_for_status()
+
+    return todo.json()["title"]
+
+
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_HTTP)
 @tracer.capture_lambda_handler
 def lambda_handler(event: dict, context: LambdaContext) -> dict:
