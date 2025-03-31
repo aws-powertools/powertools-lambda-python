ec14a12c72fd1337e7a29e5d1924b8efc1abafdb
Leandro Damascena <lcdama@amazon.pt>
2025-03-31 15:51:06 +0100

    Initial commit

--- aws_lambda_powertools/event_handler/__init__.py
+++ aws_lambda_powertools/event_handler/__init__.py
@@ -11,6 +11,7 @@ from aws_lambda_powertools.event_handler.api_gateway import (
     Response,
 )
 from aws_lambda_powertools.event_handler.appsync import AppSyncResolver
+from aws_lambda_powertools.event_handler.appsync_events import AppSyncEventsResolver
 from aws_lambda_powertools.event_handler.bedrock_agent import BedrockAgentResolver
 from aws_lambda_powertools.event_handler.lambda_function_url import (
     LambdaFunctionUrlResolver,
@@ -19,6 +20,7 @@ from aws_lambda_powertools.event_handler.vpc_lattice import VPCLatticeResolver, 
 
 __all__ = [
     "AppSyncResolver",
+    "AppSyncEventsResolver",
     "APIGatewayRestResolver",
     "APIGatewayHttpResolver",
     "ALBResolver",
---
+++ aws_lambda_powertools/event_handler/appsync_events.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+
+class AppSyncEventsResolver:
+    """
+    AppSync Events API Resolver
+    """
+
+    def __init__(self):
+        pass
