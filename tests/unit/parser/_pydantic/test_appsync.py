dc80b3ffd6c48c46a644c06534911a7af7378ca0
Vatsal Goel <144617902+VatsalGoel3@users.noreply.github.com>
2025-04-04 13:28:31 -0600

    Update test code

--- tests/unit/parser/_pydantic/test_appsync.py
+++ tests/unit/parser/_pydantic/test_appsync.py
@@ -1,7 +1,8 @@
+import pytest
+
 from aws_lambda_powertools.utilities.parser import parse, ValidationError
 from aws_lambda_powertools.utilities.parser.models import AppSyncResolverEventModel
 from tests.functional.utils import load_event
-
 
 def test_appsync_event_model_parses_successfully():
     """
