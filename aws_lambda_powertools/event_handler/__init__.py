8463fb964bae46c1f986c410a456c240364d81e2
Ana Falcão <anafalcao@poli.ufrj.br>
2025-04-30 12:14:50 -0300

    feat(bedrock_agents): add optional fields to response payload (#6336)
    * feat(bedrock_agents): add optional fields to response payload
    
    * reformat bedrock response
    
    * fix type knowledge_base_config
    
    * fix type knowledge_base_config
    
    * fix bedrock response
    
    * mypy
    
    * mypy
    
    * remove unnecessary attributes and add docstrings
    
    * remove unnecessary attributes and add docstrings
    
    * add more tests
    
    * Fix middleware validation
    
    * Fix middleware validation
    
    ---------
    
    Co-authored-by: Leandro Damascena <lcdama@amazon.pt>
    Co-authored-by: Andrea Amorosi <dreamorosi@gmail.com>

--- aws_lambda_powertools/event_handler/__init__.py
+++ aws_lambda_powertools/event_handler/__init__.py
@@ -11,7 +11,7 @@ from aws_lambda_powertools.event_handler.api_gateway import (
     Response,
 )
 from aws_lambda_powertools.event_handler.appsync import AppSyncResolver
-from aws_lambda_powertools.event_handler.bedrock_agent import BedrockAgentResolver
+from aws_lambda_powertools.event_handler.bedrock_agent import BedrockAgentResolver, BedrockResponse
 from aws_lambda_powertools.event_handler.events_appsync.appsync_events import AppSyncEventsResolver
 from aws_lambda_powertools.event_handler.lambda_function_url import (
     LambdaFunctionUrlResolver,
@@ -26,6 +26,7 @@ __all__ = [
     "ALBResolver",
     "ApiGatewayResolver",
     "BedrockAgentResolver",
+    "BedrockResponse",
     "CORSConfig",
     "LambdaFunctionUrlResolver",
     "Response",
