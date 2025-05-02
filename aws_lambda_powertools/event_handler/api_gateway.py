dd0f1ad6e4392f5fe543a55e5eeb74375b31e29b
Lucas Braga <lucas.braga@dock.tech>
2025-05-02 10:34:47 -0700

    allowed a new deserializer for parser the body

--- aws_lambda_powertools/event_handler/api_gateway.py
+++ aws_lambda_powertools/event_handler/api_gateway.py
@@ -1575,6 +1575,7 @@ class ApiGatewayResolver(BaseRouter):
         strip_prefixes: list[str | Pattern] | None = None,
         enable_validation: bool = False,
         response_validation_error_http_code: HTTPStatus | int | None = None,
+        deserializer: Callable[[str], dict] | None = None,
     ):
         """
         Parameters
@@ -1596,6 +1597,9 @@ class ApiGatewayResolver(BaseRouter):
             Enables validation of the request body against the route schema, by default False.
         response_validation_error_http_code
             Sets the returned status code if response is not validated. enable_validation must be True.
+        deserializer: Callable[[str], dict], optional
+            function to deserialize `str`, `bytes`, `bytearray` containing a JSON document to a Python `dict`,
+            by default json.loads
         """
         self._proxy_type = proxy_type
         self._dynamic_routes: list[Route] = []
@@ -1621,6 +1625,7 @@ class ApiGatewayResolver(BaseRouter):
 
         # Allow for a custom serializer or a concise json serialization
         self._serializer = serializer or partial(json.dumps, separators=(",", ":"), cls=Encoder)
+        self._deserializer = deserializer
 
         if self._enable_validation:
             from aws_lambda_powertools.event_handler.middlewares.openapi_validation import OpenAPIValidationMiddleware
@@ -2431,24 +2436,24 @@ class ApiGatewayResolver(BaseRouter):
         """Convert the event dict to the corresponding data class"""
         if self._proxy_type == ProxyEventType.APIGatewayProxyEvent:
             logger.debug("Converting event to API Gateway REST API contract")
-            return APIGatewayProxyEvent(event)
+            return APIGatewayProxyEvent(event, self._deserializer)
         if self._proxy_type == ProxyEventType.APIGatewayProxyEventV2:
             logger.debug("Converting event to API Gateway HTTP API contract")
-            return APIGatewayProxyEventV2(event)
+            return APIGatewayProxyEventV2(event, self._deserializer)
         if self._proxy_type == ProxyEventType.BedrockAgentEvent:
             logger.debug("Converting event to Bedrock Agent contract")
-            return BedrockAgentEvent(event)
+            return BedrockAgentEvent(event, self._deserializer)
         if self._proxy_type == ProxyEventType.LambdaFunctionUrlEvent:
             logger.debug("Converting event to Lambda Function URL contract")
-            return LambdaFunctionUrlEvent(event)
+            return LambdaFunctionUrlEvent(event, self._deserializer)
         if self._proxy_type == ProxyEventType.VPCLatticeEvent:
             logger.debug("Converting event to VPC Lattice contract")
-            return VPCLatticeEvent(event)
+            return VPCLatticeEvent(event, self._deserializer)
         if self._proxy_type == ProxyEventType.VPCLatticeEventV2:
             logger.debug("Converting event to VPC LatticeV2 contract")
-            return VPCLatticeEventV2(event)
+            return VPCLatticeEventV2(event, self._deserializer)
         logger.debug("Converting event to ALB contract")
-        return ALBEvent(event)
+        return ALBEvent(event, self._deserializer)
 
     def _resolve(self) -> ResponseBuilder:
         """Resolves the response or return the not found response"""
@@ -2865,6 +2870,7 @@ class APIGatewayRestResolver(ApiGatewayResolver):
         strip_prefixes: list[str | Pattern] | None = None,
         enable_validation: bool = False,
         response_validation_error_http_code: HTTPStatus | int | None = None,
+        deserializer: Callable[[str], dict] | None = None,
     ):
         """Amazon API Gateway REST and HTTP API v1 payload resolver"""
         super().__init__(
@@ -2875,6 +2881,7 @@ class APIGatewayRestResolver(ApiGatewayResolver):
             strip_prefixes,
             enable_validation,
             response_validation_error_http_code,
+            deserializer,
         )
 
     def _get_base_path(self) -> str:
